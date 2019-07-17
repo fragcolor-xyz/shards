4ac8875b289824dc82e4127f6ffe52ae5f22f4e0
Giovanni <sinkingsugar@gmail.com>
2019-07-17 23:03:30 +0900

    small hack to load chainblocks.dll properly from module space

--- src/py/chainblocks/__init__.py
+++ src/py/chainblocks/__init__.py
@@ -1,3 +1,8 @@
+# load the dll from this path
+import ctypes, os
+temp = ctypes.cdll.LoadLibrary(os.path.dirname(__file__) + "/chainblocks")
+
+# load python stuff
 from .cbtypes import *
 from .cbchain import *
 from .cblocks import *
