ff703d5f35519c1bbd8750038da6f4776ada4917
Giovanni <sinkingsugar@gmail.com>
2019-08-03 13:28:17 +0900

    remove py, remove js, try enable linux tests

--- src/py/chainblocks/__init__.py
+++
@@ -1,8 +0,0 @@
-# load the dll from this path
-import ctypes, os
-temp = ctypes.cdll.LoadLibrary(os.path.dirname(__file__) + "/chainblocks")
-# temp = ctypes.cdll.LoadLibrary(os.path.dirname(__file__) + "/fragblocks")
-
-# load python stuff
-from .cbcore import *
-from .cblocks import *
