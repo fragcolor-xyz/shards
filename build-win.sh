151223734ac1b5360698e9bfe1810d27b141ec6f
Giovanni <sinkingsugar@gmail.com>
2019-10-16 01:36:05 +0900

    wip

--- build-win.sh
+++ build-win.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+pacman -Syu --noconfirm
 pacman -S --noconfirm base-devel mingw-w64-x86_64-toolchain mingw-w64-x86_64-cmake mingw-w64-x86_64-boost mingw-w64-x86_64-ninja mingw-w64-x86_64-clang
 mkdir build
 cd build
