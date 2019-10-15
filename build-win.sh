4bb46a502b73196931ebe37dd78feb843202a407
Giovanni <sinkingsugar@gmail.com>
2019-10-16 00:57:34 +0900

    wip

--- build-win.sh
+++ build-win.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-pacman -S --noconfirm base-devel mingw-w64-x86_64-toolchain mingw-w64-x86_64-cmake mingw-w64-x86_64-boost mingw-w64-x86_64-ninja
+pacman -S --noconfirm base-devel mingw-w64-x86_64-toolchain mingw-w64-x86_64-cmake mingw-w64-x86_64-boost mingw-w64-x86_64-ninja mingw-w64-x86_64-clang
 mkdir build
 cd build
 cmake -G Ninja -DBUILD_CORE_ONLY=1 -DCMAKE_BUILD_TYPE=Debug ..
