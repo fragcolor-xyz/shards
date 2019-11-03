9fada0d748b478b3da79f8c3149c3c2e552dcf7f
Giovanni <sinkingsugar@gmail.com>
2019-11-03 20:32:39 +0900

    Wip on full build (bgfx and co too)

--- build-win32.sh
+++ build-win32.sh
@@ -1,9 +1,23 @@
 #!/bin/sh
 
-pacman -S --noconfirm base-devel mingw-w64-i686-toolchain mingw-w64-i686-cmake mingw-w64-i686-boost mingw-w64-i686-ninja mingw-w64-i686-clang
+pacman -S --noconfirm base-devel mingw-w64-i686-toolchain mingw-w64-i686-cmake mingw-w64-i686-boost mingw-w64-i686-ninja mingw-w64-i686-clang wget
+
+# bimg
+cd deps/bimg/
+export MINGW=/d/a/_temp/msys/mingw32 # chainblocks/setup-msys2, needed by bimg
+make mingw-gcc-release32
+cd ../../
+
+# SDL
+mkdir external
+cd external
+wget -O sdl.tar.gz https://www.libsdl.org/release/SDL2-devel-2.0.10-mingw.tar.gz
+tar xzf sdl.tar.gz
+cd ../
+
 mkdir build
 cd build
-cmake -G Ninja -DBUILD_CORE_ONLY=1 -DCMAKE_BUILD_TYPE=Release ..
+cmake -G Ninja -DCMAKE_BUILD_TYPE=Release ..
 ninja cbl
 ./cbl ../src/tests/general.clj
 ./cbl ../src/tests/variables.clj
