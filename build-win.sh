0e5207649583b658b8adccde88349c0193b6c501
Giovanni <sinkingsugar@gmail.com>
2019-11-03 20:49:03 +0900

    Revert "Wip on full build (bgfx and co too)"
    This reverts commit 9fada0d748b478b3da79f8c3149c3c2e552dcf7f.

--- build-win.sh
+++ build-win.sh
@@ -1,23 +1,9 @@
 #!/bin/sh
 
-pacman -S --noconfirm base-devel mingw-w64-x86_64-toolchain mingw-w64-x86_64-cmake mingw-w64-x86_64-boost mingw-w64-x86_64-ninja mingw-w64-x86_64-clang wget
-
-# bimg
-cd deps/bimg/
-export MINGW=/d/a/_temp/msys/mingw32 # chainblocks/setup-msys2, needed by bimg
-make mingw-gcc-release32
-cd ../../
-
-# SDL
-mkdir external
-cd external
-wget -O sdl.tar.gz https://www.libsdl.org/release/SDL2-devel-2.0.10-mingw.tar.gz
-tar xzf sdl.tar.gz
-cd ../
-
+pacman -S --noconfirm base-devel mingw-w64-x86_64-toolchain mingw-w64-x86_64-cmake mingw-w64-x86_64-boost mingw-w64-x86_64-ninja mingw-w64-x86_64-clang
 mkdir build
 cd build
-cmake -G Ninja -DCMAKE_BUILD_TYPE=Release ..
+cmake -G Ninja -DBUILD_CORE_ONLY=1 -DCMAKE_BUILD_TYPE=Release ..
 ninja cbl
 ./cbl ../src/tests/general.clj
 ./cbl ../src/tests/variables.clj
