c5cf463b333999690d48600e26a507f8fad34d4c
Giovanni <sinkingsugar@gmail.com>
2019-11-06 11:49:44 +0900

    Try workaround github win64 CI issue testing network

--- src/tests/network.clj
+++ src/tests/network.clj
@@ -66,9 +66,9 @@
 (sleep 1)
 (stop network-test-server)
 (stop network-test-client)
-; destroy chains
-(def network-test-server nil)
-(def network-test-client nil)
-(def client-init nil)
+; destroy chains - issues with CI, to investigate better.. valgrind, gdb, drmingw said no problem tho...
+;(def network-test-server nil)
+;(def network-test-client nil)
+;(def client-init nil)
 (sleep 1)
           
