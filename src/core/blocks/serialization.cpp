0d587c6cbe0491bcb472789bb87809331b001eca
Giovanni Petrantoni <giovanni@fragcolor.xyz>
2019-11-30 17:57:03 +0900

    Add LoadImage

--- src/core/blocks/serialization.cpp
+++ src/core/blocks/serialization.cpp
@@ -1,8 +1,16 @@
 /* SPDX-License-Identifier: BSD 3-Clause "New" or "Revised" License */
 /* Copyright © 2019 Giovanni Petrantoni */
 
+#define STB_IMAGE_IMPLEMENTATION
+
 #include "shared.hpp"
+#include <future>
+#include <stb_image.h>
 #include <string>
+
+// TODO, make ReadFile and WriteFile use maybe ASIO async to do syscalls in a
+// worker thread Maybe as a option, its kinda something to profile, but for now
+// lets wait a proper usage
 
 namespace chainblocks {
 struct FileBase {
@@ -140,8 +148,70 @@ RUNTIME_BLOCK_getParam(ReadFile);
 RUNTIME_BLOCK_activate(ReadFile);
 RUNTIME_BLOCK_END(ReadFile);
 
+struct LoadImage : public FileBase {
+  static CBTypesInfo inputTypes() { return CBTypesInfo(SharedTypes::noneInfo); }
+  static CBTypesInfo outputTypes() {
+    return CBTypesInfo(SharedTypes::imageInfo);
+  }
+
+  CBVar _output{};
+
+  void cleanup() override {
+    if (_output.valueType == Image && _output.payload.imageValue.data) {
+      stbi_image_free(_output.payload.imageValue.data);
+      _output = {};
+    }
+    FileBase::cleanup();
+  }
+
+  CBVar activate(CBContext *context, const CBVar &input) {
+    auto &fvar = _filename(context);
+    if (fvar.valueType == String) {
+      auto filename = fvar.payload.stringValue;
+      auto asyncRes = std::async(
+          std::launch::async,
+          [](std::string filename) {
+            CBVar res{};
+            res.valueType = Image;
+            int x, y, n;
+            res.payload.imageValue.data =
+                stbi_load(filename.c_str(), &x, &y, &n, 0);
+            if (!res.payload.imageValue.data) {
+              throw CBException("Failed to load image file");
+            }
+            res.payload.imageValue.width = uint16_t(x);
+            res.payload.imageValue.height = uint16_t(y);
+            res.payload.imageValue.channels = uint16_t(n);
+            return res;
+          },
+          filename);
+
+      // Wait suspending!
+      while (true) {
+        auto state = asyncRes.wait_for(std::chrono::seconds(0));
+        if (state == std::future_status::ready)
+          break;
+        auto chainState = chainblocks::suspend(context, 0);
+        if (chainState.payload.chainState != Continue) {
+          // Here communicate to the thread.. but hmm should be fine without
+          // anything in this case, cannot send cancelation anyway
+          return chainState;
+        }
+      }
+
+      // This should also throw if we had exceptions
+      return asyncRes.get();
+    } else {
+      throw CBException("No file name to load was given");
+    }
+  }
+};
+
+typedef BlockWrapper<LoadImage> LoadImageBlock;
+
 void registerSerializationBlocks() {
   REGISTER_CORE_BLOCK(WriteFile);
   REGISTER_CORE_BLOCK(ReadFile);
+  registerBlock("LoadImage", &LoadImageBlock::create);
 }
 }; // namespace chainblocks
