ac2b39f0e07962cdad4885222723e4232e438587
Giovanni Petrantoni <giovanni@fragcolor.xyz>
2020-01-30 01:05:49 +0900

    Add dbg builds for windows

--- build-win32.sh
+++ build-win32.sh
@@ -3,7 +3,7 @@
 # fail on errors
 set -e
 
-pacman -S --noconfirm base-devel mingw-w64-i686-toolchain mingw-w64-i686-cmake mingw-w64-i686-boost mingw-w64-i686-ninja mingw-w64-i686-clang wget
+pacman -S --needed --noconfirm base-devel mingw-w64-i686-toolchain mingw-w64-i686-cmake mingw-w64-i686-boost mingw-w64-i686-ninja mingw-w64-i686-clang wget
 
 # bimg
 cd deps/bimg/
@@ -31,7 +31,7 @@ cd ../
 
 mkdir build
 cd build
-cmake -G Ninja -DCMAKE_BUILD_TYPE=Release ..
+cmake -G Ninja -DCMAKE_BUILD_TYPE=$1 ..
 ninja cbl
 ./cbl ../src/tests/general.clj
 ./cbl ../src/tests/variables.clj
