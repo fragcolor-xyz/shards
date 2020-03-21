cff3aa464de171306236d2e5122e1855308e4c0c
Giovanni Petrantoni <giovanni@fragcolor.xyz>
2020-03-21 14:19:13 +0900

    Genetic wip

--- src/core/blocks/genetic.cpp
+++ src/core/blocks/genetic.cpp
@@ -2,6 +2,7 @@
 #define GENETIC_H
 
 #include "shared.hpp"
+#include <limits>
 #include <sstream>
 #include <taskflow/taskflow.hpp>
 
@@ -18,7 +19,7 @@ struct Evolve {
       _baseChain = value;
       break;
     case 1:
-      _population = value.payload.intValue;
+      _popsize = value.payload.intValue;
       break;
     case 2:
       _mutation = value.payload.floatValue;
@@ -29,6 +30,8 @@ struct Evolve {
     case 4:
       _elitism = value.payload.floatValue;
       break;
+    case 5:
+      _dnaNames = value;
     default:
       break;
     }
@@ -39,13 +42,15 @@ struct Evolve {
     case 0:
       return _baseChain;
     case 1:
-      return Var(_population);
+      return Var(_popsize);
     case 2:
       return Var(_mutation);
     case 3:
       return Var(_extinction);
     case 4:
       return Var(_elitism);
+    case 5:
+      return _dnaNames;
     default:
       return CBVar();
     }
@@ -68,30 +73,59 @@ struct Evolve {
   };
 
   void cleanup() {
-    if (_chains.size() > 0) {
+    if (_population.size() > 0) {
       tf::Taskflow cleanupFlow;
-      cleanupFlow.parallel_for(_chains.begin(), _chains.end(), [&](CBVar &var) {
-        auto chain = CBChain::sharedFromRef(var.payload.chainValue);
-        stop(chain.get());
-        Serialization::varFree(var);
-      });
+      cleanupFlow.parallel_for(
+          _population.begin(), _population.end(), [&](Individual &i) {
+            // Free and release chain
+            auto chain = CBChain::sharedFromRef(i.chain.payload.chainValue);
+            stop(chain.get());
+            Serialization::varFree(i.chain);
+            // Cleanup variables too
+            i.variables.clear();
+          });
       Tasks.run(cleanupFlow).get();
+
+      _chain2Individual.clear();
+      _sortedPopulation.clear();
+      _population.clear();
     }
     _baseChain.cleanup();
   }
 
   void warmup(CBContext *ctx) { _baseChain.warmup(ctx); }
 
+  inline void mutateChain(CBChain *chain);
+
   struct TickObserver {
+    Evolve &self;
+
     void before_tick(CBChain *chain) {}
     void before_prepare(CBChain *chain) {}
 
     void before_stop(CBChain *chain) {
       // Collect DNA variables values
+      const auto individualIt = self._chain2Individual.find(chain);
+      if (individualIt == self._chain2Individual.end()) {
+        assert(false);
+      }
+      auto &individual = individualIt->second.get();
     }
 
     void before_start(CBChain *chain) {
       // Do mutations
+      const auto individualIt = self._chain2Individual.find(chain);
+      if (individualIt == self._chain2Individual.end()) {
+        assert(false);
+      }
+      auto &individual = individualIt->second.get();
+      // Load variables if we have them
+      for (auto &var : individual.variables) {
+        auto &slot = chain->variables[var.first];
+        cloneVar(slot, var.second);
+      }
+      // Call mutate
+      self.mutateChain(chain);
     }
   };
 
@@ -101,41 +135,66 @@ struct Evolve {
       // Init on the first run!
       // We reuse those chains for every era
       // Only the DNA changes
-      if (_chains.size() == 0) {
+      if (_population.size() == 0) {
         std::stringstream chainStream;
         Writer w(chainStream);
         Serialization::serialize(_baseChain, w);
         auto chainStr = chainStream.str();
-        _chains.resize(_population);
+        _population.resize(_popsize);
         tf::Taskflow initFlow;
-        initFlow.parallel_for(_chains.begin(), _chains.end(), [&](CBVar &var) {
-          std::stringstream inputStream(chainStr);
-          Reader r(inputStream);
-          Serialization::deserialize(r, var);
-        });
+        initFlow.parallel_for(_population.begin(), _population.end(),
+                              [&](Individual &i) {
+                                std::stringstream inputStream(chainStr);
+                                Reader r(inputStream);
+                                Serialization::deserialize(r, i.chain);
+                              });
         Tasks.run(initFlow).get();
+
+        // Also populate chain2Indi
+        for (auto &i : _population) {
+          auto chain = CBChain::sharedFromRef(i.chain.payload.chainValue);
+          _chain2Individual.emplace(chain.get(), i);
+          _sortedPopulation.emplace_back(i);
+        }
       }
       // We run chains up to completion
       // From validation to end, every iteration/era
       tf::Taskflow runFlow;
-      runFlow.parallel_for(_chains.begin(), _chains.end(), [&](CBVar &var) {
-        CBNode node;
-        TickObserver obs;
-        auto chain = CBChain::sharedFromRef(var.payload.chainValue);
-        node.schedule(obs, chain.get());
-        while (!node.empty()) {
-          if (!node.tick(obs)) {
-            // Are we ignoring errors?
-          }
-        }
-      });
+      runFlow.parallel_for(
+          _population.begin(), _population.end(), [&](Individual &i) {
+            CBNode node;
+            TickObserver obs{*this};
+            auto chain = CBChain::sharedFromRef(i.chain.payload.chainValue);
+            node.schedule(obs, chain.get());
+            while (!node.empty()) {
+              if (!node.tick(obs)) {
+                // Are we ignoring errors?
+              }
+            }
+          });
       Tasks.run(runFlow).get();
-      // Find the best fitness.. do stuff...
-      return Var(0.0);
+
+      std::sort(_sortedPopulation.begin(), _sortedPopulation.end(),
+                [](std::reference_wrapper<Individual> a,
+                   std::reference_wrapper<Individual> b) {
+                  return a.get().fitness > b.get().fitness;
+                });
+
+      return Var(_sortedPopulation.back().get().fitness);
     });
   }
 
 private:
+  struct Individual {
+    CBVar chain{};
+    std::unordered_map<std::string, OwnedVar, std::hash<std::string>,
+                       std::equal_to<std::string>,
+                       boost::alignment::aligned_allocator<
+                           std::pair<const std::string, OwnedVar>, 16>>
+        variables;
+    double fitness{std::numeric_limits<double>::min()};
+  };
+
   tf::Executor &Tasks{Singleton<tf::Executor>::value};
   static inline Parameters _params{
       {"Fitness",
@@ -151,8 +210,12 @@ private:
        "A list of variable names that represent the DNA of the subject.",
        {CoreInfo::StringSeqType}}};
   ParamVar _baseChain{};
-  std::vector<CBVar> _chains;
-  int64_t _population = 64;
+  ParamVar _dnaNames{};
+  std::vector<Individual> _population;
+  std::vector<std::reference_wrapper<Individual>> _sortedPopulation;
+  std::unordered_map<CBChain *, std::reference_wrapper<Individual>>
+      _chain2Individual;
+  int64_t _popsize = 64;
   double _mutation = 0.2;
   double _extinction = 0.1;
   double _elitism = 0.1;
@@ -161,6 +224,16 @@ private:
 struct Mutant {};
 
 void registerBlocks() { REGISTER_CBLOCK("Evolve", Evolve); }
+
+inline void mutateBlock(CBlock *blk) {
+  // must find a mutant root or inner
+}
+
+inline void Evolve::mutateChain(CBChain *chain) {
+  for (auto &blk : chain->blocks) {
+    mutateBlock(blk);
+  }
+}
 } // namespace Genetic
 } // namespace chainblocks
 
