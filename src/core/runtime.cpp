4ecaed35f12b53088044c70a1e5480c3e3e5ca09
Giovanni Petrantoni <giovanni@fragcolor.xyz>
2020-04-12 13:06:50 +0900

    Fix flowstopper check bug in validation

--- src/core/runtime.cpp
+++ src/core/runtime.cpp
@@ -1288,9 +1288,9 @@ CBValidationResult validateConnections(const std::vector<CBlock *> &chain,
         chainblocks::arrayPush(result.exposedInfo, type);
     }
   }
-  if (strcmp(ctx.bottom->name(ctx.bottom), "Restart") == 0 ||
-      strcmp(ctx.bottom->name(ctx.bottom), "Stop") == 0 ||
-      strcmp(ctx.bottom->name(ctx.bottom), "Return") == 0) {
+  if (ctx.bottom && (strcmp(ctx.bottom->name(ctx.bottom), "Restart") == 0 ||
+                     strcmp(ctx.bottom->name(ctx.bottom), "Stop") == 0 ||
+                     strcmp(ctx.bottom->name(ctx.bottom), "Return")) == 0) {
     result.flowStopper = true;
   }
   return result;
