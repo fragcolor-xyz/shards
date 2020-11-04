b10e2c974fa85496a91ec4a3af6285f157838edd
Giovanni Petrantoni <sinkingsugar@gmail.com>
2020-11-04 09:44:38 +0900

    minor rust fixes

--- rust/src/blocks/io.rs
+++ rust/src/blocks/io.rs
@@ -1,85 +1,85 @@
 mod csv {
-  extern crate csv;
-  mod read {
-    use crate::block::Block;
-    use crate::chainblocksc::CBStrings;
-    use crate::chainblocksc::CBTypeInfo_Details;
-    use crate::chainblocksc::CBTypeInfo_Details_Path;
-    use crate::chainblocksc::CBType_ContextVar;
-    use crate::chainblocksc::CBType_Path;
-    use crate::chainblocksc::CBType_Seq;
-    use crate::chainblocksc::CBType_String;
-    use crate::chainblocksc::CBTypesInfo;
-    use crate::core::getRootPath;
-    use crate::core::init;
-    use crate::core::referenceVariable;
-    use crate::core::registerBlock;
-    use crate::core::releaseVariable;
-    use crate::types::common_type;
-    use crate::types::ClonedVar;
-    use crate::types::Context;
-    use crate::types::ParameterInfo;
-    use crate::types::Parameters;
-    use crate::types::Type;
-    use crate::types::Types;
-    use crate::types::Var;
-    use core::any::Any;
-    use core::cell::Cell;
-    use csv::Error;
-    use csv::Reader;
-    use csv::ReaderBuilder;
-    use csv::StringRecord;
-    use std::convert::TryFrom;
-    use std::convert::TryInto;
-    use std::ffi::CString;
-    use std::io::Read;
-    use std::path::Path;
+    extern crate csv;
+    mod read {
+        use crate::block::Block;
+        use crate::chainblocksc::CBStrings;
+        use crate::chainblocksc::CBTypeInfo_Details;
+        use crate::chainblocksc::CBTypeInfo_Details_Path;
+        use crate::chainblocksc::CBType_ContextVar;
+        use crate::chainblocksc::CBType_Path;
+        use crate::chainblocksc::CBType_Seq;
+        use crate::chainblocksc::CBType_String;
+        use crate::chainblocksc::CBTypesInfo;
+        use crate::core::getRootPath;
+        use crate::core::init;
+        use crate::core::referenceVariable;
+        use crate::core::registerBlock;
+        use crate::core::releaseVariable;
+        use crate::types::common_type;
+        use crate::types::ClonedVar;
+        use crate::types::Context;
+        use crate::types::ParameterInfo;
+        use crate::types::Parameters;
+        use crate::types::Type;
+        use crate::types::Types;
+        use crate::types::Var;
+        use core::any::Any;
+        use core::cell::Cell;
+        use csv::Error;
+        use csv::Reader;
+        use csv::ReaderBuilder;
+        use csv::StringRecord;
+        use std::convert::TryFrom;
+        use std::convert::TryInto;
+        use std::ffi::CString;
+        use std::io::Read;
+        use std::path::Path;
 
-    // either all in one go or one record per iteration
-    // from string variable or path (a file)
+        // either all in one go or one record per iteration
+        // from string variable or path (a file)
 
-    struct Row {
-      strs: Vec<CString>,
-      vars: Vec<Var>,
-    }
+        struct Row {
+            strs: Vec<CString>,
+            vars: Vec<Var>,
+        }
 
-    struct CSVRead {
-      input_types: Types,
-      strings: Types,
-      output_types: Types,
-      buffer_types: Types,
-      parameters: Parameters,
-      iterating: bool,
-      looped: bool,
-      reinit: bool,
-      source: ClonedVar,
-      records: Option<Box<dyn Iterator<Item = Result<StringRecord, Error>>>>,
-      rows: Vec<Row>,
-      slurp: Vec<Var>,
-      headers: bool,
-    }
+        struct CSVRead {
+            input_types: Types,
+            strings: Types,
+            output_types: Types,
+            buffer_types: Types,
+            parameters: Parameters,
+            iterating: bool,
+            looped: bool,
+            reinit: bool,
+            source: ClonedVar,
+            records: Option<Box<dyn Iterator<Item = Result<StringRecord, Error>>>>,
+            rows: Vec<Row>,
+            slurp: Vec<Var>,
+            headers: bool,
+        }
 
-    impl Default for CSVRead {
-      fn default() -> Self {
-        let path = Type {
-          basicType: CBType_Path,
-          details: CBTypeInfo_Details {
-            path: CBTypeInfo_Details_Path {
-              extensions: CBStrings::default(),
-              existing: true,
-              isFile: true,
-              relative: true,
-            },
-          },
-        };
-        let buffer_types = vec![common_type::string, path];
-        let buffer = Type {
-          basicType: CBType_ContextVar,
-          details: CBTypeInfo_Details {
-            contextVarTypes: CBTypesInfo::from(&buffer_types),
-          },
-        };
-        Self {
+        impl Default for CSVRead {
+            fn default() -> Self {
+                let path = Type {
+                    basicType: CBType_Path,
+                    details: CBTypeInfo_Details {
+                        path: CBTypeInfo_Details_Path {
+                            extensions: CBStrings::default(),
+                            existing: true,
+                            isFile: true,
+                            relative: true,
+                        },
+                    },
+                };
+                let buffer_types = vec![common_type::string, path];
+                let buffer = Type {
+                    basicType: CBType_ContextVar,
+                    details: CBTypeInfo_Details {
+                        contextVarTypes: CBTypesInfo::from(&buffer_types),
+                    },
+                };
+                Self {
                     input_types: vec![common_type::none],
                     strings: vec![common_type::strings],
                     output_types: vec![common_type::strings],
@@ -115,179 +115,180 @@ mod csv {
                     slurp: Vec::<Var>::new(),
                     headers: true,
                 }
-      }
-    }
-
-    impl CSVRead {
-      fn load_file_reader(&mut self, path: &str) {
-        let root_path = Path::new(getRootPath());
-        let fullpath = root_path.join(path);
-        let reader = ReaderBuilder::new()
-          .has_headers(self.headers)
-          .from_path(fullpath)
-          .unwrap();
-        self.records = Some(Box::new(reader.into_records()));
-      }
-
-      fn load_str_reader(&mut self, text: &'static [u8]) {
-        let reader = ReaderBuilder::new()
-          .has_headers(self.headers)
-          .from_reader(text);
-        self.records = Some(Box::new(reader.into_records()));
-      }
-    }
-
-    impl Block for CSVRead {
-      fn name(&mut self) -> &str {
-        "CSV.Read"
-      }
-      fn inputTypes(&mut self) -> &Types {
-        &self.input_types
-      }
-      fn outputTypes(&mut self) -> &Types {
-        if self.iterating {
-          self.output_types = vec![common_type::strings];
-        } else {
-          self.output_types = vec![Type {
-            basicType: CBType_Seq,
-            details: CBTypeInfo_Details {
-              seqTypes: (&self.strings).into(),
-            },
-          }];
-        }
-        &self.output_types
-      }
-      fn parameters(&mut self) -> Option<&Parameters> {
-        Some(&self.parameters)
-      }
-      fn setParam(&mut self, idx: i32, value: &Var) {
-        match idx {
-          0 => {
-            self.source = value.into();
-            self.reinit = true;
-          }
-          1 => {
-            self.iterating = value.try_into().unwrap();
-          }
-          2 => {
-            self.looped = value.try_into().unwrap();
-          }
-          3 => {
-            self.headers = value.try_into().unwrap();
-          }
-          _ => {
-            unimplemented!();
-          }
-        };
-      }
-      fn getParam(&mut self, idx: i32) -> Var {
-        match idx {
-          0 => self.source.0,
-          1 => Var::from(self.iterating),
-          2 => Var::from(self.looped),
-          3 => Var::from(self.headers),
-          _ => {
-            unimplemented!();
-          }
-        }
-      }
-      fn activate(&mut self, context: &Context, _input: &Var) -> Var {
-        if self.reinit {
-          self.records = None;
-          match self.source.0.valueType {
-            CBType_Path => {
-              let vcpath = CString::try_from(&self.source.0).unwrap();
-              let vpath = vcpath.to_str().unwrap();
-              self.load_file_reader(vpath);
             }
-            CBType_ContextVar => {
-              let var = referenceVariable(context, (&self.source.0).try_into().unwrap());
-              match var.valueType {
-                CBType_Path => {
-                  let vcpath = CString::try_from(var).unwrap();
-                  let vpath = vcpath.to_str().unwrap();
-                  self.load_file_reader(vpath);
-                }
-                CBType_String => {
-                  self.load_str_reader(var.try_into().unwrap());
-                }
-                _ => panic!("Wrong Source type (variable)"),
-              }
-              releaseVariable(var);
-            }
-            _ => panic!("Wrong Source type."),
-          };
-          self.reinit = false;
         }
 
-        self.rows.clear();
-        self.slurp.clear();
-        if let Some(records) = self.records.as_mut() {
-          if self.iterating {
-            // a single seq of strings
-            if let Some(record) = records.next() {
-              if let Ok(data) = record {
-                let it = data.iter();
-                let mut row = Row {
-                  strs: Vec::<CString>::new(),
-                  vars: Vec::<Var>::new(),
+        impl CSVRead {
+            fn load_file_reader(&mut self, path: &str) {
+                let root_path = Path::new(getRootPath());
+                let fullpath = root_path.join(path);
+                let reader = ReaderBuilder::new()
+                    .has_headers(self.headers)
+                    .from_path(fullpath)
+                    .unwrap();
+                self.records = Some(Box::new(reader.into_records()));
+            }
+
+            fn load_str_reader(&mut self, text: &'static [u8]) {
+                let reader = ReaderBuilder::new()
+                    .has_headers(self.headers)
+                    .from_reader(text);
+                self.records = Some(Box::new(reader.into_records()));
+            }
+        }
+
+        impl Block for CSVRead {
+            fn name(&mut self) -> &str {
+                "CSV.Read"
+            }
+            fn inputTypes(&mut self) -> &Types {
+                &self.input_types
+            }
+            fn outputTypes(&mut self) -> &Types {
+                if self.iterating {
+                    self.output_types = vec![common_type::strings];
+                } else {
+                    self.output_types = vec![Type {
+                        basicType: CBType_Seq,
+                        details: CBTypeInfo_Details {
+                            seqTypes: (&self.strings).into(),
+                        },
+                    }];
+                }
+                &self.output_types
+            }
+            fn parameters(&mut self) -> Option<&Parameters> {
+                Some(&self.parameters)
+            }
+            fn setParam(&mut self, idx: i32, value: &Var) {
+                match idx {
+                    0 => {
+                        self.source = value.into();
+                        self.reinit = true;
+                    }
+                    1 => {
+                        self.iterating = value.try_into().unwrap();
+                    }
+                    2 => {
+                        self.looped = value.try_into().unwrap();
+                    }
+                    3 => {
+                        self.headers = value.try_into().unwrap();
+                    }
+                    _ => {
+                        unimplemented!();
+                    }
                 };
-                for item in it {
-                  let s = CString::new(item).unwrap();
-                  row.vars.push(Var::from(&s));
-                  row.strs.push(s);
+            }
+            fn getParam(&mut self, idx: i32) -> Var {
+                match idx {
+                    0 => self.source.0,
+                    1 => Var::from(self.iterating),
+                    2 => Var::from(self.looped),
+                    3 => Var::from(self.headers),
+                    _ => {
+                        unimplemented!();
+                    }
                 }
-                self.rows.push(row);
-                Var::from(&self.rows[0].vars)
-              } else {
-                Var::from(&self.slurp)
-              }
-            } else {
-              if self.looped {
-                // no optimal at all..
-                // should use seek
-                self.reinit = true;
-              }
-              Var::from(&self.slurp)
             }
-          } else {
-            // parses the whole file
-            for record in records {
-              if let Ok(data) = record {
-                let it = data.iter();
-                let mut row = Row {
-                  strs: Vec::<CString>::new(),
-                  vars: Vec::<Var>::new(),
-                };
-                for item in it {
-                  let s = CString::new(item).unwrap();
-                  row.vars.push(Var::from(&s));
-                  row.strs.push(s);
+            fn activate(&mut self, context: &Context, _input: &Var) -> Var {
+                if self.reinit {
+                    self.records = None;
+                    match self.source.0.valueType {
+                        CBType_Path => {
+                            let vcpath = CString::try_from(&self.source.0).unwrap();
+                            let vpath = vcpath.to_str().unwrap();
+                            self.load_file_reader(vpath);
+                        }
+                        CBType_ContextVar => {
+                            let var =
+                                referenceVariable(context, (&self.source.0).try_into().unwrap());
+                            match var.valueType {
+                                CBType_Path => {
+                                    let vcpath = CString::try_from(var).unwrap();
+                                    let vpath = vcpath.to_str().unwrap();
+                                    self.load_file_reader(vpath);
+                                }
+                                CBType_String => {
+                                    self.load_str_reader(var.try_into().unwrap());
+                                }
+                                _ => panic!("Wrong Source type (variable)"),
+                            }
+                            releaseVariable(var);
+                        }
+                        _ => panic!("Wrong Source type."),
+                    };
+                    self.reinit = false;
                 }
-                self.rows.push(row);
-              }
-            }
 
-            for row in &self.rows {
-              self.slurp.push(Var::from(&row.vars));
-            }
+                self.rows.clear();
+                self.slurp.clear();
+                if let Some(records) = self.records.as_mut() {
+                    if self.iterating {
+                        // a single seq of strings
+                        if let Some(record) = records.next() {
+                            if let Ok(data) = record {
+                                let it = data.iter();
+                                let mut row = Row {
+                                    strs: Vec::<CString>::new(),
+                                    vars: Vec::<Var>::new(),
+                                };
+                                for item in it {
+                                    let s = CString::new(item).unwrap();
+                                    row.vars.push(Var::from(&s));
+                                    row.strs.push(s);
+                                }
+                                self.rows.push(row);
+                                Var::from(&self.rows[0].vars)
+                            } else {
+                                Var::from(&self.slurp)
+                            }
+                        } else {
+                            if self.looped {
+                                // no optimal at all..
+                                // should use seek
+                                self.reinit = true;
+                            }
+                            Var::from(&self.slurp)
+                        }
+                    } else {
+                        // parses the whole file
+                        for record in records {
+                            if let Ok(data) = record {
+                                let it = data.iter();
+                                let mut row = Row {
+                                    strs: Vec::<CString>::new(),
+                                    vars: Vec::<Var>::new(),
+                                };
+                                for item in it {
+                                    let s = CString::new(item).unwrap();
+                                    row.vars.push(Var::from(&s));
+                                    row.strs.push(s);
+                                }
+                                self.rows.push(row);
+                            }
+                        }
 
-            Var::from(&self.slurp)
-          }
-        } else {
-          Var::from(&self.slurp)
+                        for row in &self.rows {
+                            self.slurp.push(Var::from(&row.vars));
+                        }
+
+                        Var::from(&self.slurp)
+                    }
+                } else {
+                    Var::from(&self.slurp)
+                }
+            }
         }
-      }
+
+        #[ctor]
+        fn attach() {
+            init();
+            registerBlock::<CSVRead>("CSV.Read");
+        }
     }
 
-    #[ctor]
-    fn attach() {
-      init();
-      registerBlock::<CSVRead>("CSV.Read");
+    mod write {
+        // either append to file/buffer or overwrite each activation
     }
-  }
-
-  mod write {
-    // either append to file/buffer or overwrite each activation
-  }
 }
--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -7,7 +7,7 @@
 #![allow(improper_ctypes)]
 
 #[macro_use]
-extern crate ctor;
+#[cfg(test)] extern crate ctor;
 
 extern crate libc;
 
@@ -135,101 +135,102 @@ macro_rules! blocks {
 
 #[cfg(feature = "cb_static")]
 mod cb_static {
-  #[link(name = "cb_static", kind = "static")]
-  extern "C" {}
+    #[link(name = "cb_static", kind = "static")]
+    extern "C" {}
 }
 
 #[cfg(feature = "cb_dynamic")]
 mod cb_static {
-  #[link(name = "cb_shared", kind = "dylib")]
-  extern "C" {}
+    #[link(name = "cb_shared", kind = "dylib")]
+    extern "C" {}
 }
 
 // --features "dummy"
 // #[cfg(any(test, feature = "dummy"))]
 #[macro_use]
 mod dummy_block {
-  // run with: RUST_BACKTRACE=1 cargo test -- --nocapture
+    // run with: RUST_BACKTRACE=1 cargo test -- --nocapture
 
-  use super::block::create;
-  use super::Types;
-  use crate::block::cblock_construct;
-  use crate::block::Block;
-  use crate::block::BlockWrapper;
-  use crate::cblog;
-  use crate::chainblocksc::CBContext;
-  use crate::chainblocksc::CBTypeInfo;
-  use crate::chainblocksc::CBType_Int;
-  use crate::chainblocksc::CBTypesInfo;
-  use crate::chainblocksc::CBVar;
-  use crate::chainblocksc::CBlock;
-  use crate::core::cloneVar;
-  use crate::core::createBlock;
-  use crate::core::init;
-  use crate::core::log;
-  use crate::core::registerBlock;
-  use crate::core::sleep;
-  use crate::core::suspend;
-  use crate::core::Core;
-  use crate::types::common_type;
-  use crate::types::ClonedVar;
-  use crate::types::Var;
-  use std::ffi::CStr;
-  use std::ffi::CString;
-  use std::convert::TryInto;
+    use super::block::create;
+    use super::Types;
+    use crate::block::cblock_construct;
+    use crate::block::Block;
+    use crate::block::BlockWrapper;
+    use crate::cblog;
+    use crate::chainblocksc::CBContext;
+    use crate::chainblocksc::CBTypeInfo;
+    use crate::chainblocksc::CBType_Int;
+    use crate::chainblocksc::CBTypesInfo;
+    use crate::chainblocksc::CBVar;
+    use crate::chainblocksc::CBlock;
+    use crate::core::cloneVar;
+    use crate::core::createBlock;
+    use crate::core::init;
+    use crate::core::log;
+    use crate::core::registerBlock;
+    use crate::core::sleep;
+    use crate::core::suspend;
+    use crate::core::Core;
+    use crate::types::common_type;
+    use crate::types::ClonedVar;
+    use crate::types::Var;
+    use std::convert::TryInto;
+    use std::ffi::CStr;
+    use std::ffi::CString;
 
-  struct DummyBlock {
-    inputTypes: Types,
-    outputTypes: Types,
-  }
-
-  impl Default for DummyBlock {
-    fn default() -> Self {
-      DummyBlock {
-        inputTypes: Types::from(vec![common_type::none]),
-        outputTypes: Types::from(vec![common_type::any]),
-      }
-    }
-  }
-
-  type WDummyBlock = BlockWrapper<DummyBlock>;
-
-  impl Block for DummyBlock {
-    fn name(&mut self) -> &str {
-      "Dummy"
+    struct DummyBlock {
+        inputTypes: Types,
+        outputTypes: Types,
     }
 
-    fn inputTypes(&mut self) -> &Types {
-      &self.inputTypes
+    impl Default for DummyBlock {
+        fn default() -> Self {
+            DummyBlock {
+                inputTypes: Types::from(vec![common_type::none]),
+                outputTypes: Types::from(vec![common_type::any]),
+            }
+        }
     }
 
-    fn outputTypes(&mut self) -> &Types {
-      &self.outputTypes
-    }
-    fn activate(&mut self, context: &CBContext, _input: &Var) -> Result<Var, &str> {
-      log("Dummy - activate: Ok!");
-      let mut x: String = "Before...".to_string();
-      log(&x);
-      suspend(context, 2.0);
-      x.push_str(" - and After!");
-      log(&x);
-      log("Dummy - activate: Resumed!");
-      Ok(Var::default())
+    type WDummyBlock = BlockWrapper<DummyBlock>;
+
+    impl Block for DummyBlock {
+        fn name(&mut self) -> &str {
+            "Dummy"
+        }
+
+        fn inputTypes(&mut self) -> &Types {
+            &self.inputTypes
+        }
+
+        fn outputTypes(&mut self) -> &Types {
+            &self.outputTypes
+        }
+        fn activate(&mut self, context: &CBContext, _input: &Var) -> Result<Var, &str> {
+            log("Dummy - activate: Ok!");
+            let mut x: String = "Before...".to_string();
+            log(&x);
+            suspend(context, 2.0);
+            x.push_str(" - and After!");
+            log(&x);
+            log("Dummy - activate: Resumed!");
+            Ok(Var::default())
+        }
+
+        fn registerName() -> &'static str {
+            "Dummy\0"
+        }
     }
 
-    fn registerName() -> &'static str {
-      "Dummy\0"
+    #[cfg(test)]
+    #[ctor]
+    fn registerDummy() {
+        init();
+        registerBlock::<DummyBlock>();
     }
-  }
 
-  #[ctor]
-  fn registerDummy() {
-    init();
-    registerBlock::<DummyBlock>();
-  }
-
-  fn macroTest() {
-    blocks!((10)
+    fn macroTest() {
+        blocks!((10)
                 (Log)
                 (Set :Name ..x)
                 (Repeat
@@ -237,45 +238,45 @@ mod dummy_block {
                   (Msg "repeating...")
                   (Log)))
                 (Msg :Message "Done"));
-  }
-
-  #[test]
-  fn instanciate() {
-    init();
-
-    let mut blk = create::<DummyBlock>();
-    assert_eq!("Dummy".to_string(), blk.block.name());
-
-    let blkname = CString::new("Dummy").expect("CString failed...");
-    unsafe {
-      let cblk = (*Core).createBlock.unwrap()(blkname.as_ptr());
-      (*cblk).setup.unwrap()(cblk);
-      (*cblk).destroy.unwrap()(cblk);
     }
 
-    let svar1: Var = "Hello\0".into();
-    let svar2: Var = "Hello\0".into();
-    let svar3: Var = 10.into();
-    let sstr1: &str = svar1.as_ref().try_into().unwrap();
-    assert_eq!("Hello", sstr1);
-    assert!(svar1 == svar2);
-    assert!(svar1 != svar3);
+    #[test]
+    fn instanciate() {
+        init();
 
-    let a = Var::from(10);
-    let mut b = Var::from(true);
-    cloneVar(&mut b, &a);
-    unsafe {
-      assert_eq!(a.valueType, CBType_Int);
-      assert_eq!(b.valueType, CBType_Int);
-      assert_eq!(a.payload.__bindgen_anon_1.intValue, 10);
-      assert_eq!(
-        a.payload.__bindgen_anon_1.intValue,
-        b.payload.__bindgen_anon_1.intValue
-      );
+        let mut blk = create::<DummyBlock>();
+        assert_eq!("Dummy".to_string(), blk.block.name());
+
+        let blkname = CString::new("Dummy").expect("CString failed...");
+        unsafe {
+            let cblk = (*Core).createBlock.unwrap()(blkname.as_ptr());
+            (*cblk).setup.unwrap()(cblk);
+            (*cblk).destroy.unwrap()(cblk);
+        }
+
+        let svar1: Var = "Hello\0".into();
+        let svar2: Var = "Hello\0".into();
+        let svar3: Var = 10.into();
+        let sstr1: &str = svar1.as_ref().try_into().unwrap();
+        assert_eq!("Hello", sstr1);
+        assert!(svar1 == svar2);
+        assert!(svar1 != svar3);
+
+        let a = Var::from(10);
+        let mut b = Var::from(true);
+        cloneVar(&mut b, &a);
+        unsafe {
+            assert_eq!(a.valueType, CBType_Int);
+            assert_eq!(b.valueType, CBType_Int);
+            assert_eq!(a.payload.__bindgen_anon_1.intValue, 10);
+            assert_eq!(
+                a.payload.__bindgen_anon_1.intValue,
+                b.payload.__bindgen_anon_1.intValue
+            );
+        }
+
+        let _v: ClonedVar = a.into();
+
+        cblog!("Hello chainblocks-rs");
     }
-
-    let _v: ClonedVar = a.into();
-
-    cblog!("Hello chainblocks-rs");
-  }
 }
