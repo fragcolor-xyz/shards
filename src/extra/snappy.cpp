7bfaa48b9c099ce9cfb5bb3661f7e7f099283b48
Giovanni Petrantoni <sinkingsugar@gmail.com>
2020-11-10 22:00:41 +0900

    deprecate snappy, fixes wasm safari...

--- src/extra/snappy.cpp
+++ src/extra/snappy.cpp
@@ -1,6 +1,9 @@
 /* SPDX-License-Identifier: BSD 3-Clause "New" or "Revised" License */
 /* Copyright © 2019-2020 Giovanni Petrantoni */
 
+// Deprecating this in favor of brotli
+// will be removed in the future!
+
 #include "blocks/shared.hpp"
 #include "runtime.hpp"
 #include <snappy.h>
