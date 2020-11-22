187f584d82799c93b9c78299838599d20a1d4466
Giovanni Petrantoni <sinkingsugar@gmail.com>
2020-11-22 17:25:26 +0900

    wip

--- build-win32.sh
+++ build-win32.sh
@@ -4,7 +4,6 @@
 set -e
 
 pacman -S --needed --noconfirm base-devel mingw-w64-i686-toolchain mingw-w64-i686-cmake mingw-w64-i686-boost mingw-w64-i686-ninja mingw-w64-i686-clang mingw-w64-i686-lld wget mingw-w64-i686-python
-rustup target add i686-pc-windows-gnu
 
 # setup libbacktrace
 cd deps/libbacktrace
