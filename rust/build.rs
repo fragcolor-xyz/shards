e740fc6e0834b10d7a365a751f8d0a879a2af281
Giovanni Petrantoni <sinkingsugar@gmail.com>
2020-11-26 21:25:11 +0900

    ensure bindgen does 32bit job under 32 bit platform

--- rust/build.rs
+++ rust/build.rs
@@ -3,37 +3,44 @@ extern crate bindgen;
 
 #[cfg(feature = "run_bindgen")]
 fn bindgen_it() {
-    use std::env::var;
+  use std::env::var;
 
-    let chainblocks_dir = var("CHAINBLOCKS_DIR").unwrap_or("../".to_string());
+  let chainblocks_dir = var("CHAINBLOCKS_DIR").unwrap_or("../".to_string());
 
-    println!("cargo:rustc-link-search={}/build", chainblocks_dir);
-    // Tell cargo to invalidate the built crate whenever the wrapper changes
-    println!(
-        "cargo:rerun-if-changed={}/include/chainblocks.h",
-        chainblocks_dir
-    );
+  println!("cargo:rustc-link-search={}/build", chainblocks_dir);
+  // Tell cargo to invalidate the built crate whenever the wrapper changes
+  println!(
+    "cargo:rerun-if-changed={}/include/chainblocks.h",
+    chainblocks_dir
+  );
 
-    let header_path = chainblocks_dir + "/include/chainblocks.h";
+  let header_path = chainblocks_dir + "/include/chainblocks.h";
 
-    let bindings = bindgen::Builder::default()
-        .header(header_path)
-        .clang_arg("-DCB_NO_ANON")
-        .clang_arg("-DCB_USE_ENUMS")
-        .derive_default(true)
-        .generate()
-        .expect("Unable to generate bindings");
+  let mut builder = bindgen::Builder::default()
+    .header(header_path)
+    .clang_arg("-DCB_NO_ANON")
+    .clang_arg("-DCB_USE_ENUMS");
 
-    bindings
-        .write_to_file("src/chainblocksc.rs")
-        .expect("Couldn't write bindings!");
+  if cfg!(target_arch = "x86") {
+    builder = builder.clang_arg("-m32");
+  }
 
-    println!("Done processing chainblocks.h");
+  let bindings = builder
+    .derive_default(true)
+    .use_core()
+    .generate()
+    .expect("Unable to generate bindings");
+
+  bindings
+    .write_to_file("src/chainblocksc.rs")
+    .expect("Couldn't write bindings!");
+
+  println!("Done processing chainblocks.h");
 }
 
 #[cfg(not(feature = "run_bindgen"))]
 fn bindgen_it() {}
 
 fn main() {
-    bindgen_it();
+  bindgen_it();
 }
