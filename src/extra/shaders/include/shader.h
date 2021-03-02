7668e3cbf56a108fffc7d63c1082ddf31ac051fa
Giovanni Petrantoni <7008900+sinkingsugar@users.noreply.github.com>
2021-03-02 16:13:30 +0900

    add header guard to shader.h

--- src/extra/shaders/include/shader.h
+++ src/extra/shaders/include/shader.h
@@ -1,8 +1,13 @@
 /* SPDX-License-Identifier: BSD 3-Clause "New" or "Revised" License */
 /* Copyright © 2021 Giovanni Petrantoni */
 
+#ifndef CB_SHADER_H
+#define CB_SHADER_H
+
 #include "bgfx_shader.h"
 #include "shaderlib.h"
 
 uniform vec4 u_private_time4; // we might change this in the future, use yzw for other stuff
 #define u_time u_private_time4.x
+
+#endif
