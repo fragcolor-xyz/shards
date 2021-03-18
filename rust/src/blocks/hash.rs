cb98d92681dce7e3c1ae2230891a760ed0062d54
Giovanni Petrantoni <7008900+sinkingsugar@users.noreply.github.com>
2021-03-18 11:19:05 +0900

    add Sha3 hashing

--- rust/src/blocks/hash.rs
+++ rust/src/blocks/hash.rs
@@ -17,7 +17,7 @@ use core::time::Duration;
 use std::convert::TryFrom;
 use std::convert::TryInto;
 use std::ffi::CStr;
-use tiny_keccak::{Hasher, Keccak};
+use tiny_keccak::{Hasher, Keccak, Sha3};
 
 lazy_static! {
   static ref INPUT_TYPES: Vec<Type> = vec![
@@ -29,69 +29,72 @@ lazy_static! {
   static ref OUTPUT_TYPE: Vec<Type> = vec![common_type::bytes];
 }
 
-struct Keccak256 {
-  output: Vec<u8>,
-}
-
-impl Default for Keccak256 {
-  fn default() -> Self {
-    Keccak256 { output: Vec::new() }
-  }
-}
-
-impl Block for Keccak256 {
-  fn registerName() -> &'static str {
-    cstr!("Hash.Keccak256")
-  }
-  fn hash() -> u32 {
-    compile_time_crc32::crc32!("Hash.Keccak256-rust-0x20200101")
-  }
-  fn name(&mut self) -> &str {
-    "Hash.Keccak256"
-  }
-  fn inputTypes(&mut self) -> &std::vec::Vec<Type> {
-    &INPUT_TYPES
-  }
-  fn outputTypes(&mut self) -> &std::vec::Vec<Type> {
-    &OUTPUT_TYPE
-  }
-  fn activate(&mut self, _: &Context, input: &Var) -> Result<Var, &str> {
-    let mut k = Keccak::v256();
-    self.output.resize(32, 0);
-
-    if input.is_seq() {
-      let s: Seq = input.try_into().unwrap();
-      for val in s.iter() {
-        let bytes: Result<&[u8], &str> = val.as_ref().try_into();
-        if let Ok(bytes) = bytes {
-          k.update(bytes);
-        } else {
-          let string: Result<&str, &str> = val.as_ref().try_into();
-          if let Ok(string) = string {
-            let bytes = string.as_bytes();
-            k.update(bytes);
-          }
-        }
-      }
-    } else {
-      let bytes: Result<&[u8], &str> = input.as_ref().try_into();
-      if let Ok(bytes) = bytes {
-        k.update(bytes);
-      } else {
-        let string: Result<&str, &str> = input.as_ref().try_into();
-        if let Ok(string) = string {
-          let bytes = string.as_bytes();
-          k.update(bytes);
-        }
+macro_rules! add_hasher {
+  ($block_name:ident, $name_str:literal, $hash:literal, $algo:expr, $size:literal) => {
+    struct $block_name {
+      output: Vec<u8>,
+    }
+    impl Default for $block_name {
+      fn default() -> Self {
+        $block_name { output: Vec::new() }
       }
     }
-
-    k.finalize(&mut self.output);
-
-    Ok(self.output.as_slice().into())
-  }
+    impl Block for $block_name {
+      fn registerName() -> &'static str {
+        cstr!($name_str)
+      }
+      fn hash() -> u32 {
+        compile_time_crc32::crc32!($hash)
+      }
+      fn name(&mut self) -> &str {
+        $name_str
+      }
+      fn inputTypes(&mut self) -> &std::vec::Vec<Type> {
+        &INPUT_TYPES
+      }
+      fn outputTypes(&mut self) -> &std::vec::Vec<Type> {
+        &OUTPUT_TYPE
+      }
+      fn activate(&mut self, _: &Context, input: &Var) -> Result<Var, &str> {
+        let mut k = $algo();
+        self.output.resize($size, 0);
+        if input.is_seq() {
+          let s: Seq = input.try_into().unwrap();
+          for val in s.iter() {
+            let bytes: Result<&[u8], &str> = val.as_ref().try_into();
+            if let Ok(bytes) = bytes {
+              k.update(bytes);
+            } else {
+              let string: Result<&str, &str> = val.as_ref().try_into();
+              if let Ok(string) = string {
+                let bytes = string.as_bytes();
+                k.update(bytes);
+              }
+            }
+          }
+        } else {
+          let bytes: Result<&[u8], &str> = input.as_ref().try_into();
+          if let Ok(bytes) = bytes {
+            k.update(bytes);
+          } else {
+            let string: Result<&str, &str> = input.as_ref().try_into();
+            if let Ok(string) = string {
+              let bytes = string.as_bytes();
+              k.update(bytes);
+            }
+          }
+        }
+        k.finalize(&mut self.output);
+        Ok(self.output.as_slice().into())
+      }
+    }
+  };
 }
+
+add_hasher!(Keccak256, "Hash.Keccak256", "Hash.Keccak256-rust-0x20200101", Keccak::v256, 32);
+add_hasher!(CBSha3, "Hash.Sha3", "Hash.Sha3-rust-0x20200101", Sha3::v256, 32);
 
 pub fn registerBlocks() {
   registerBlock::<Keccak256>();
+  registerBlock::<CBSha3>();
 }
