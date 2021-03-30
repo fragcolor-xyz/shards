a9eb3d0fce04216f2ff0688777f8f6a345f25ca7
Giovanni Petrantoni <7008900+sinkingsugar@users.noreply.github.com>
2021-03-30 21:41:19 +0900

    edn more wip and some tests to bump CI wasm stuff

--- src/core/blocks/edn.cpp
+++ src/core/blocks/edn.cpp
@@ -53,6 +53,74 @@ struct Uglify {
   static CBTypesInfo inputTypes() { return CoreInfo::StringType; }
   static CBTypesInfo outputTypes() { return CoreInfo::StringType; }
 
+  OwnedVar to_var(token::Token &token) {
+    switch (token.value.index()) {
+    case token::value::BOOL:
+      return Var(std::get<bool>(token.value));
+    case token::value::CHAR:
+      return Var(std::get<char>(token.value));
+    case token::value::LONG:
+      return Var(std::get<int64_t>(token.value));
+    case token::value::DOUBLE:
+      return Var(std::get<double>(token.value));
+    case token::value::STRING: {
+      return Var(std::get<std::string>(token.value));
+    }
+    }
+    return Var::Empty;
+  }
+
+  template <class T> OwnedVar to_var(T &list) {
+    std::vector<OwnedVar> vars;
+    BOOST_FOREACH (auto &item, list) { vars.emplace_back(to_var(item)); }
+    return Var(vars);
+  }
+
+  OwnedVar to_var(form::FormWrapper formWrapper) {
+    return to_var(formWrapper.form);
+  }
+
+  CBMap to_var(form::FormWrapperMap &map) {
+    CBMap res;
+    BOOST_FOREACH (auto &item, map) {
+      OwnedVar key = to_var(item.first.form);
+      if (key.valueType != CBType::String)
+        throw ActivationError("Map key must be a string");
+      OwnedVar value = to_var(item.second.form);
+      res[key.payload.stringValue] = value;
+    }
+    return res;
+  }
+
+  OwnedVar to_var(form::Form form) {
+    switch (form.index()) {
+    case form::SPECIAL: {
+      auto error = std::get<form::Special>(form);
+      LOG(ERROR) << "EDN parsing error: " << error.message;
+      throw ActivationError("EDN parsing error");
+    }
+    case form::TOKEN:
+      return to_var(std::get<token::Token>(form));
+    case form::LIST:
+      return to_var<std::list<form::FormWrapper>>(
+          std::get<std::list<form::FormWrapper>>(form));
+    case form::VECTOR:
+      return to_var<std::vector<form::FormWrapper>>(
+          std::get<std::vector<form::FormWrapper>>(form));
+    case form::MAP: {
+      CBMap m = to_var(std::get<form::FormWrapperMap>(form));
+      CBVar tmp{};
+      tmp.valueType = CBType::Table;
+      tmp.payload.tableValue.api = &Globals::TableInterface;
+      tmp.payload.tableValue.opaque = &m;
+      return tmp;
+    }
+    case form::SET:
+      return to_var<form::FormWrapperSet>(std::get<form::FormWrapperSet>(form));
+    }
+    return Var::Empty;
+  }
+
   bool find_symbols(std::string &s) { return true; }
 
   bool find_symbols(token::Token &token) {
