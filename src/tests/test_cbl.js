d86ea4dfb122153a269e9b1e8172d7747906f4f7
Giovanni <7008900+sinkingsugar@users.noreply.github.com>
2021-04-05 23:13:26 +0900

    fix emscripten build tests

--- src/tests/test_cbl.js
+++ src/tests/test_cbl.js
@@ -1,4 +1,4 @@
-const cbl = require('../../build/cbl.js');
+const cbl = require('../../build/' + process.argv[2]);
 cbl({
   arguments: ["-e", '(do (def Root (Node)) (schedule Root (Chain "x" (Pause 2.0) (Msg "Hello") 0 (Exit))) (run Root 0.1))']
 });
