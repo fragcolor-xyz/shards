1e15d21e4040103c3bd8b08e9d7abace294da2a6
Giovanni Petrantoni <7008900+sinkingsugar@users.noreply.github.com>
2021-04-10 15:55:37 +0900

    kissfft cmake build and dsp blocks starting point

---
+++ src/extra/dsp.cpp
@@ -0,0 +1,13 @@
+/* SPDX-License-Identifier: BSD 3-Clause "New" or "Revised" License */
+/* Copyright © 2021 Giovanni Petrantoni */
+
+#include "blocks/shared.hpp"
+#include "runtime.hpp"
+
+namespace chainblocks {
+namespace DSP {
+struct FFT {};
+
+void registerBlocks() {}
+} // namespace DSP
+} // namespace chainblocks
