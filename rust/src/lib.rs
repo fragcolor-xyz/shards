24335f5b1715d2c9742f68cb59f4e5c24b6c8268
Nicolas Musset <musset.nicolas@gmail.com>
2021-09-08 17:32:05 +0900

    Update copyright notices in rust library

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -1,3 +1,6 @@
+/* SPDX-License-Identifier: BSD-3-Clause */
+/* Copyright © 2020 Fragcolor Pte. Ltd. */
+
 #![allow(non_upper_case_globals)]
 #![allow(non_camel_case_types)]
 #![allow(non_snake_case)]
