e71620a0a7a2e219cd75a999eeb696527c6144dd
Guus Waals <_@guusw.nl>
2021-09-26 12:11:14 +0200

    set built-in thread variables for find_package(Threads) on Apple

--- cmake/Platform.cmake
+++ cmake/Platform.cmake
@@ -120,3 +120,12 @@ if(NOT SKIP_HEAVY_INLINE)
     endif()
   endif()
 endif()
+
+# This tells FindPackage(Threads) that threads are built in
+if(APPLE)
+    set(CMAKE_THREAD_LIBS_INIT "-lpthread")
+    set(CMAKE_HAVE_THREADS_LIBRARY 1)
+    set(CMAKE_USE_WIN32_THREADS_INIT 0)
+    set(CMAKE_USE_PTHREADS_INIT 1)
+    set(THREADS_PREFER_PTHREAD_FLAG ON)
+endif()
