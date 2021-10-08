859aa7099f1492640255fa0f2f4a690e131bd11c
Nicolas Musset <musset.nicolas@gmail.com>
2021-10-08 13:02:41 +0900

    Implement transform gizmo

--- src/extra/gizmo.cpp
+++ src/extra/gizmo.cpp
@@ -9,17 +9,17 @@ namespace chainblocks {
 namespace Gizmo {
 
 namespace Helper {
-  static const float identity[16] = {1.f, 0.f, 0.f, 0.f,
-  0.f, 1.f, 0.f, 0.f,
-  0.f, 0.f, 1.f, 0.f,
-  0.f, 0.f, 0.f, 1.f, };
+static const float identity[16] = {
+    1.f, 0.f, 0.f, 0.f, 0.f, 1.f, 0.f, 0.f,
+    0.f, 0.f, 1.f, 0.f, 0.f, 0.f, 0.f, 1.f,
+};
 
-  // FIXME: copied from linal.cpp, should be shared
-  constexpr linalg::aliases::float3 AxisX{1.0, 0.0, 0.0};
-  constexpr linalg::aliases::float3 AxisY{0.0, 1.0, 0.0};
-  constexpr linalg::aliases::float3 AxisZ{0.0, 0.0, 1.0};
-  const float PI = 3.141592653589793238463f;
-  const float PI_2 = PI*0.5f;
+// FIXME: copied from linal.cpp, should be shared
+constexpr linalg::aliases::float3 AxisX{1.0, 0.0, 0.0};
+constexpr linalg::aliases::float3 AxisY{0.0, 1.0, 0.0};
+constexpr linalg::aliases::float3 AxisZ{0.0, 0.0, 1.0};
+const float PI = 3.141592653589793238463f;
+const float PI_2 = PI * 0.5f;
 }; // namespace Helper
 
 struct CubeView {
@@ -177,7 +177,7 @@ struct Grid : public BGFX::BaseConsumer {
 
     float arrView[16];
     float arrProj[16];
-    Mat4::ToArrayUnsafe(_matView , arrView);
+    Mat4::ToArrayUnsafe(_matView, arrView);
     Mat4::ToArrayUnsafe(currentView.proj, arrProj);
 
     ImGuiIO &io = ::ImGui::GetIO();
@@ -206,9 +206,143 @@ private:
   Mat4 _matView{};
 };
 
+struct Transform : public BGFX::BaseConsumer {
+  static CBOptionalString help() {
+    return CBCCSTR("Displays a gizmo that can be used to move, rotate or scale "
+                   "an object.");
+  }
+
+  static CBTypesInfo inputTypes() { return CoreInfo::AnyType; }
+  static CBOptionalString inputhelp() {
+    return CBCCSTR("The input value is not used and will pass through.");
+  }
+
+  static CBTypesInfo outputTypes() { return CoreInfo::AnyType; }
+  static CBOptionalString outputhelp() {
+    return CBCCSTR("The output of this block will be its input.");
+  }
+
+  static CBParametersInfo parameters() { return _params; }
+
+  void setParam(int index, const CBVar &value) {
+    switch (index) {
+    case 0:
+      _matrix = value;
+      break;
+    case 1:
+      _mode = value;
+      break;
+    case 2:
+      _operation = value;
+      break;
+    case 3:
+      _snap = value;
+      break;
+
+    default:
+      throw CBException("Parameter out of range.");
+    }
+  }
+
+  CBVar getParam(int index) {
+    switch (index) {
+    case 0:
+      return _matrix;
+    case 1:
+      return _mode;
+    case 2:
+      return _operation;
+    case 3:
+      return _snap;
+
+    default:
+      throw CBException("Parameter out of range.");
+    }
+  }
+
+  void cleanup() {
+    _matrix.cleanup();
+    _mode.cleanup();
+    _operation.cleanup();
+    _snap.cleanup();
+
+    BGFX::BaseConsumer::cleanup();
+  }
+
+  void warmup(CBContext *context) {
+    BGFX::BaseConsumer::warmup(context);
+
+    _matrix.warmup(context);
+    _mode.warmup(context);
+    _operation.warmup(context);
+    _snap.warmup(context);
+  }
+
+  CBVar activate(CBContext *context, const CBVar &input) {
+    auto *ctx =
+        reinterpret_cast<BGFX::Context *>(_bgfxCtx->payload.objectValue);
+    const auto &currentView = ctx->currentView();
+
+    float arrView[16];
+    float arrProj[16];
+    Mat4::ToArrayUnsafe(currentView.view, arrView);
+    Mat4::ToArrayUnsafe(currentView.proj, arrProj);
+
+    auto &matrix = _matrix.get();
+    // HACKish, I use Mat4 because the API hides some complexity, but it feels
+    // unnecessary to do that many copies should manipulate the seq of float4
+    // directly
+    Mat4 mat{};
+    mat = matrix;
+    float arrMatrix[16];
+    Mat4::ToArrayUnsafe(mat, arrMatrix);
+
+    auto &snap = _snap.get();
+    auto useSnap = snap.valueType == CBType::Float3;
+    auto snapPtr = useSnap
+                       ? reinterpret_cast<float *>(&snap.payload.float3Value)
+                       : nullptr;
+
+    ImGuizmo::Manipulate(
+        arrView, arrProj,
+        Enums::OperationToGuizmo(_operation.get().payload.enumValue),
+        Enums::ModeToGuizmo(_mode.get().payload.enumValue), arrMatrix, nullptr,
+        snapPtr);
+
+    chainblocks::cloneVar(matrix, Mat4::FromArrayUnsafe(arrMatrix));
+
+    return input;
+  }
+
+private:
+  static inline Parameters _params = {
+      {"Matrix",
+       CBCCSTR("Object matrix modified by this transform gizmo."),
+       {Type::VariableOf(CoreInfo::Float4x4Type)}},
+      // TODO add checks: matrix is required and must be non-null
+      {"Mode",
+       CBCCSTR("Type of coordinates used. Either local to the object or global "
+               "(world)."),
+       {Enums::ModeType, Type::VariableOf(Enums::ModeType)}},
+      {"Operation",
+       CBCCSTR("Operation to apply (translation, rotation or scaling)."),
+       {Enums::OperationType, Type::VariableOf(Enums::OperationType)}},
+      {"Snap",
+       CBCCSTR("Snapping values on all 3 axes. World unit for translation. "
+               "Angle in degrees for rotation. Multiplier for scaling. Use "
+               "`None` or `(float3 0 0 0)` to disable."),
+       {CoreInfo::NoneType, CoreInfo::Float3Type, CoreInfo::Float3VarType}}};
+
+  ParamVar _matrix{};
+  ParamVar _mode{};
+  ParamVar _operation{};
+  ParamVar _snap{};
+};
+
 void registerGizmoBlocks() {
   REGISTER_CBLOCK("Gizmo.CubeView", CubeView);
   REGISTER_CBLOCK("Gizmo.Grid", Grid);
+  REGISTER_CBLOCK("Gizmo.Transform", Transform);
 }
 }; // namespace Gizmo
 }; // namespace chainblocks
--- src/extra/gizmo.hpp
+++ src/extra/gizmo.hpp
@@ -16,6 +16,41 @@ struct Enums {
   static inline EnumInfo<GridAxis> GridAxisEnumInfo{"GizmoGridAxis", CoreCC,
                                                     GridAxisCC};
   static inline Type GridAxisType = Type::Enum(CoreCC, GridAxisCC);
+
+  enum class Mode { Local, World };
+  static constexpr uint32_t ModeCC = 'gizM';
+  static inline EnumInfo<Mode> ModeEnumInfo{"GizmoMode", CoreCC, ModeCC};
+  static inline Type ModeType = Type::Enum(CoreCC, ModeCC);
+
+  enum class Operation { Translate, Rotate, Scale, Universal };
+  static constexpr uint32_t OperationCC = 'gizO';
+  static inline EnumInfo<Operation> OperationEnumInfo{"GizmoOperation", CoreCC,
+                                                      OperationCC};
+  static inline Type OperationType = Type::Enum(CoreCC, OperationCC);
+
+  static constexpr ImGuizmo::MODE ModeToGuizmo(CBEnum eval) {
+    switch (Mode(eval)) {
+    case Mode::Local:
+      return ImGuizmo::MODE::LOCAL;
+    case Mode::World:
+      return ImGuizmo::MODE::WORLD;
+    }
+    return ImGuizmo::MODE(0);
+  }
+
+  static constexpr ImGuizmo::OPERATION OperationToGuizmo(CBEnum eval) {
+    switch (Operation(eval)) {
+    case Operation::Translate:
+      return ImGuizmo::OPERATION::TRANSLATE;
+    case Operation::Rotate:
+      return ImGuizmo::OPERATION::ROTATE;
+    case Operation::Scale:
+      return ImGuizmo::OPERATION::SCALE;
+    case Operation::Universal:
+      return ImGuizmo::OPERATION::UNIVERSAL;
+    }
+    return ImGuizmo::OPERATION(0);
+  }
 };
 
 } // namespace Gizmo
