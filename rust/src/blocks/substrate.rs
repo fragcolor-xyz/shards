9c5ccd3e7c81d4c78d5b72d35eb5f82560e0c716
Giovanni Petrantoni <7008900+sinkingsugar@users.noreply.github.com>
2021-10-31 11:57:18 +0900

    add StorageKey and StorageMap

--- rust/src/blocks/substrate.rs
+++ rust/src/blocks/substrate.rs
@@ -17,19 +17,21 @@ use crate::types::Seq;
 use crate::types::Table;
 use crate::types::Type;
 use crate::types::BYTES_TYPES;
+use crate::types::STRINGS_TYPES;
 use crate::types::STRING_TYPES;
 use crate::CString;
 use crate::Types;
 use crate::Var;
 use core::time::Duration;
-use sp_core::crypto::AccountId32;
-use sp_core::crypto::Pair;
-use sp_core::crypto::Ss58Codec;
-use sp_core::ed25519;
-use sp_core::sr25519;
-use std::convert::TryFrom;
-use std::convert::TryInto;
+use sp_core::crypto::{AccountId32, Pair, Ss58Codec};
+use sp_core::storage::StorageKey;
+use sp_core::{blake2_128, ed25519, sr25519, twox_128};
+use std::convert::{TryFrom, TryInto};
 use std::ffi::CStr;
+// use substrate_api_client::extrinsic::codec::Compact;
+// use substrate_api_client::extrinsic::xt_primitives::{
+//   GenericAddress, GenericExtra, UncheckedExtrinsicV4,
+// };
 
 lazy_static! {
   static ref ID_PARAMETERS: Parameters = vec![(
@@ -130,6 +132,123 @@ impl Block for AccountId {
   }
 }
 
+#[derive(Default)]
+struct CBStorageKey {
+  output: ClonedVar,
+  v: Vec<u8>,
+}
+
+impl Block for CBStorageKey {
+  fn registerName() -> &'static str {
+    cstr!("Substrate.StorageKey")
+  }
+
+  fn hash() -> u32 {
+    compile_time_crc32::crc32!("Substrate.StorageKey-rust-0x20200101")
+  }
+
+  fn name(&mut self) -> &str {
+    "Substrate.StorageKey"
+  }
+
+  fn inputTypes(&mut self) -> &Vec<Type> {
+    &STRINGS_TYPES
+  }
+
+  fn inputHelp(&mut self) -> OptionalString {
+    OptionalString(cbccstr!(
+      "A sequence of two strings representing the key we want to encode."
+    ))
+  }
+
+  fn outputTypes(&mut self) -> &Vec<Type> {
+    &BYTES_TYPES
+  }
+
+  fn outputHelp(&mut self) -> OptionalString {
+    OptionalString(cbccstr!("The encoded storage key."))
+  }
+
+  fn activate(&mut self, _: &Context, input: &Var) -> Result<Var, &str> {
+    let strings: Seq = input.try_into()?;
+    if strings.len() != 2 {
+      return Err("Invalid input length");
+    }
+    self.v.clear();
+    for string in strings {
+      let string: &str = string.as_ref().try_into()?;
+      let hash = twox_128(string.as_bytes());
+      self.v.extend(&hash[..]);
+    }
+    self.output = self.v.as_slice().into();
+    Ok(self.output.0)
+  }
+}
+
+#[derive(Default)]
+struct CBStorageMap {
+  output: ClonedVar,
+  v: Vec<u8>,
+}
+
+impl Block for CBStorageMap {
+  fn registerName() -> &'static str {
+    cstr!("Substrate.StorageMap")
+  }
+
+  fn hash() -> u32 {
+    compile_time_crc32::crc32!("Substrate.StorageMap-rust-0x20200101")
+  }
+
+  fn name(&mut self) -> &str {
+    "Substrate.StorageMap"
+  }
+
+  fn inputTypes(&mut self) -> &Vec<Type> {
+    &STRINGS_TYPES
+  }
+
+  fn inputHelp(&mut self) -> OptionalString {
+    OptionalString(cbccstr!(
+      "A sequence of three strings representing the map we want to encode, the last one being SCALE encoded data of the key."
+    ))
+  }
+
+  fn outputTypes(&mut self) -> &Vec<Type> {
+    &BYTES_TYPES
+  }
+
+  fn outputHelp(&mut self) -> OptionalString {
+    OptionalString(cbccstr!("The encoded storage map."))
+  }
+
+  fn activate(&mut self, _: &Context, input: &Var) -> Result<Var, &str> {
+    let strings: Seq = input.try_into()?;
+    if strings.len() != 3 {
+      return Err("Invalid input length");
+    }
+    self.v.clear();
+    for i in 0..2 {
+      let string: &str = strings[i].as_ref().try_into()?;
+      let hash = twox_128(string.as_bytes());
+      self.v.extend(&hash[..]);
+    }
+    let string: &str = strings[2].as_ref().try_into()?;
+    let bytes =
+      hex::decode(string.trim_start_matches("0x").trim_start_matches("0X")).map_err(|e| {
+        cblog!("{}", e);
+        "Failed to parse input hex string"
+      })?;
+    let hash = blake2_128(&bytes);
+    self.v.extend(&hash[..]);
+    self.v.extend(bytes);
+    self.output = self.v.as_slice().into();
+    Ok(self.output.0)
+  }
+}
+
 pub fn registerBlocks() {
   registerBlock::<AccountId>();
+  registerBlock::<CBStorageKey>();
+  registerBlock::<CBStorageMap>();
 }
