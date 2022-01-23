c2dc5868b64cd62a8970516cec30907a18c8b95c
Guus Waals <_@guusw.nl>
2022-01-23 11:38:54 +0100

    Remove BGFX tests, add new window + draw test

--- src/tests/test_extra.cpp
+++ src/tests/test_extra.cpp
@@ -14,61 +14,16 @@
 #include <catch2/catch_all.hpp>
 
 int main(int argc, char *argv[]) {
-  chainblocks::GetGlobals().RootPath = "./";
-  registerCoreBlocks();
-  int result = Catch::Session().run(argc, argv);
+	chainblocks::GetGlobals().RootPath = "./";
+	registerCoreBlocks();
+	int result = Catch::Session().run(argc, argv);
 #ifdef __EMSCRIPTEN_PTHREADS__
-  // in this case we need to call exit our self
-  exit(0);
+	// in this case we need to call exit our self
+	exit(0);
 #endif
-  return result;
+	return result;
 }
 
-namespace chainblocks {
-namespace BGFX_Tests {
-  extern void testVertexAttribute();
-  extern void testModel();
-  extern void testCamera();
-  extern void testDraw();
-  extern void testUniforms();
-  extern void testShaderCompiler();
-  extern void testScreenshot();
-}
-namespace GLTF_Tests {
-  extern void testLoad();
-}
-}
+namespace chainblocks {}
 
 using namespace chainblocks;
-
-TEST_CASE("GFX-VertexAttribute", "[gfx]") {
-  BGFX_Tests::testVertexAttribute();
-}
-
-TEST_CASE("GFX-Model", "[gfx]") {
-  BGFX_Tests::testModel();
-}
-
-TEST_CASE("GFX-Camera", "[gfx]") {
-  BGFX_Tests::testCamera();
-}
-
-TEST_CASE("GFX-Draw", "[gfx]") {
-  BGFX_Tests::testDraw();
-}
-
-TEST_CASE("GFX-Uniforms", "[gfx]") {
-  BGFX_Tests::testUniforms();
-}
-
-TEST_CASE("GFX-ShaderCompiler", "[gfx]") { //[!mayfail]
-  BGFX_Tests::testShaderCompiler();
-}
-
-TEST_CASE("GFX-Screenshot", "[gfx]") { //[!mayfail]
-  BGFX_Tests::testScreenshot();
-}
-
-TEST_CASE("GLTF-Load", "[gltf]") {
-  GLTF_Tests::testLoad();
-}
