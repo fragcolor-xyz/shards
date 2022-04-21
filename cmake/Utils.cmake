4b1433d9256a5bf65da7c8a218e1b554b4aa3504
Guus Waals <_@guusw.nl>
2022-04-21 15:46:32 +0200

    Remove some read-only properties from duplicated targets (CMake)

--- cmake/Utils.cmake
+++ cmake/Utils.cmake
@@ -1,12 +1,12 @@
 # Get all propreties that cmake supports
 if(NOT CMAKE_PROPERTIES_TO_DUPLICATE)
   execute_process(COMMAND cmake --help-property-list OUTPUT_VARIABLE CMAKE_PROPERTIES_TO_DUPLICATE)
-  
+
   # Convert command output into a CMake list
   string(REGEX REPLACE ";" "\\\\;" CMAKE_PROPERTIES_TO_DUPLICATE "${CMAKE_PROPERTIES_TO_DUPLICATE}")
   string(REGEX REPLACE "\n" ";" CMAKE_PROPERTIES_TO_DUPLICATE "${CMAKE_PROPERTIES_TO_DUPLICATE}")
-  
-  set(IGNORED_PROPERTIES "IMPORTED_GLOBAL" "NAME" "TYPE" "SOURCES")
+
+  set(IGNORED_PROPERTIES "IMPORTED_GLOBAL" "NAME" "TYPE" "SOURCES" "HEADER_SETS" "INTERFACE_HEADER_SETS")
   foreach(IGNORED_PROPERTY ${IGNORED_PROPERTIES})
     list(REMOVE_ITEM CMAKE_PROPERTIES_TO_DUPLICATE ${IGNORED_PROPERTY})
   endforeach()
@@ -14,7 +14,7 @@ endif()
 
 function(duplicate_library_target TARGET TYPE NEW_TARGET)
   add_library(${NEW_TARGET} ${TYPE})
-  
+
   foreach(PROPERTY ${CMAKE_PROPERTIES_TO_DUPLICATE})
     string(REPLACE "<CONFIG>" "${CMAKE_BUILD_TYPE}" PROPERTY ${PROPERTY})
 
@@ -29,7 +29,7 @@ function(duplicate_library_target TARGET TYPE NEW_TARGET)
       set_target_properties(${NEW_TARGET} PROPERTIES ${PROPERTY} "${VALUE}")
     endif()
   endforeach()
-    
+
   get_target_property(TARGET_SOURCE_DIR ${TARGET} SOURCE_DIR)
   get_target_property(TARGET_SOURCES ${TARGET} SOURCES)
   foreach(SOURCE ${TARGET_SOURCES})
