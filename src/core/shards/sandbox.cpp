b25b8e63ae8776e0eb60d422329bb5fb6a8af26b
inabe-a <inabe@vivaldi.net>
2022-06-05 17:33:33 +0900

    More experiments

--- src/core/shards/sandbox.cpp
+++ src/core/shards/sandbox.cpp
@@ -14,13 +14,36 @@ struct Sand {
   static SHTypesInfo inputTypes() { return CoreInfo::NoneType; }
   static SHTypesInfo outputTypes() { return CoreInfo::AnyType; }
 
-  static inline Parameters _params{};
+  static inline Parameters _params{{"grains", SHCCSTR("of sand."), {CoreInfo::IntType}}};
   static SHParametersInfo parameters() { return _params; }
+
+  SHVar getParam(int index) {
+    if (index == 0)
+      return Var(int64_t(_grains));
+
+    return Var();
+  }
+
+  void setParam(int index, const SHVar &value) {
+    if (index == 0)
+      _grains = uint32_t(value.payload.intValue);
+  }
+
+  void warmup(SHContext *context) {
+    SHLOG_TRACE("Getting the sand wet");
+  }
+
+  void cleanup() {
+    _grains = 0;
+  }
 
   SHVar activate(SHContext *context, const SHVar &input) {
     SHLOG_TRACE("Sand activate()-d");
     return Var();
   }
+
+private:
+  uint32_t _grains{1};
 };
 
 void registerShards() {
