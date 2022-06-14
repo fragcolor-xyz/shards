5f995399e641f4d46eb73161d4bbc178fd066001
Nicolas Musset <musset.nicolas@gmail.com>
2022-06-14 11:21:03 +0200

    Add documentation for `IsNotNone` shard

--- src/core/shards/core.hpp
+++ src/core/shards/core.hpp
@@ -482,8 +482,16 @@ struct IsNone {
 };
 
 struct IsNotNone {
+  static SHOptionalString help() { return SHCCSTR("Gets whether the type of the input is different from `None`."); }
+
   static SHTypesInfo inputTypes() { return CoreInfo::AnyType; }
+  static SHOptionalString inputHelp() { return SHCCSTR("The value which type to check against."); }
+
   static SHTypesInfo outputTypes() { return CoreInfo::BoolType; }
+  static SHOptionalString outputHelp() {
+    return SHCCSTR("`true` is the type of input different from `None`; otherwise, `false`.");
+  }
+
   SHVar activate(SHContext *context, const SHVar &input) { return shards::Var(input.valueType != None); }
 };
 
