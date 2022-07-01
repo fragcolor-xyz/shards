6a05c148432d6990a6fb0999602f02b6758c9618
Guus Waals <_@guusw.nl>
2022-07-01 14:02:52 +0200

    Explicitly add bindgen system c++ include path

--- cmake/osx_rust_build.sh
+++ cmake/osx_rust_build.sh
@@ -3,5 +3,5 @@ set -ex
 TARGET_SDKROOT=`xcrun --sdk $1 --show-sdk-path`
 shift
 export SDKROOT=`xcrun --sdk macosx --show-sdk-path`
-export BINDGEN_EXTRA_CLANG_ARGS="-isysroot $TARGET_SDKROOT $BINDGEN_EXTRA_CLANG_ARGS"
+export BINDGEN_EXTRA_CLANG_ARGS="-isysroot $TARGET_SDKROOT -isystem $TARGET_SDKROOT/usr/include/c++/v1 $BINDGEN_EXTRA_CLANG_ARGS"
 $@
