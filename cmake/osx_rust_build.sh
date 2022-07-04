53d7b044ec13815269b5b4cb41d9d18c38b00c93
Guus Waals <_@guusw.nl>
2022-07-04 12:41:42 +0200

    Add some comments about the workings of osx_rust_build.sh

--- cmake/osx_rust_build.sh
+++ cmake/osx_rust_build.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+# Script to work around some issues when calling `cargo build`/`cargo rustc` from Xcode for iOS
+#
+# The SDKROOT needs to be set to the HOST SDKROOT so that build scripts are able to build
+#  if not, it results in linker errors similar to this issue https://github.com/TimNN/cargo-lipo/issues/41
+#
+# Additionally, bindgen needs to be passed the sysroot for the target platform to be able to find the system headers
+
 set -ex
 TARGET_SDKROOT=`xcrun --sdk $1 --show-sdk-path`
 shift
