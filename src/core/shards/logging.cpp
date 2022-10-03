686b69323d73faff2ab45369be4f822209864b7f
Nicolas Musset <musset.nicolas@gmail.com>
2022-10-03 17:28:53 +0200

    CaptureLog: parameters

--- src/core/shards/logging.cpp
+++ src/core/shards/logging.cpp
@@ -5,9 +5,7 @@
 #include <numeric>
 #include <string>
 #include <log/log.hpp>
-#include <spdlog/sinks/base_sink.h>
 #include <spdlog/sinks/dist_sink.h>
-// #include <spdlog/sinks/ringbuffer_sink.h>
 
 namespace shards {
 struct LoggingBase {
@@ -108,53 +106,47 @@ struct Msg : public LoggingBase {
 /*
  * Custom ring buffer sink
  */
-template<typename Mutex>
-class custom_ringbuffer_sink final : public spdlog::sinks::base_sink<Mutex>
-{
+template <typename Mutex> class custom_ringbuffer_sink final : public spdlog::sinks::base_sink<Mutex> {
 public:
-    explicit custom_ringbuffer_sink(size_t n_items)
-        : q_{n_items}
-    {}
+  explicit custom_ringbuffer_sink(size_t n_items, spdlog::level::level_enum min_level = spdlog::level::debug)
+      : q_{n_items}, min_level_(min_level) {}
 
-    std::vector<spdlog::details::log_msg_buffer> last_raw(size_t lim = 0)
-    {
-        std::lock_guard<Mutex> lock(spdlog::sinks::base_sink<Mutex>::mutex_);
-        auto items_available = q_.size();
-        auto n_items = lim > 0 ? (std::min)(lim, items_available) : items_available;
-        std::vector<spdlog::details::log_msg_buffer> ret;
-        ret.reserve(n_items);
-        for (size_t i = (items_available - n_items); i < items_available; i++)
-        {
-            ret.push_back(q_.at(i));
-        }
-        return ret;
+  std::vector<spdlog::details::log_msg_buffer> last_raw(size_t lim = 0) {
+    std::lock_guard<Mutex> lock(spdlog::sinks::base_sink<Mutex>::mutex_);
+    auto items_available = q_.size();
+    auto n_items = lim > 0 ? (std::min)(lim, items_available) : items_available;
+    std::vector<spdlog::details::log_msg_buffer> ret;
+    ret.reserve(n_items);
+    for (size_t i = (items_available - n_items); i < items_available; i++) {
+      ret.push_back(q_.at(i));
     }
+    return ret;
+  }
 
-    std::vector<std::string> last_formatted(size_t lim = 0)
-    {
-        std::lock_guard<Mutex> lock(spdlog::sinks::base_sink<Mutex>::mutex_);
-        auto items_available = q_.size();
-        auto n_items = lim > 0 ? (std::min)(lim, items_available) : items_available;
-        std::vector<std::string> ret;
-        ret.reserve(n_items);
-        for (size_t i = (items_available - n_items); i < items_available; i++)
-        {
-            spdlog::memory_buf_t formatted;
-            spdlog::sinks::base_sink<Mutex>::formatter_->format(q_.at(i), formatted);
-            ret.push_back(fmt::to_string(formatted));
-        }
-        return ret;
+  std::vector<std::string> last_formatted(size_t lim = 0) {
+    std::lock_guard<Mutex> lock(spdlog::sinks::base_sink<Mutex>::mutex_);
+    auto items_available = q_.size();
+    auto n_items = lim > 0 ? (std::min)(lim, items_available) : items_available;
+    std::vector<std::string> ret;
+    ret.reserve(n_items);
+    for (size_t i = (items_available - n_items); i < items_available; i++) {
+      spdlog::memory_buf_t formatted;
+      spdlog::sinks::base_sink<Mutex>::formatter_->format(q_.at(i), formatted);
+      ret.push_back(fmt::to_string(formatted));
     }
+    return ret;
+  }
 
 protected:
-    void sink_it_(const spdlog::details::log_msg &msg) override
-    {
-        q_.push_back(spdlog::details::log_msg_buffer{msg});
-    }
-    void flush_() override {}
+  void sink_it_(const spdlog::details::log_msg &msg) override {
+    if (msg.level >= min_level_)
+      q_.push_back(spdlog::details::log_msg_buffer{msg});
+  }
+  void flush_() override {}
 
 private:
-    spdlog::details::circular_q<spdlog::details::log_msg_buffer> q_;
+  spdlog::details::circular_q<spdlog::details::log_msg_buffer> q_;
+  spdlog::level::level_enum min_level_;
 };
 
 using custom_ringbuffer_sink_mt = custom_ringbuffer_sink<std::mutex>;
@@ -169,6 +161,45 @@ struct CaptureLog {
     // return CoreInfo::StringSeqType;
   }
 
+  static SHParametersInfo parameters() { return _params; }
+
+  void setParam(int index, const SHVar &value) {
+    switch (index) {
+    case 0:
+      _n_items = size_t(value.payload.intValue);
+      break;
+    case 1:
+      _min_level = value.payload.stringValue;
+      break;
+    case 2:
+      _pattern = value.payload.stringValue;
+      break;
+    default:
+      break;
+    }
+  }
+
+  SHVar getParam(int index) {
+    auto res = SHVar();
+    switch (index) {
+    case 0:
+      res.valueType = Int;
+      res.payload.intValue = _n_items;
+      break;
+    case 1:
+      res.valueType = String;
+      res.payload.stringValue = _min_level.c_str();
+      break;
+    case 2:
+      res.valueType = String;
+      res.payload.stringValue = _pattern.c_str();
+      break;
+    default:
+      break;
+    }
+    return res;
+  }
+
   void warmup(SHContext *context) {
     auto logger = spdlog::default_logger();
     assert(logger);
@@ -176,15 +207,9 @@ struct CaptureLog {
     assert(sink);
     auto dist_sink = std::dynamic_pointer_cast<spdlog::sinks::dist_sink_mt>(sink);
     if (dist_sink) {
-      _ring = std::make_shared<custom_ringbuffer_sink_mt>(8);
-#ifdef __ANDROID
-      // Logcat already countains timestamps & log level
-      _ring->set_formatter(std::unique_ptr<spdlog::formatter>(
-          new spdlog::pattern_formatter(std::move("[T-%t] [%s::%#] %v"), spdlog::pattern_time_type::local)));
-#else
-      _ring->set_formatter(std::unique_ptr<spdlog::formatter>(new spdlog::pattern_formatter(
-          std::move("%^[%l]%$ [%Y-%m-%d %T.%e] %v"), spdlog::pattern_time_type::local)));
-#endif
+      _ring = std::make_shared<custom_ringbuffer_sink_mt>(_n_items, spdlog::level::from_str(_min_level));
+      _ring->set_formatter(
+          std::unique_ptr<spdlog::formatter>(new spdlog::pattern_formatter(_pattern, spdlog::pattern_time_type::local)));
       dist_sink->add_sink(_ring);
     }
   }
@@ -215,6 +240,15 @@ struct CaptureLog {
   }
 
 private:
+  static inline Parameters _params{
+      {"Size", SHCCSTR("The maximum number of logs to retain."), {CoreInfo::IntType}},
+      {"MinLevel", SHCCSTR("The minimum level of logs to capture."), {CoreInfo::StringType}},
+      {"Pattern", SHCCSTR("The pattern used to format the logs."), {CoreInfo::StringType}},
+  };
+
+  size_t _n_items{8};
+  std::string _min_level{"debug"};
+  std::string _pattern{"%^[%l]%$ [%Y-%m-%d %T.%e] [T-%t] [%s::%#] %v"};
   std::shared_ptr<custom_ringbuffer_sink_mt> _ring;
   std::string _s{};
 };
