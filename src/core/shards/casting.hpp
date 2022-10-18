e447366758f5efb370ec6b1f49b17c403a64c366
Guus Waals <_@guusw.nl>
2022-10-18 23:41:01 +0200

    Add MakeInt/Float2/3/4 shards
    Used to recompose vectors after splitting them without having to use an intermediate sequence

--- src/core/shards/casting.hpp
+++ src/core/shards/casting.hpp
@@ -194,5 +194,106 @@ template <SHType ToType> struct ToNumber {
     return output;
   }
 };
+
+template <SHType ToType> struct MakeVector {
+  const VectorTypeTraits *_outputVectorType{nullptr};
+  const NumberTypeTraits *_outputNumberType{nullptr};
+  const NumberConversion *_componentConversion{nullptr};
+
+  std::vector<ParamVar> params;
+
+  MakeVector() {
+    auto vectorType = VectorTypeLookup::getInstance().get(ToType);
+    params.resize(vectorType->dimension);
+  }
+
+  SHTypesInfo inputTypes() { return CoreInfo::NoneType; }
+  SHTypesInfo outputTypes() {
+    if (!_outputVectorType)
+      return CoreInfo::AnyType;
+    return _outputVectorType->type;
+  }
+
+  void setParam(int index, const SHVar &value) {
+    if (index >= params.size())
+      return;
+    params[index] = value;
+  }
+
+  SHVar getParam(int index) {
+    if (index >= params.size())
+      return Var::Empty;
+    return params[index];
+  }
+
+  static inline std::vector<std::string> componentNames = []() {
+    std::vector<std::string> result;
+    for (size_t i = 0; i < 16; i++) {
+      result.push_back(fmt::format("{}", i));
+    }
+    return result;
+  }();
+
+  static SHParametersInfo parameters() {
+    static Parameters params = []() {
+      Parameters params;
+      auto vectorType = VectorTypeLookup::getInstance().get(ToType);
+      auto paramType = getCompatibleUnitType(vectorType->numberType);
+
+      for (size_t i = 0; i < vectorType->dimension; i++) {
+        params._infos.emplace_back(componentNames[i].c_str(), Types({Type::VariableOf(paramType), paramType}));
+      }
+
+      for (auto &param : params._infos)
+        params._pinfos.emplace_back(param);
+
+      return params;
+    }();
+
+    return params;
+  }
+
+  SHTypeInfo compose(const SHInstanceData &data) {
+    _outputVectorType = VectorTypeLookup::getInstance().get(ToType);
+    if (!_outputVectorType) {
+      throw ComposeError("Conversion not implemented for this type");
+    }
+
+    auto &numberTypeLookup = NumberTypeLookup::getInstance();
+    _outputNumberType = numberTypeLookup.get(_outputVectorType->numberType);
+
+    NumberType componentNumberType = _outputNumberType->isInteger ? NumberType::Int64 : NumberType::Float64;
+    _componentConversion = numberTypeLookup.getConversion(componentNumberType, _outputNumberType->type);
+
+    return _outputVectorType->type;
+  }
+
+  void warmup(SHContext *context) {
+    for (auto &param : params)
+      param.warmup(context);
+  }
+
+  void cleanup() {
+    for (auto &param : params)
+      param.cleanup();
+  }
+
+  SHVar activate(SHContext *context, const SHVar &input) {
+    shassert(_outputVectorType);
+
+    SHVar output{};
+    output.valueType = _outputVectorType->shType;
+
+    uint8_t *dstData = (uint8_t *)&output.payload.floatValue;
+    for (size_t i = 0; i < _outputVectorType->dimension; i++) {
+      auto &param = params[i].get();
+      _componentConversion->convertOne(&param.payload.floatValue, dstData);
+      dstData += _componentConversion->outStride;
+    }
+
+    return output;
+  }
+};
+
 } // namespace shards
 #endif // SH_CORE_BLOCKS_CASTING
