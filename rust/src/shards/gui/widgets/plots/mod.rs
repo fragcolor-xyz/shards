436e1a719f8692e7d805a59f25497b56c7a27d9c
Nicolas Musset <musset.nicolas@gmail.com>
2022-10-25 16:41:47 +0200

    UI: plot line

--- rust/src/shards/gui/widgets/plots/mod.rs
+++ rust/src/shards/gui/widgets/plots/mod.rs
@@ -4,12 +4,18 @@
 use crate::core::registerShard;
 use crate::fourCharacterCode;
 use crate::types::ExposedTypes;
+use crate::types::FLOAT2_TYPES;
 use crate::types::FRAG_CC;
 use crate::types::ParamVar;
 use crate::types::ShardsVar;
 use crate::types::Type;
 
 static EGUI_PLOT_UI_TYPE: Type = Type::object(FRAG_CC, fourCharacterCode(*b"egPC"));
+
+lazy_static!{  
+  pub static ref SEQ_OF_FLOAT2: Type = Type::seq(&FLOAT2_TYPES);
+  pub static ref SEQ_OF_FLOAT2_TYPES: Vec<Type> = vec![*SEQ_OF_FLOAT2];
+}
 
 const PLOT_UI_NAME: &'static str = "UI.PlotUI";
 
@@ -24,8 +30,17 @@ struct Plot {
   exposing: ExposedTypes,
 }
 
+struct PlotLine {
+  plot_ui: ParamVar,
+  requiring: ExposedTypes,
+  color: ParamVar,
+  name: ParamVar,
+}
+
 mod plot;
+mod plot_line;
 
 pub fn registerShards() {
   registerShard::<Plot>();
+  registerShard::<PlotLine>();
 }
