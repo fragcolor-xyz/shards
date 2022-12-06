25d1644fd7dfe7faf3d6ec7603ad8539cfae42f1
Guus Waals <_@guusw.nl>
2022-12-06 22:48:06 +0100

    Add fmtlib formatting to ops_internal.hpp

--- src/core/ops_internal.hpp
+++ src/core/ops_internal.hpp
@@ -5,10 +5,9 @@
 #define SH_CORE_OPS_INTERNAL
 
 #include <ops.hpp>
-
-#include <spdlog/spdlog.h>
-
-#include "spdlog/fmt/ostr.h" // must be included
+#include <sstream>
+#include <spdlog/fmt/fmt.h>
+#include <spdlog/fmt/ostr.h> // must be included
 
 namespace shards {
 struct DocsFriendlyFormatter {
@@ -25,5 +24,44 @@ static inline DocsFriendlyFormatter defaultFormatter{};
 inline std::ostream &operator<<(std::ostream &os, const SHVar &v) { return shards::defaultFormatter.format(os, v); }
 inline std::ostream &operator<<(std::ostream &os, const SHTypeInfo &v) { return shards::defaultFormatter.format(os, v); }
 inline std::ostream &operator<<(std::ostream &os, const SHTypesInfo &v) { return shards::defaultFormatter.format(os, v); }
+
+template <typename T> struct StringStreamFormatter {
+  constexpr auto parse(fmt::format_parse_context &ctx) -> decltype(ctx.begin()) {
+    auto it = ctx.begin(), end = ctx.end();
+    if (it != end)
+      throw fmt::format_error("invalid format");
+    return it;
+  }
+
+  template <typename FormatContext> auto format(const T &v, FormatContext &ctx) -> decltype(ctx.out()) {
+    std::stringstream ss;
+    ss << v;
+    return format_to(ctx.out(), "{}", ss.str());
+  }
+};
+
+template <> struct fmt::formatter<SHVar> {
+  StringStreamFormatter<SHVar> base;
+  constexpr auto parse(format_parse_context &ctx) -> decltype(ctx.begin()) { return base.parse(ctx); }
+  template <typename FormatContext> auto format(const SHVar &v, FormatContext &ctx) -> decltype(ctx.out()) {
+    return base.format(v, ctx);
+  }
+};
+
+template <> struct fmt::formatter<SHTypeInfo> {
+  StringStreamFormatter<SHTypeInfo> base;
+  constexpr auto parse(format_parse_context &ctx) -> decltype(ctx.begin()) { return base.parse(ctx); }
+  template <typename FormatContext> auto format(const SHTypeInfo &v, FormatContext &ctx) -> decltype(ctx.out()) {
+    return base.format(v, ctx);
+  }
+};
+
+template <> struct fmt::formatter<SHTypesInfo> {
+  StringStreamFormatter<SHTypesInfo> base;
+  constexpr auto parse(format_parse_context &ctx) -> decltype(ctx.begin()) { return base.parse(ctx); }
+  template <typename FormatContext> auto format(const SHTypesInfo &v, FormatContext &ctx) -> decltype(ctx.out()) {
+    return base.format(v, ctx);
+  }
+};
 
 #endif // SH_CORE_OPS_INTERNAL
