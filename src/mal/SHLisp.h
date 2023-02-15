8bbbe9adce5e6f7a8c39e9ee2f33cdd9054267b3
Giovanni Petrantoni <7008900+sinkingsugar@users.noreply.github.com>
2023-02-15 11:45:56 +0800

    various review fixes

--- src/mal/SHLisp.h
+++ src/mal/SHLisp.h
@@ -8,6 +8,7 @@ SHARDS_API __cdecl void *shLispCreate(const char *path);
 SHARDS_API __cdecl void *shLispCreateSub(void *parent);
 SHARDS_API __cdecl void shLispDestroy(void *env);
 SHARDS_API __cdecl SHBool shLispEval(void *env, const char *str, SHVar *output = nullptr);
+SHARDS_API __cdecl SHBool shLispAddToEnv(void *env, const char *str, SHVar input, bool reference);
 }
 
 #endif /* BAF4257E_4054_4E92_B538_547BD5F05C0D */
