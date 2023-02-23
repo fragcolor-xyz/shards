0b15fa5abd72297d2db6f2040397b8f61bd7a69e
Guus Waals <_@guusw.nl>
2023-02-23 21:34:33 +0800

    Fix tracy in gfx tests

--- src/gfx/tests/main.cpp
+++ src/gfx/tests/main.cpp
@@ -2,7 +2,16 @@
 #include <catch2/catch_all.hpp>
 #include <spdlog/spdlog.h>
 
+// Defined in the gfx rust crate
+//   used to initialize tracy on the rust side, since it required special intialization (C++ doesn't)
+//   but since we link to the dll, we can use it from C++ too
+extern "C" void gfxTracyInit();
+
 int main(int argc, char *argv[]) {
+#ifdef TRACY_ENABLE
+  gfxTracyInit();
+#endif
+
   Catch::Session session;
 
   spdlog::set_level(spdlog::level::debug);
