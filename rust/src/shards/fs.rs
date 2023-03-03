7968ef2abfd41917ccec1d6442f3e57676a0fad3
Nicolas Musset <musset.nicolas@gmail.com>
2023-03-03 10:34:10 +0100

    Add FS.SaveFileDialog

--- rust/src/shards/fs.rs
+++ rust/src/shards/fs.rs
@@ -55,6 +55,20 @@ lazy_static! {
     )
       .into(),
   ];
+  static ref SAVEFILEDIALOG_PARAMETERS: Parameters = vec![
+    (
+      cstr!("Filters"),
+      shccstr!("To filter files based on extensions."),
+      &STRINGS_VAR_OR_NONE_TYPES[..],
+    )
+      .into(),
+    (
+      cstr!("CurrentDir"),
+      shccstr!("Set the current directory"),
+      STRING_VAR_OR_NONE_SLICE,
+    )
+      .into(),
+  ];
 }
 
 struct FileDialog {
@@ -237,6 +251,125 @@ impl FileDialog {
   }
 }
 
+struct SaveFileDialog {
+  filters: ParamVar,
+  current_dir: ParamVar,
+  output: ClonedVar,
+}
+
+impl Default for SaveFileDialog {
+  fn default() -> Self {
+    Self {
+      filters: ParamVar::default(),
+      current_dir: ParamVar::default(),
+      output: ClonedVar::default(),
+    }
+  }
+}
+
+impl Shard for SaveFileDialog {
+  fn registerName() -> &'static str
+  where
+    Self: Sized,
+  {
+    cstr!("FS.SaveFileDialog")
+  }
+
+  fn hash() -> u32
+  where
+    Self: Sized,
+  {
+    compile_time_crc32::crc32!("FS.SaveFileDialog-rust-0x20200101")
+  }
+
+  fn name(&mut self) -> &str {
+    "FS.SaveFileDialog"
+  }
+
+  fn inputTypes(&mut self) -> &Types {
+    &NONE_TYPES
+  }
+
+  fn outputTypes(&mut self) -> &Types {
+    &STRING_TYPES
+  }
+
+  fn parameters(&mut self) -> Option<&Parameters> {
+    Some(&SAVEFILEDIALOG_PARAMETERS)
+  }
+
+  fn setParam(&mut self, index: i32, value: &Var) -> Result<(), &str> {
+    match index {
+      0 => Ok(self.filters.set_param(value)),
+      1 => Ok(self.current_dir.set_param(value)),
+      _ => Err("Invalid parameter index"),
+    }
+  }
+
+  fn getParam(&mut self, index: i32) -> Var {
+    match index {
+      0 => self.filters.get_param(),
+      1 => self.current_dir.get_param(),
+      _ => Var::default(),
+    }
+  }
+
+  fn warmup(&mut self, ctx: &Context) -> Result<(), &str> {
+    self.filters.warmup(ctx);
+    self.current_dir.warmup(ctx);
+
+    Ok(())
+  }
+
+  fn cleanup(&mut self) -> Result<(), &str> {
+    self.current_dir.cleanup();
+    self.filters.cleanup();
+
+    Ok(())
+  }
+
+  fn activate(&mut self, context: &Context, input: &Var) -> Result<Var, &str> {
+    Ok(run_blocking(self, context, input))
+  }
+}
+
+impl BlockingShard for SaveFileDialog {
+  #[cfg(any(target_os = "windows", target_os = "macos", target_os = "linux"))]
+  fn activate_blocking(&mut self, _context: &Context, _input: &Var) -> Result<Var, &str> {
+    let mut dialog = rfd::FileDialog::new();
+    let filters = self.filters.get();
+    if !filters.is_none() {
+      let filters: Seq = filters.try_into()?;
+      for filter in filters.iter() {
+        let filter: &str = filter.try_into()?;
+        dialog = dialog.add_filter(filter, &[filter]);
+      }
+    }
+    let current_dir = self.current_dir.get();
+    if !current_dir.is_none() {
+      let cd: &str = current_dir.try_into()?;
+      if let Ok(start_path) = std::path::PathBuf::from(cd).canonicalize() {
+        dialog = dialog.set_directory(start_path);
+      }
+    }
+    
+    if let Some(path) = dialog.save_file() {
+      let path = path.display().to_string();
+      let output = Var::ephemeral_string(path.as_str());
+      self.output = output.as_ref().into();
+      Ok(self.output.0)
+    } else {
+      Err("Operation was cancelled")
+    }
+  }
+
+  #[cfg(not(any(target_os = "windows", target_os = "macos", target_os = "linux")))]
+  fn activate_blocking(&mut self, _context: &Context, _input: &Var) -> Result<Var, &str> {
+    Err("Not supported")
+  }
+}
+
 pub fn registerShards() {
   registerShard::<FileDialog>();
+  registerShard::<SaveFileDialog>();
 }
