b59e393562c4d34c75a47a7b0f52d2e633c7af90
Nicolas Musset <musset.nicolas@gmail.com>
2023-03-06 13:59:52 +0100

    Fix some tests
    Also fix stackcount regex issues on Windows

--- src/mal/Reader.cpp
+++ src/mal/Reader.cpp
@@ -2,11 +2,15 @@
 /* Copyright © 2019 Fragcolor Pte. Ltd. */
 /* Copyright (C) 2015 Joel Martin <github@martintribe.org> */
 
+#if defined(__clang__) && defined(_WIN32)
+#define _REGEX_MAX_STACK_COUNT 200000
+#endif
+#include <regex>
+
 #include "MAL.h"
 #include "Types.h"
 
 #include <iostream>
-#include <regex>
 
 typedef std::regex Regex;
 
