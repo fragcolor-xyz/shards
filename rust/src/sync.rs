5d061243115f04ffd52253b268e6af41c07bd10c
Giovanni Petrantoni <7008900+sinkingsugar@users.noreply.github.com>
2023-04-21 14:26:25 +0800

    wip

--- rust/src/sync.rs
+++ rust/src/sync.rs
@@ -50,13 +50,21 @@ pub fn crdts_test() {
   assert_eq!(bobs_friends, Some(vec!["erik"]));
 }
 
+#[derive(Clone, Debug, PartialEq, Eq, PartialOrd, Hash, Ord)]
+struct Bytes32([u8; 32]);
+
 pub fn crdts_test2() {
-  let mut friend_map: Map<&str, MVReg<Var, u64>, u64> = Map::new();
+  let actor1 = Bytes32([1; 32]);
+  let actor2 = Bytes32([2; 32]);
+
+  let mut friend_map: Map<&str, MVReg<Var, Bytes32>, Bytes32> = Map::new();
 
   let svar1: Var = Var::ephemeral_string("janet\0");
 
   let read_ctx = friend_map.len(); // we read anything from the map to get a add context
-  friend_map.apply(friend_map.update("bob", read_ctx.derive_add_ctx(1), |r, c| r.write(svar1, c)));
+  friend_map.apply(friend_map.update("bob", read_ctx.derive_add_ctx(actor1), |r, c| r.write(svar1, c)));
+
+  assert_eq!(friend_map.get(&"bob").val.unwrap().read().val, vec![svar1]);
 
   let mut friend_map_on_2nd_device = friend_map.clone();
 
@@ -65,35 +73,75 @@ pub fn crdts_test2() {
   // the map on the 2nd devices adds 'erik' to `bob`'s friends
   friend_map_on_2nd_device.apply(friend_map_on_2nd_device.update(
     "bob",
-    friend_map_on_2nd_device.len().derive_add_ctx(2),
+    friend_map_on_2nd_device.len().derive_add_ctx(actor2),
     |r, c| r.write(svar2, c),
   ));
 
-  // Meanwhile, on the first device we remove
-  // the entire 'bob' entry from the friend map.
-  friend_map.apply(friend_map.rm("bob", friend_map.get(&"bob").derive_rm_ctx()));
+  assert_eq!(friend_map_on_2nd_device.get(&"bob").val.unwrap().read().val, vec![svar2]);
 
-  assert!(friend_map.get(&"bob").val.is_none());
+  // // Meanwhile, on the first device we remove
+  // // the entire 'bob' entry from the friend map.
+  // friend_map.apply(friend_map.rm("bob", friend_map.get(&"bob").derive_rm_ctx()));
+
+  // assert!(friend_map.get(&"bob").val.is_none());
 
   // once these two devices synchronize...
   let friend_map_snapshot = friend_map.clone();
   let friend_map_on_2nd_device_snapshot = friend_map_on_2nd_device.clone();
 
+  assert_eq!(friend_map_snapshot.get(&"bob").val.unwrap().read().val, vec![svar1]);
+  assert_eq!(friend_map_on_2nd_device_snapshot.get(&"bob").val.unwrap().read().val, vec![svar2]);
+
   friend_map.merge(friend_map_on_2nd_device_snapshot);
   friend_map_on_2nd_device.merge(friend_map_snapshot);
   assert_eq!(friend_map, friend_map_on_2nd_device);
 
-  // ... we see that "bob" is present but only
-  // contains `erik`.
-  //
-  // This is because the `erik` entry was not
-  // seen by the first device when it deleted
-  // the entry.
-  let bobs_friends = friend_map
-    .get(&"bob")
-    .val
-    .map(|set| set.read().val)
-    .map(|hashmap| hashmap.into_iter().collect::<Vec<_>>());
+  assert_eq!(friend_map.get(&"bob").val.unwrap().read().val, vec![svar2]);
+  assert_eq!(friend_map_on_2nd_device.get(&"bob").val.unwrap().read().val, vec![svar2]);
+}
 
-  assert_eq!(bobs_friends, Some(vec![svar2]));
+pub fn crdts_test3() {
+  let mut friend_map: Map<&str, MVReg<&str, u64>, u64> = Map::new();
+
+  let svar1 = "janet";
+
+  let read_ctx = friend_map.len(); // we read anything from the map to get a add context
+  friend_map.apply(friend_map.update("bob", read_ctx.derive_add_ctx(1), |r, c| r.write(svar1, c)));
+
+  assert_eq!(friend_map.get(&"bob").val.unwrap().read().val, vec![svar1]);
+
+  let mut friend_map_on_2nd_device = friend_map.clone();
+  // let mut friend_map_on_2nd_device: Map<&str, MVReg<&str, u64>, u64> = Map::new();
+
+  let svar2 = "erik";
+
+  // the map on the 2nd devices adds 'erik' to `bob`'s friends
+  friend_map_on_2nd_device.apply(friend_map_on_2nd_device.update(
+    "bob",
+    friend_map_on_2nd_device.len().derive_add_ctx(2),
+    |r, c| r.write(svar2, c),
+  ));
+
+  assert_eq!(friend_map_on_2nd_device.get(&"bob").val.unwrap().read().val, vec![svar2]);
+
+  // // Meanwhile, on the first device we remove
+  // // the entire 'bob' entry from the friend map.
+  // friend_map.apply(friend_map.rm("bob", friend_map.get(&"bob").derive_rm_ctx()));
+
+  // assert!(friend_map.get(&"bob").val.is_none());
+
+  // once these two devices synchronize...
+  let friend_map_snapshot = friend_map.clone();
+  let friend_map_on_2nd_device_snapshot = friend_map_on_2nd_device.clone();
+
+  assert_eq!(friend_map_snapshot.get(&"bob").val.unwrap().read().val, vec![svar1]);
+  assert_eq!(friend_map_on_2nd_device_snapshot.get(&"bob").val.unwrap().read().val, vec![svar2]);
+
+  friend_map.merge(friend_map_on_2nd_device_snapshot);
+  friend_map_on_2nd_device.merge(friend_map_snapshot);
+  assert_eq!(friend_map, friend_map_on_2nd_device);
+
+
+  assert_eq!(friend_map.get(&"bob").val.unwrap().read().val, vec![svar2]);
+  assert_eq!(friend_map_on_2nd_device.get(&"bob").val.unwrap().read().val, vec![svar2]);
 }
