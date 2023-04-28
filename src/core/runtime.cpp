b40fe30b8de5f56e1f2d0e575305e585447a9f41
Giovanni Petrantoni <7008900+sinkingsugar@users.noreply.github.com>
2023-04-28 09:42:59 +0800

    win32 fixes

--- src/core/runtime.cpp
+++ src/core/runtime.cpp
@@ -3308,62 +3308,78 @@ void incRef(ShardPtr shard) {
 } // namespace shards
 
 #ifdef TRACY_ENABLE
-void* operator new(std::size_t count) {
-    void* ptr = std::malloc(count);
-    TracySecureAlloc(ptr, count);
-    return ptr;
+void *operator new(std::size_t count) {
+  void *ptr = std::malloc(count);
+  TracySecureAlloc(ptr, count);
+  return ptr;
 }
 
-void* operator new[](std::size_t count) {
-    void* ptr = std::malloc(count);
-    TracySecureAlloc(ptr, count);
-    return ptr;
+void *operator new[](std::size_t count) {
+  void *ptr = std::malloc(count);
+  TracySecureAlloc(ptr, count);
+  return ptr;
 }
 
-void* operator new(std::size_t count, std::align_val_t alignment) {
-    std::size_t align_value = static_cast<std::size_t>(alignment);
-    std::size_t aligned_count = (count + align_value - 1) / align_value * align_value;
-    void* ptr = std::aligned_alloc(align_value, aligned_count);
-    TracySecureAlloc(ptr, count);
-    return ptr;
+void *operator new(std::size_t count, std::align_val_t alignment) {
+  std::size_t align_value = static_cast<std::size_t>(alignment);
+  std::size_t aligned_count = (count + align_value - 1) / align_value * align_value;
+#ifdef WIN32
+  void *ptr = _aligned_malloc(aligned_count, align_value);
+#else
+  void *ptr = std::aligned_alloc(align_value, aligned_count);
+#endif
+  TracySecureAlloc(ptr, count);
+  return ptr;
 }
 
-void* operator new[](std::size_t count, std::align_val_t alignment) {
-    std::size_t align_value = static_cast<std::size_t>(alignment);
-    std::size_t aligned_count = (count + align_value - 1) / align_value * align_value;
-    void* ptr = std::aligned_alloc(align_value, aligned_count);
-    TracySecureAlloc(ptr, count);
-    return ptr;
+void *operator new[](std::size_t count, std::align_val_t alignment) {
+  std::size_t align_value = static_cast<std::size_t>(alignment);
+  std::size_t aligned_count = (count + align_value - 1) / align_value * align_value;
+#ifdef WIN32
+  void *ptr = _aligned_malloc(aligned_count, align_value);
+#else
+  void *ptr = std::aligned_alloc(align_value, aligned_count);
+#endif
+  TracySecureAlloc(ptr, count);
+  return ptr;
 }
 
-void operator delete(void* ptr) noexcept {
-    TracySecureFree(ptr);
-    std::free(ptr);
+void operator delete(void *ptr) noexcept {
+  TracySecureFree(ptr);
+  std::free(ptr);
 }
 
-void operator delete[](void* ptr) noexcept {
-    TracySecureFree(ptr);
-    std::free(ptr);
+void operator delete[](void *ptr) noexcept {
+  TracySecureFree(ptr);
+  std::free(ptr);
 }
 
-void operator delete(void* ptr, std::align_val_t alignment) noexcept {
-    TracySecureFree(ptr);
-    std::free(ptr);
+void operator delete(void *ptr, std::align_val_t alignment) noexcept {
+  TracySecureFree(ptr);
+#ifdef WIN32
+  _aligned_free(ptr);
+#else
+  std::free(ptr);
+#endif
 }
 
-void operator delete[](void* ptr, std::align_val_t alignment) noexcept {
-    TracySecureFree(ptr);
-    std::free(ptr);
+void operator delete[](void *ptr, std::align_val_t alignment) noexcept {
+  TracySecureFree(ptr);
+#ifdef WIN32
+  _aligned_free(ptr);
+#else
+  std::free(ptr);
+#endif
 }
 
-void operator delete(void* ptr, std::size_t count) noexcept {
-    TracySecureFree(ptr);
-    std::free(ptr);
+void operator delete(void *ptr, std::size_t count) noexcept {
+  TracySecureFree(ptr);
+  std::free(ptr);
 }
 
-void operator delete[](void* ptr, std::size_t count) noexcept {
-    TracySecureFree(ptr);
-    std::free(ptr);
+void operator delete[](void *ptr, std::size_t count) noexcept {
+  TracySecureFree(ptr);
+  std::free(ptr);
 }
 
 #endif
