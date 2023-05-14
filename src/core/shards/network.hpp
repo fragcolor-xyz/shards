a88e1446555dd8e1e9b0bd65b6b4aabaee119918
Giovanni Petrantoni <7008900+sinkingsugar@users.noreply.github.com>
2023-05-14 22:41:59 +0800

    improve network

---
+++ src/core/shards/network.hpp
@@ -0,0 +1,26 @@
+#ifndef NETWORK_HPP
+#define NETWORK_HPP
+
+#include <memory>
+#pragma clang attribute push(__attribute__((no_sanitize("undefined"))), apply_to = function)
+#include <boost/container/stable_vector.hpp>
+#pragma clang attribute pop
+
+// ASIO must go first!!
+#include <boost/asio.hpp>
+
+#include "shards.h"
+
+using boost::asio::ip::udp;
+
+struct OnPeerConnected {
+  udp::endpoint endpoint;
+  std::weak_ptr<SHWire> wire;
+};
+
+struct OnPeerDisconnected {
+  udp::endpoint endpoint;
+  std::weak_ptr<SHWire> wire;
+};
+
+#endif
