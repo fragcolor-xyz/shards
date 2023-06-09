a36809fa67c17bd85f2f15d53287fe37ca5c9c61
Guus Waals <_@guusw.nl>
2023-06-09 13:34:01 +0800

    Fix SHLISP_API & add FLATTEN/ALWAYS_INLINE to ensure inlining

--- shards/shards/SHLisp.h
+++ shards/shards/SHLisp.h
@@ -3,13 +3,19 @@
 
 #include <shards/shards.h>
 
+#ifdef shards_EXPORTS
+#define SHLISP_API SHARDS_EXPORT
+#else
+#define SHLISP_API SHARDS_IMPORT
+#endif
+
 extern "C" {
-SHARDS_API __cdecl void *shLispCreate(const char *path);
-SHARDS_API __cdecl void *shLispCreateSub(void *parent);
-SHARDS_API __cdecl void *shLispSetPrefix(const char *envNamespace);
-SHARDS_API __cdecl void shLispDestroy(void *env);
-SHARDS_API __cdecl SHBool shLispEval(void *env, const char *str, SHVar *output = nullptr);
-SHARDS_API __cdecl SHBool shLispAddToEnv(void *env, const char *str, SHVar input, bool reference);
+SHLISP_API __cdecl void *shLispCreate(const char *path);
+SHLISP_API __cdecl void *shLispCreateSub(void *parent);
+SHLISP_API __cdecl void shLispSetPrefix(const char *envNamespace);
+SHLISP_API __cdecl void shLispDestroy(void *env);
+SHLISP_API __cdecl SHBool shLispEval(void *env, const char *str, SHVar *output = nullptr);
+SHLISP_API __cdecl SHBool shLispAddToEnv(void *env, const char *str, SHVar input, bool reference);
 }
 
 #endif /* BAF4257E_4054_4E92_B538_547BD5F05C0D */
