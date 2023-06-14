e03f7c5bf308294890e5aee040638986dc00e024
Guus Waals <_@guusw.nl>
2023-06-14 14:05:59 +0800

    Fixes for Table Var keys

--- shards/modules/gfx/steps.cpp
+++ shards/modules/gfx/steps.cpp
@@ -259,7 +259,7 @@ struct DrawablePassShard {
     }
 
     Var ignoreDrawableFeaturesVar;
-    if (getFromTable(context, inputTable, "IgnoreDrawableFeatures", ignoreDrawableFeaturesVar)) {
+    if (getFromTable(context, inputTable, Var("IgnoreDrawableFeatures"), ignoreDrawableFeaturesVar)) {
       checkType(ignoreDrawableFeaturesVar.valueType, SHType::Bool, "IgnoreDrawableFeatures");
       step.ignoreDrawableFeatures = (bool)ignoreDrawableFeaturesVar;
     } else {
