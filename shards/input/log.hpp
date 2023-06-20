6d9d9de39cef07171fbead3f4a143280fb737e0a
Guus Waals <_@guusw.nl>
2023-06-20 13:51:18 +0800

    Various input fixes. Fix indexing for WriteOutput to "depth". Add input logger

---
+++ shards/input/log.hpp
@@ -0,0 +1,20 @@
+#ifndef C2052FE4_794D_41AC_8A5F_DE82A84C764C
+#define C2052FE4_794D_41AC_8A5F_DE82A84C764C
+
+#include <log/log.hpp>
+
+namespace shards::input { 
+inline shards::logging::Logger getLogger() {
+  auto logger = spdlog::get("input");
+  if (!logger) {
+    logger = std::make_shared<spdlog::logger>("input");
+    // Set default log level to warn to be less verbose
+    logger->set_level(spdlog::level::info);
+    shards::logging::init(logger);
+  }
+  return logger;
+}
+}
+
+
+#endif /* C2052FE4_794D_41AC_8A5F_DE82A84C764C */
