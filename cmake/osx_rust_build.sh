6b5c4d50eaa8da5708c104bdb9e6b5ae5c1b1abc
Guus Waals <_@guusw.nl>
2023-06-27 11:40:47 +0800

    Update wgpu/wgpu-native for vision fixes. Rust build script should work with new XCode beta tools

--- cmake/osx_rust_build.sh
+++ cmake/osx_rust_build.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 # Script to work around some issues when calling `cargo build`/`cargo rustc` from Xcode for iOS
 #
-# The SDKROOT needs to be set to the HOST SDKROOT so that build scripts are able to build
-#  if not, it results in linker errors similar to this issue https://github.com/TimNN/cargo-lipo/issues/41
+# Based how corrosion sets the library path
+# https://github.com/corrosion-rs/corrosion/blob/6a0d3ae99d8600eac7c693cb282cee4693cdc0b7/cmake/Corrosion.cmake#L873
+# Without this, build scripts will not build, because it can not link them
 #
 # Additionally, bindgen needs to be passed the sysroot for the target platform to be able to find the system headers
 
 set -ex
 TARGET_SDKROOT=`xcrun --sdk $1 --show-sdk-path`
 shift
-export SDKROOT=`xcrun --sdk macosx --show-sdk-path`
+
 export BINDGEN_EXTRA_CLANG_ARGS="-isysroot $TARGET_SDKROOT -isystem $TARGET_SDKROOT/usr/include/c++/v1 $BINDGEN_EXTRA_CLANG_ARGS"
+export LIBRARY_PATH=`xcrun --sdk macosx --show-sdk-path`/usr/lib
+
 $@
