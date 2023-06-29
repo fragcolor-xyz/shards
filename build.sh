87125d63cfa6409fb5c0fb861e444343279f1133
Carlos Fontes <carlos@fragcolor.xyz>
2023-06-29 18:27:36 +0900

    Automate shards CLI build

--- build.sh
+++ build.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
 
-set -e
-
 git submodule update --init --recursive
 
 ./bootstrap
 
 test -d build || mkdir build
 test -d build/debug || mkdir build/debug
-test -d build/release || mkdir build/release
 
 pushd build
+
 cmake -G Ninja -DCMAKE_BUILD_TYPE=Debug -B./Debug ..
+
 ninja -C Debug shards
+
 popd build
