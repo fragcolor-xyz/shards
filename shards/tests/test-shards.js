696a13d0b36117cdd572584edaae55e75c899d0d
Giovanni Petrantoni <7008900+sinkingsugar@users.noreply.github.com>
2023-07-01 22:12:24 +0800

    fix wasm simple test

--- shards/tests/test-shards.js
+++ shards/tests/test-shards.js
@@ -3,5 +3,5 @@
 
 const shards = require('../../build/' + process.argv[2]);
 shards({
-  arguments: ["--", "-e", '(do (def Root (Mesh)) (schedule Root (Wire "x" (Pause 2.0) (Msg "Hello") "process.kill(process.pid, \\\"SIGTERM\\\")" (_Emscripten.Eval))) (run Root 0.1))']
+  arguments: ["-e", '(do (def Root (Mesh)) (schedule Root (Wire "x" (Pause 2.0) (Msg "Hello") "process.kill(process.pid, \\\"SIGTERM\\\")" (_Emscripten.Eval))) (run Root 0.1))']
 });
