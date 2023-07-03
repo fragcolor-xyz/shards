c7fbe3a9c1334c3d835a6e1e3d64c3981075321b
Guus Waals <_@guusw.nl>
2023-07-03 13:08:40 +0800

    Cleanup

--- shards/gfx/mesh_utils.cpp
+++ shards/gfx/mesh_utils.cpp
@@ -14,14 +14,6 @@ using namespace boost::container;
 
 namespace gfx {
 
-inline bool isFlippedCoordinateSpace(float3 right, float3 forward, float3 up) {
-  float3 expectedForward = linalg::normalize(linalg::cross(right, up));
-  if (linalg::dot(forward, expectedForward) < 0) {
-    return true;
-  }
-  return false;
-}
-
 StorageType indexFormatToStorageType(IndexFormat fmt) {
   switch (fmt) {
   case IndexFormat::UInt16:
@@ -280,7 +272,6 @@ MeshPtr generateLocalBasisAttribute(MeshPtr mesh) {
 
   auto &indexData = mesh->getIndexData();
   auto &vertexData = mesh->getVertexData();
-  size_t numVertices = mesh->getNumVertices();
   size_t numIndices = mesh->getNumIndices();
 
   if (srcFormat.primitiveType != PrimitiveType::TriangleList) {
@@ -322,7 +313,6 @@ MeshPtr generateLocalBasisAttribute(MeshPtr mesh) {
     }
   }
 
-  // (float*)(*((boost::container::vector<gfx::AttribBuffer,boost::container::small_vector_allocator<gfx::AttribBuffer,boost::container::new_allocator<void>,void>,void>*)&srcAttributes))[2].buf.data(),100
   // Validate required attributes
   {
     if (!positionIndex.has_value()) {
@@ -354,20 +344,6 @@ MeshPtr generateLocalBasisAttribute(MeshPtr mesh) {
     throw std::runtime_error("Mesh does not have an index buffer");
   }
 
-  // MeshFormat dstFormat = srcFormat;
-  // dstFormat.vertexAttributes.clear();
-
-  // size_t dstStride{};
-  // small_vector<size_t, 16> dstAttribOffsets;
-
-  // Directly copy existing attributes
-  // for (size_t i = 0; i < attributesToCopy.size(); i++) {
-  //   auto &attrib = srcFormat.vertexAttributes[attributesToCopy[i]];
-  //   dstFormat.vertexAttributes.push_back(attrib);
-  //   dstAttribOffsets.push_back(dstStride);
-  //   dstStride += attrib.numComponents * getStorageTypeSize(attrib.type);
-  // }
-
   AttribBuffer tangentBuffer;
   tangentBuffer.initZero(StorageType::Float32, 4, numIndices);
 
@@ -392,108 +368,34 @@ MeshPtr generateLocalBasisAttribute(MeshPtr mesh) {
       .m_getNumVerticesOfFace = [](const SMikkTSpaceContext *ctx, const int iFace) -> int { return 3; },
   };
   iface.m_getPosition = [](const SMikkTSpaceContext *ctx, float fvPosOut[], const int iFace, const int iVert) {
-    //  decltype(type)
     auto vec = ((Context *)ctx)->positionBuffer.read<float, 3, false>(iFace * 3 + iVert);
     fvPosOut[0] = float(vec.x);
     fvPosOut[1] = float(vec.y);
     fvPosOut[2] = float(vec.z);
   };
   iface.m_getNormal = [](const SMikkTSpaceContext *ctx, float fvPosOut[], const int iFace, const int iVert) {
-    //  decltype(type)
     auto vec = ((Context *)ctx)->normalBuffer.read<float, 3, false>(iFace * 3 + iVert);
     fvPosOut[0] = float(vec.x);
     fvPosOut[1] = float(vec.y);
     fvPosOut[2] = float(vec.z);
   };
   iface.m_getTexCoord = [](const SMikkTSpaceContext *ctx, float fvPosOut[], const int iFace, const int iVert) {
-    //  decltype(type)
     auto vec = ((Context *)ctx)->texCoordBuffer.read<float, 2, false>(iFace * 3 + iVert);
+    // NOTE: Texcoord is flipped to match glTF space
+    //   reference: https://github.com/mrdoob/three.js/issues/25263
     fvPosOut[0] = fmod(float(vec.x), 1.0f);
     fvPosOut[1] = 1.0f - fmod(float(vec.y), 1.0f);
   };
 
   iface.m_setTSpaceBasic = [](const SMikkTSpaceContext *ctx, const float fvTangent[], const float fSign, const int iFace,
                               const int iVert) {
-    // auto z = linalg::normalize(((Context *)ctx)->normalBuffer.read<float, 3, false>(iFace * 3 + iVert));
     float3 x = float3(fvTangent);
-
-    // if (linalg::length2(y) == 0.0f) {
-    //   y = -generateTangent(z); // prevent NAN
-    // }
-
-    // float3 y = linalg::normalize(linalg::cross(z, x));
-    // x = linalg::normalize(linalg::cross(y, z));
-
-    // if (isFlippedCoordinateSpace(x, z, y)) {
-    //   SPDLOG_INFO("flipped");
-    // }
-
-    // float d0 = linalg::dot(x, y);
-    // float d1 = linalg::dot(y, z);
-    // float d2 = linalg::dot(z, x);
-    // if (!isRoughlyEqual(d0, 0.0f) || !isRoughlyEqual(d1, 0.0f) || !isRoughlyEqual(d2, 0.0f)) {
-    //   SPDLOG_INFO("non-orthogonal");
-    // }
-
-    // float4 qbase = linalg::normalize(linalg::rotation_quat(float3x3(x, y, z)));
-
-    // auto &qbaseBuffer = ((Context *)ctx)->qbaseBuffer;
-    // qbaseBuffer.write(iFace * 3 + iVert, qbase);
     auto &tangentBuffer = ((Context *)ctx)->tangentBuffer;
     tangentBuffer.write(iFace * 3 + iVert, float4(x, fSign));
   };
-  // iface.m_setTSpace = [](const SMikkTSpaceContext *ctx, const float fvTangent[], const float fvBiTangent[], const float fMagS,
-  //                        const float fMagT, const tbool bIsOrientationPreserving, const int iFace, const int iVert) {
-  //   auto z = linalg::normalize(((Context *)ctx)->normalBuffer.read<float, 3, false>(iFace * 3 + iVert));
 
-  //   float3 x(fvTangent);
-  //   float3 y(fvBiTangent);
-
-  //   float fSign = bIsOrientationPreserving ? 1.0f : (-1.0f);
-  //   float3 bitangent = fSign * cross(z, x);
-  //   // float3 y = linalg::normalize(fSign * linalg::cross(z, x));
-  //   // x = linalg::normalize(linalg::cross(y, z));
-  //   // float4 qbase = linalg::normalize(linalg::rotation_quat(float3x3(x, y, z)));
-  //   float4 qbase = float4(x * fMagS, 0.0);
-
-  //   auto &qbaseBuffer = ((Context *)ctx)->qbaseBuffer;
-  //   qbaseBuffer.write(iFace * 3 + iVert, qbase);
-  // };
   context.ctx.m_pInterface = &iface;
   genTangSpaceDefault(&context.ctx);
-
-  // if (normalIndex) {
-  //   auto& posBuffer = srcAttributes[positionIndex.value()];
-  //   auto& uvBuffer = srcAttributes[positionIndex.value()];
-  //   auto& normalBuffer = srcAttributes[positionIndex.value()];
-  //   // Normals are either present as vertex attributes or approximated.
-  //   for (size_t ti = 0; ti < numIndices / 3; ti++) {
-  //     // vec2 UV = getNormalUV();
-  //     // vec2 uv_dx = dFdx(UV);
-  //     // vec2 uv_dy = dFdy(UV);
-
-  //     // if (length(uv_dx) + length(uv_dy) <= 1e-6) {
-  //     //   uv_dx = vec2(1.0, 0.0);
-  //     //   uv_dy = vec2(0.0, 1.0);
-  //     // }
-
-  //     // vec3 t_ = (uv_dy.t * dFdx(v_Position) - uv_dx.t * dFdy(v_Position)) / (uv_dx.s * uv_dy.t - uv_dy.s * uv_dx.t);
-
-  //     // vec3 n, t, b, ng;
-  //     float3 positions[3];
-  //     float2 uvs[3];
-  //     for (size_t i = 0; i < 3; i++) {
-  //       positions[i] =
-  //     }
-  //     ng = normalize(v_Normal);
-  //     t = normalize(t_ - ng * dot(ng, t_));
-  //     b = cross(ng, t);
-  //   }
-  // } else {
-  //   ng = normalize(cross(dFdx(v_Position), dFdy(v_Position)));
-  //   t = normalize(t_ - ng * dot(ng, t_));
-  //   b = cross(ng, t);
-  // }
 
   small_vector<std::tuple<AttribBuffer *, std::string>, 16> outAttribs;
   for (size_t i : attributesToCopy) {
@@ -501,102 +403,5 @@ MeshPtr generateLocalBasisAttribute(MeshPtr mesh) {
   }
   outAttribs.push_back({&tangentBuffer, "tangent"});
   return generateMesh(std::nullopt, outAttribs);
-  // MeshPtr newMesh = std::make_shared<Mesh>();
-  // newMesh->update(dstFormat, std::move(newVertexData), std::vector<uint8_t>(indexData));
-  // return newMesh;
-
-  // auto &srcFormat = mesh->getFormat();
-
-  // small_vector<size_t, 16> srcAttribOffsets;
-  // small_vector<size_t, 16> srcAttribs;
-  // small_vector<size_t, 16> dstAttribs;
-
-  // size_t i{};
-  // std::optional<size_t> normalIndex;
-  // std::optional<size_t> tangentIndex;
-  // std::optional<size_t> texCoordIndex;
-  // std::optional<size_t> positionIndex;
-  // auto filterAttrib = [&](const MeshVertexAttribute &attrib) {
-  //   if (attrib.name == "position") {
-  //     positionIndex = i;
-  //     return true;
-  //   }
-  //   if (attrib.name == "normal") {
-  //     normalIndex = i;
-  //     return true;
-  //   }
-  //   if (attrib.name == "texCoord0") {
-  //     texCoordIndex = i;
-  //     return true;
-  //   }
-  //   if (attrib.name == "tangent") {
-  //     tangentIndex = i;
-  //     return false;
-  //   }
-  //   if (attrib.name == "binormal" || attrib.name == "bitangent") {
-  //     return false;
-  //   }
-  //   return true;
-  // };
-
-  // i = 0;
-  // size_t offset{};
-  // for (auto &attrib : srcFormat.vertexAttributes) {
-  //   srcAttribOffsets.push_back(offset);
-  //   offset += attrib.numComponents * getStorageTypeSize(attrib.type);
-  //   if (filterAttrib(attrib)) {
-  //     srcAttribs.push_back(i);
-  //     dstAttribs.push_back(i);
-  //   }
-  //   i++;
-  // }
-
-  // if (!positionIndex.has_value()) {
-  //   throw std::runtime_error("Mesh does not have a position attribute");
-  // }
-
-  // auto &indexData = mesh->getIndexData();
-  // auto &vertexData = mesh->getVertexData();
-  // size_t srcStride = srcFormat.getVertexSize();
-  // auto readFloatAttrib = [&](float *out, size_t attribIndex, size_t index) {
-  //   size_t vertIdx = srcFormat.indexFormat == IndexFormat::UInt16 ? ((uint16_t *)indexData.data())[index]
-  //                                                                 : ((uint32_t *)indexData.data())[index];
-  //   const uint8_t *srcData = vertexData.data() + srcStride * vertIdx + srcAttribOffsets[attribIndex];
-  //   memcpy(out, srcData, sizeof(float) * srcFormat.vertexAttributes[attribIndex].numComponents);
-  // };
-
-  // auto readFloatTriangleAttribs = [&](float *first, size_t numElements, size_t attribIndex, size_t startingIndex) {
-  //   for (size_t i = 0; i < 3; i++) {
-  //     readFloatAttrib(first + i * numElements, attribIndex, startingIndex + i);
-  //   }
-  // };
-
-  // size_t numIndices = mesh->getNumIndices();
-  // if (numIndices > 0) {
-  //   if (srcFormat.primitiveType == PrimitiveType::TriangleList) {
-  //     for (size_t i = 0; i < numIndices; i += 3) {
-  //       float3 positions[3];
-  //       readFloatTriangleAttribs(&positions[0].x, 3, positionIndex.value(), i);
-
-  //       if (normalIndex && texCoordIndex) {
-  //         float3 normal[3];
-  //         readFloatTriangleAttribs(&normal[0].x, 3, normalIndex.value(), i);
-
-  //         float2 uv[3];
-  //         readFloatTriangleAttribs(&uv[0].x, 2, texCoordIndex.value(), i);
-  //         // float3 t0 =
-  //       } else {
-  //         throw std::runtime_error("Mesh does not have a normal or texCoord0 attribute to derive tangent from");
-  //       }
-  //     }
-  //   } else {
-  //     throw std::runtime_error("Unsupported primitive type");
-  //   }
-  // } else {
-  //   throw std::runtime_error("generateTangentInfo only supports indexed meshes");
-  // }
-
-  // MeshFormat dstFormat = srcFormat;
-  // dstFormat.vertexAttributes.clear();
 }
 } // namespace gfx
