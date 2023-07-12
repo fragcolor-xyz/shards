d331a679d0ffa7aa470723297fecca0b013219ba
Yttruire <teozhihao99@gmail.com>
2023-07-12 15:13:49 +0800

    Update SVG crates and add Offset parameter to SVG.ToImage

--- shards/modules/svg/src/lib.rs
+++ shards/modules/svg/src/lib.rs
@@ -9,6 +9,7 @@ extern crate lazy_static;
 #[macro_use]
 extern crate compile_time_crc32;
 
+use core::ptr::null_mut;
 use shards::core::log;
 use shards::core::registerShard;
 use shards::shard::Shard;
@@ -17,6 +18,7 @@ use shards::shardsc::SHVarPayload;
 use shards::shardsc::SHVarPayload__bindgen_ty_1;
 use shards::shardsc::SHIMAGE_FLAGS_PREMULTIPLIED_ALPHA;
 use shards::shardsc::{SHType_Bytes, SHType_Image, SHType_String};
+use shards::types;
 use shards::types::common_type;
 use shards::types::ClonedVar;
 use shards::types::Context;
@@ -25,15 +27,16 @@ use shards::types::Parameters;
 use shards::types::Seq;
 use shards::types::Table;
 use shards::types::Type;
+use shards::types::Var;
 use shards::types::IMAGE_TYPES;
 use shards::types::INT2_TYPES_SLICE;
-use std::ffi::CString;
-use shards::types;
-use shards::types::Var;
-use core::ptr::null_mut;
+use shards::types::FLOAT2_TYPES_SLICE;
 use std::convert::TryInto;
+use std::ffi::CString;
 use tiny_skia::Pixmap;
-use usvg::ScreenSize;
+use usvg::tiny_skia_path::IntSize;
+use usvg::Transform;
+use usvg::TreeParsing;
 
 pub fn pixmap_to_var(pmap: &mut Pixmap) -> Var {
   Var {
@@ -55,12 +58,21 @@ pub fn pixmap_to_var(pmap: &mut Pixmap) -> Var {
 
 lazy_static! {
   static ref INPUT_TYPES: Vec<Type> = vec![common_type::string, common_type::bytes];
-  static ref PARAMETERS: Parameters = vec![(
+  static ref PARAMETERS: Parameters = vec![
+  (
     cstr!("Size"),
     shccstr!(
       "The desired output size, if (0, 0) will default to the size defined in the svg data."
     ),
     INT2_TYPES_SLICE
+  )
+    .into(),
+    (
+    cstr!("Offset"),
+    shccstr!(
+      "A positive x and y value offsets towards the right and the bottom of the screen respectively. (0.0, 0.0) by default. " // TODO: Double check description when done
+    ),
+    FLOAT2_TYPES_SLICE
   )
     .into()];
 }
@@ -69,6 +81,7 @@ lazy_static! {
 struct ToImage {
   pixmap: Option<Pixmap>,
   size: (i64, i64),
+  offset: (f32, f32),
 }
 
 impl Shard for ToImage {
@@ -93,12 +106,14 @@ impl Shard for ToImage {
   fn setParam(&mut self, index: i32, value: &Var) -> Result<(), &str> {
     match index {
       0 => Ok(self.size = value.try_into()?),
+      1 => Ok(self.offset = value.try_into()?),
       _ => unreachable!(),
     }
   }
   fn getParam(&mut self, index: i32) -> Var {
     match index {
       0 => self.size.into(),
+      1 => self.offset.into(),
       _ => unreachable!(),
     }
   }
@@ -106,12 +121,10 @@ impl Shard for ToImage {
     let mut opt = usvg::Options::default();
     // Get file's absolute directory.
     // opt.resources_dir = std::fs::canonicalize(&args[1]).ok().and_then(|p| p.parent().map(|p| p.to_path_buf()));
-    opt.fontdb.load_system_fonts();
+    // opt.fontdb.load_system_fonts();
     // opt.fontdb.set_generic_families();
 
-    let opt = opt.to_ref();
-
-    let rtree = match input.valueType {
+    let ntree = match input.valueType {
       SHType_Bytes => usvg::Tree::from_data(input.try_into().unwrap(), &opt).map_err(|e| {
         shlog!("{}", e);
         "Failed parse SVG bytes"
@@ -134,34 +147,44 @@ impl Shard for ToImage {
       })?,
     );
 
+    let (offset_x, offset_y): (f32, f32) = (
+      self.offset.0.try_into().map_err(|e| {
+        shlog!("{}", e);
+        "Invalid x offset"
+      })?,
+      self.offset.1.try_into().map_err(|e| {
+        shlog!("{}", e);
+        "Invalid y offset"
+      })?,
+    );
+
     let pixmap_size = if w == 0 && h == 0 {
-      Ok(rtree.svg_node().size.to_screen_size())
+      Ok(ntree.size.to_int_size())
     } else {
-      ScreenSize::new(w, h).ok_or("Invalid size")
+      IntSize::from_wh(w, h).ok_or("Invalid size")
     }?;
 
     if self.pixmap.is_none() {
       self.pixmap = Some(
-        tiny_skia::Pixmap::new(pixmap_size.width(), pixmap_size.height())
-          .ok_or("Failed to create pixmap")?,
+        Pixmap::new(pixmap_size.width(), pixmap_size.height()).ok_or("Failed to create pixmap")?,
       );
     } else {
       let pm = self.pixmap.as_ref().unwrap();
       if pixmap_size.width() != pm.width() || pixmap_size.height() != pm.height() {
         self.pixmap = Some(
-          tiny_skia::Pixmap::new(pixmap_size.width(), pixmap_size.height())
+          Pixmap::new(pixmap_size.width(), pixmap_size.height())
             .ok_or("Failed to create pixmap")?,
         );
       }
     }
 
-    resvg::render(
-      &rtree,
-      usvg::FitTo::Size(pixmap_size.width(), pixmap_size.height()),
-      tiny_skia::Transform::default(),
-      self.pixmap.as_mut().unwrap().as_mut(),
-    )
-    .ok_or("Failed to render SVG")?;
+    let mut rtree = resvg::Tree::from_usvg(&ntree);
+    rtree.size = pixmap_size.to_size();
+
+    rtree.render(
+      usvg::Transform::from_translate(offset_x, offset_y),
+      &mut self.pixmap.as_mut().unwrap().as_mut(),
+    );
 
     Ok(pixmap_to_var(self.pixmap.as_mut().unwrap()))
   }
