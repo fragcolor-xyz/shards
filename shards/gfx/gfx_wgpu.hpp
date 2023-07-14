d2a64b34278720fe7707887fbbfb254a5644ca3d
Guus Waals <_@guusw.nl>
2023-07-14 14:20:35 +0800

    Create custom C api for creating shaders/pipelines with error handling

--- shards/gfx/gfx_wgpu.hpp
+++ shards/gfx/gfx_wgpu.hpp
@@ -85,4 +85,40 @@ void gfxWgpuDeviceGetLimits(WGPUDevice device, WGPUSupportedLimits *outLimits);
 // When copying textures into buffers the bytesPerRow should be aligned to this number
 inline constexpr size_t WGPU_COPY_BYTES_PER_ROW_ALIGNMENT = 256;
 
+#if WEBGPU_NATIVE
+extern "C" {
+struct GFXShaderModuleDescriptor {
+  const char *label;
+  const char *wgsl;
+};
+
+struct GFXShaderModuleResult {
+  WGPUShaderModule module;
+  const char *error;
+};
+
+typedef void (*GFXShaderModuleResultCallback)(const GFXShaderModuleResult *result, void *userData);
+void gfxDeviceCreateShaderModule(WGPUDevice device, const GFXShaderModuleDescriptor *descriptor,
+                                 GFXShaderModuleResultCallback callback, void *userData);
+
+struct GFXRenderPipelineResult {
+  WGPURenderPipeline pipeline;
+  const char *error;
+};
+
+typedef void (*GFXRenderPipelineResultCallback)(const GFXRenderPipelineResult *result, void *userData);
+void gfxDeviceCreateRenderPipeline(WGPUDevice device, const WGPURenderPipelineDescriptor *descriptor,
+                                   GFXRenderPipelineResultCallback callback, void *userData);
+
+struct GFXComputePipelineResult {
+  WGPUComputePipeline pipeline;
+  const char *error;
+};
+
+typedef void (*GFXComputePipelineResultCallback)(const GFXComputePipelineResult *result, void *userData);
+void gfxDeviceCreateComputePipeline(WGPUDevice device, const WGPUComputePipelineDescriptor *descriptor,
+                                    GFXComputePipelineResultCallback callback, void *userData);
+}
+#endif
+
 #endif // GFX_GFX_WGPU
