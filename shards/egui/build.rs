2833f34c8de1bddf02f7f577bebc527aa9bd97cf
Guus Waals <_@guusw.nl>
2023-08-26 13:17:26 +0800

    Don't define WGPU_NATIVE on emscripten during bindgen

--- shards/egui/build.rs
+++ shards/egui/build.rs
@@ -14,17 +14,17 @@ fn main() {
   println!("cargo:rerun-if-changed=input.hpp");
   println!("cargo:rerun-if-changed=context.hpp");
 
-  let builder = gfx_build::setup_bindgen_for_gfx(gfx_path, bindgen::Builder::default());
-  let bindings = builder
+  let mut builder = gfx_build::setup_bindgen_for_gfx(gfx_path, bindgen::Builder::default());
+  builder = builder
     .header("rust_interop.hpp")
     .allowlist_type("egui::.*")
     .allowlist_var("SDLK_.*")
     .allowlist_type("SDL_KeyCode")
     .size_t_is_usize(true)
     .layout_tests(false)
-    .clang_arg(format!("-I{}/include", shards_root))
-    .generate()
-    .expect("Unable to generate bindings");
+    .clang_arg(format!("-I{}/include", shards_root));
+
+  let bindings = builder.generate().expect("Unable to generate bindings");
 
   let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
   bindings
