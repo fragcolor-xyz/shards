73373bff7bd96f77a1e034663a38c8fa4a378aef
Yttruire <teozhihao99@gmail.com>
2023-09-25 16:36:19 +0800

    Add PopupButton shard with basic functionality and cleanup Window

---
+++ shards/egui/src/containers/popup.rs
@@ -0,0 +1,160 @@
+/* SPDX-License-Identifier: BSD-3-Clause */
+/* Copyright © 2022 Fragcolor Pte. Ltd. */
+
+use crate::containers::WindowFlags;
+use crate::containers::SEQ_OF_WINDOW_FLAGS;
+use crate::containers::WINDOW_FLAGS_TYPE;
+use crate::util;
+use crate::widgets::text_util;
+use crate::Anchor;
+use crate::ANCHOR_TYPES;
+use crate::BOOL_VAR_SLICE;
+use crate::HELP_OUTPUT_EQUAL_INPUT;
+use shards::cstr;
+use shards::shard;
+use shards::shard::Shard;
+use shards::shard_impl;
+use shards::types::OptionalString;
+use shards::types::ANY_TYPES;
+use shards::types::{
+  common_type, Context, ExposedInfo, ExposedTypes, InstanceData, ParamVar, Seq, ShardsVar, Type,
+  Types, Var, ANYS_TYPES, SHARDS_OR_NONE_TYPES, STRING_TYPES,
+};
+
+use crate::{CONTEXTS_NAME, FLOAT2_VAR_SLICE, PARENTS_UI_NAME};
+use shards::{
+  core::register_shard,
+  types::{BOOL_OR_NONE_SLICE, INT_OR_NONE_TYPES_SLICE, STRING_VAR_OR_NONE_SLICE},
+};
+
+#[derive(shard)]
+#[shard_info(
+  "UI.PopupButton",
+  "Wraps a button with a popup that can act as a drop-down menu or suggestion menu."
+)]
+struct PopupButtonShard {
+  #[shard_param("Label", "The text label of the button.", STRING_TYPES)]
+  pub label: ParamVar,
+  #[shard_param("Wrap", "Wrap the text depending on the layout.", BOOL_OR_NONE_SLICE)]
+  pub wrap: ParamVar,
+  #[shard_param("Style", "The text style.", ANYS_TYPES)]
+  pub style: ParamVar,
+  #[shard_param(
+    "Contents",
+    "The shards to execute and render inside the popup ui when the button is pressed.",
+    SHARDS_OR_NONE_TYPES
+  )]
+  pub contents: ShardsVar,
+  #[shard_warmup]
+  contexts: ParamVar,
+  #[shard_warmup]
+  parents: ParamVar,
+  #[shard_required]
+  required: ExposedTypes,
+}
+
+impl Default for PopupButtonShard {
+  fn default() -> Self {
+    Self {
+      contexts: ParamVar::new_named(CONTEXTS_NAME),
+      parents: ParamVar::new_named(PARENTS_UI_NAME),
+      label: ParamVar::default(),
+      wrap: ParamVar::default(),
+      style: ParamVar::default(),
+      contents: ShardsVar::default(),
+      required: Vec::new(),
+    }
+  }
+}
+
+#[shard_impl]
+impl Shard for PopupButtonShard {
+  fn input_types(&mut self) -> &Types {
+    &ANY_TYPES
+  }
+
+  fn input_help(&mut self) -> OptionalString {
+    OptionalString(shccstr!(
+      "The value that will be passed to the Contents shards of the rendered window."
+    ))
+  }
+
+  fn output_types(&mut self) -> &Types {
+    &ANY_TYPES
+  }
+
+  fn output_help(&mut self) -> OptionalString {
+    *HELP_OUTPUT_EQUAL_INPUT
+  }
+
+  fn warmup(&mut self, context: &Context) -> Result<(), &str> {
+    self.warmup_helper(context)?;
+
+    Ok(())
+  }
+
+  fn cleanup(&mut self) -> Result<(), &str> {
+    self.cleanup_helper()?;
+
+    Ok(())
+  }
+
+  fn compose(&mut self, data: &InstanceData) -> Result<Type, &str> {
+    self.compose_helper(data)?;
+
+    util::require_context(&mut self.required);
+    util::require_parents(&mut self.required);
+
+    if !self.contents.is_empty() {
+      self.contents.compose(data)?;
+    }
+
+    // Always passthrough the input
+    Ok(data.inputType)
+  }
+
+  fn activate(&mut self, context: &Context, input: &Var) -> Result<Var, &str> {
+    if let Some(ui) = util::get_current_parent_opt(self.parents.get())? {
+      let label: &str = self.label.get().try_into()?;
+      let mut text = egui::RichText::new(label);
+
+      let style = self.style.get();
+      if !style.is_none() {
+        text = text_util::get_styled_text(text, &style.try_into()?)?;
+      }
+
+      let mut button = egui::Button::new(text);
+
+      let wrap = self.wrap.get();
+      if !wrap.is_none() {
+        let wrap: bool = wrap.try_into()?;
+        button = button.wrap(wrap);
+      }
+
+      let response = ui.add(button);
+      let popup_id = ui.make_persistent_id(label);
+
+      if response.clicked() {
+        ui.memory_mut(|mem| mem.toggle_popup(popup_id));
+      }
+      let below = egui::AboveOrBelow::Below;
+      if !self.contents.is_empty() {
+        if let Some(inner) = egui::popup::popup_above_or_below_widget(ui, popup_id, &response, below, |ui| {
+          util::activate_ui_contents(context, input, ui, &mut self.parents, &mut self.contents)
+        }) {
+          // Only if popup is open will there be an inner result. In such a case, verify that nothing went wrong.
+          inner?;
+        }
+      }
+    } else {
+      return Err("No UI parent");
+    }
+
+    // Always passthrough the input
+    Ok(*input)
+  }
+}
+
+pub fn register_shards() {
+  register_shard::<PopupButtonShard>();
+}
