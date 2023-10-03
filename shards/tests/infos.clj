919f0c9dc7eb3ae1229a3f217b552777d0d1be17
vantemoon <sylviassunrx0820@outlook.com>
2023-10-03 12:27:14 +0800

    Add strings and timing

--- shards/tests/infos.clj
+++
@@ -1,7 +0,0 @@
-; SPDX-License-Identifier: BSD-3-Clause
-; Copyright © 2021 Fragcolor Pte. Ltd.
-
-; strings are compressed by default, need to unpack if we use info
-(decompress-strings)
-(prn (map (fn* [name] [name (shard-info name)]) (shards)))
-(prn (map (fn* [name] [name (enum-info name)]) (enums)))
