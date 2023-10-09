35fb317854c67e35986f43147669a1a32bcc128e
Guus Waals <_@guusw.nl>
2023-10-09 21:07:23 +0800

    Change window close button behavior.
    It's now a shards variable, when pressed will call
    Close button only shows when this is set

--- shards/egui/src/containers/window.rs
+++ shards/egui/src/containers/window.rs
@@ -15,6 +15,7 @@ use shards::shard::Shard;
 use shards::shard_impl;
 use shards::types::OptionalString;
 use shards::types::ANY_TYPES;
+use shards::types::WireState;
 use shards::types::{
   common_type, Context, ExposedInfo, ExposedTypes, InstanceData, ParamVar, Seq, ShardsVar, Type,
   Types, Var, SHARDS_OR_NONE_TYPES, STRING_TYPES,
@@ -35,7 +36,7 @@ lazy_static! {
   "UI.Window",
   "Creates a floating window which can be dragged, closed, collapsed, and resized."
 )]
-struct FormabbleWindowShard {
+struct WindowShard {
   #[shard_param("Title", "The window title displayed on the titlebar.", STRING_TYPES)]
   pub title: ParamVar,
   #[shard_param(
@@ -55,17 +56,11 @@ struct FormabbleWindowShard {
   )]
   pub height: ParamVar,
   #[shard_param(
-    "Closable",
-    "Whether the window will have a close button in its window title bar. If this is enabled, an open variable must be provided.",
-    BOOL_OR_NONE_SLICE
+    "Closed",
+    "When provided with a callback, this window will have a close button and call this when pressed.",
+    SHARDS_OR_NONE_TYPES,
   )]
-  pub closable: ParamVar,
-  #[shard_param(
-    "Open",
-    "Whether the window is open and being shown, or hidden.",
-    BOOL_VAR_SLICE
-  )]
-  pub open: ParamVar,
+  pub closed: ShardsVar,
   #[shard_param("Flags", "Window flags.", WINDOW_FLAGS_OR_SEQ_TYPES)]
   pub flags: ParamVar,
   #[shard_param(
@@ -83,9 +78,10 @@ struct FormabbleWindowShard {
   parents: ParamVar,
   #[shard_required]
   required: ExposedTypes,
+  has_close_button: bool,
 }
 
-impl Default for FormabbleWindowShard {
+impl Default for WindowShard {
   fn default() -> Self {
     Self {
       contexts: ParamVar::new_named(CONTEXTS_NAME),
@@ -95,19 +91,19 @@ impl Default for FormabbleWindowShard {
       anchor: ParamVar::default(),
       width: ParamVar::default(),
       height: ParamVar::default(),
-      closable: ParamVar::default(),
-      open: ParamVar::default(),
+      closed: ShardsVar::default(),
       flags: ParamVar::default(),
       id: ParamVar::default(),
       cached_id: None,
       contents: ShardsVar::default(),
       required: Vec::new(),
+      has_close_button: false,
     }
   }
 }
 
 #[shard_impl]
-impl Shard for FormabbleWindowShard {
+impl Shard for WindowShard {
   fn input_types(&mut self) -> &Types {
     &ANY_TYPES
   }
@@ -154,8 +150,11 @@ impl Shard for FormabbleWindowShard {
       self.required.push(id_info);
     }
 
-    if !self.contents.is_empty() {
-      self.contents.compose(data)?;
+    self.contents.compose(data)?;
+
+    self.has_close_button = !self.closed.is_empty();
+    if self.has_close_button {
+      self.closed.compose(data)?;
     }
 
     // Always passthrough the input
@@ -163,116 +162,106 @@ impl Shard for FormabbleWindowShard {
   }
 
   fn activate(&mut self, context: &Context, input: &Var) -> Result<Var, &str> {
-    // Try to retrieve a state variable determining whether the window should be shown
-    let open: bool = if !self.open.is_none() {
-      self.open.get().try_into()?
-    } else {
-      true
-    };
-
     // If the window should be drawn this frame
-    if open {
-      let gui_ctx = &util::get_current_context(&self.contexts)?.egui_ctx;
+    let gui_ctx = &util::get_current_context(&self.contexts)?.egui_ctx;
 
-      let mut failed = false;
-      if !self.contents.is_empty() {
-        let title: &str = self.title.get().try_into()?;
-        let mut window = egui::Window::new(title);
+    let mut open = true;
+    let mut failed = false;
+    if !self.contents.is_empty() {
+      let title: &str = self.title.get().try_into()?;
+      let mut window = egui::Window::new(title);
 
-        if let Ok(id) = <&str>::try_from(self.id.get()) {
-          let id = self.cached_id.get_or_insert_with(|| egui::Id::new(id));
-          window = window.id(*id);
-        }
+      if let Ok(id) = <&str>::try_from(self.id.get()) {
+        let id = self.cached_id.get_or_insert_with(|| egui::Id::new(id));
+        window = window.id(*id);
+      }
 
-        window = if self.anchor.get().is_none() {
-          // note: in egui the position is relative to the top-left corner of the whole UI
-          // but a window is constrained by the available rect of the central panel.
-          // Thus, we offset it to make it more intuitive for users.
-          // i.e. the position is now relative to the top-left corner of the central panel.
-          let position = self.position.get();
-          if !position.is_none() {
-            let pos: (f32, f32) = self.position.get().try_into()?;
-            let rect = gui_ctx.available_rect();
-            window.default_pos(egui::Pos2 {
-              x: pos.0 + rect.min.x,
-              y: pos.1 + rect.min.y,
-            })
-          } else {
-            window
-          }
+      window = if self.anchor.get().is_none() {
+        // note: in egui the position is relative to the top-left corner of the whole UI
+        // but a window is constrained by the available rect of the central panel.
+        // Thus, we offset it to make it more intuitive for users.
+        // i.e. the position is now relative to the top-left corner of the central panel.
+        let position = self.position.get();
+        if !position.is_none() {
+          let pos: (f32, f32) = self.position.get().try_into()?;
+          let rect = gui_ctx.available_rect();
+          window.default_pos(egui::Pos2 {
+            x: pos.0 + rect.min.x,
+            y: pos.1 + rect.min.y,
+          })
         } else {
-          let offset: (f32, f32) = self.position.get().try_into().unwrap_or_default();
-          let anchor: Anchor = self.anchor.get().try_into()?;
-          window.anchor(anchor.into(), offset)
-        };
+          window
+        }
+      } else {
+        let offset: (f32, f32) = self.position.get().try_into().unwrap_or_default();
+        let anchor: Anchor = self.anchor.get().try_into()?;
+        window.anchor(anchor.into(), offset)
+      };
 
-        let width = self.width.get();
+      let width = self.width.get();
+      if !width.is_none() {
+        let width: i64 = width.try_into()?;
+        window = window.min_width(width as f32).default_width(width as f32);
+      }
+
+      let height = self.height.get();
+      if !height.is_none() {
+        let height: i64 = height.try_into()?;
+        window = window
+          .min_height(height as f32)
+          .default_height(height as f32)
+      }
+
+      if self.has_close_button {
+        // if close button is enabled, it must be provided with a state variable for tracking whether window is open
+        // this open variable is to provide a way for other Shards to be notified of the window being closed
+        window = window.open(&mut open);
+      }
+      // else, if no closable provided, then default to false, which does nothing
+
+      for bits in WindowShard::try_get_flags(self.flags.get())? {
+        match (WindowFlags { bits }) {
+          WindowFlags::NoTitleBar => {
+            window = window.title_bar(false);
+          }
+          WindowFlags::NoResize => {
+            window = window.resizable(false);
+          }
+          WindowFlags::Scrollbars => {
+            window = window.scroll2([true, true]);
+          }
+          WindowFlags::NoCollapse => {
+            window = window.collapsible(false);
+          }
+          WindowFlags::Immovable => {
+            window = window.movable(false);
+          }
+          _ => (),
+        }
+      }
+
+      window.show(gui_ctx, |ui| {
         if !width.is_none() {
-          let width: i64 = width.try_into()?;
-          window = window.min_width(width as f32).default_width(width as f32);
+          ui.set_width(ui.available_width());
         }
-
-        let height = self.height.get();
         if !height.is_none() {
-          let height: i64 = height.try_into()?;
-          window = window
-            .min_height(height as f32)
-            .default_height(height as f32)
+          ui.set_height(ui.available_height());
         }
-
-        let closable = self.closable.get();
-        if !closable.is_none() {
-          let closable: bool = closable.try_into()?;
-
-          if closable {
-            // if close button is enabled, it must be provided with a state variable for tracking whether window is open
-            // this open variable is to provide a way for other Shards to be notified of the window being closed
-            if !self.open.is_variable() {
-              return Err("Window: open variable required when closable is true");
-            } else {
-              window = window.open(self.open.get_mut().try_into()?);
-            }
-          }
-        } 
-        // else, if no closable provided, then default to false, which does nothing
-
-        for bits in FormabbleWindowShard::try_get_flags(self.flags.get())? {
-          match (WindowFlags { bits }) {
-            WindowFlags::NoTitleBar => {
-              window = window.title_bar(false);
-            }
-            WindowFlags::NoResize => {
-              window = window.resizable(false);
-            }
-            WindowFlags::Scrollbars => {
-              window = window.scroll2([true, true]);
-            }
-            WindowFlags::NoCollapse => {
-              window = window.collapsible(false);
-            }
-            WindowFlags::Immovable => {
-              window = window.movable(false);
-            }
-            _ => (),
-          }
+        if util::activate_ui_contents(context, input, ui, &mut self.parents, &mut self.contents)
+          .is_err()
+        {
+          failed = true;
         }
+      });
 
-        window.show(gui_ctx, |ui| {
-          if !width.is_none() {
-            ui.set_width(ui.available_width());
-          }
-          if !height.is_none() {
-            ui.set_height(ui.available_height());
-          }
-          if util::activate_ui_contents(context, input, ui, &mut self.parents, &mut self.contents)
-            .is_err()
-          {
-            failed = true;
-          }
-        });
+      if failed {
+        return Err("Failed to activate window contents");
+      }
 
-        if failed {
-          return Err("Failed to activate window contents");
+      if !open {
+        let mut tmp = Var::default();
+        if self.closed.activate(context, input, &mut tmp) == WireState::Error {
+          return Err("Failed to activate close callback");
         }
       }
     }
@@ -282,7 +271,7 @@ impl Shard for FormabbleWindowShard {
   }
 }
 
-impl FormabbleWindowShard {
+impl WindowShard {
   fn try_get_flags(var: &Var) -> Result<Vec<i32>, &str> {
     match var.valueType {
       shards::SHType_Enum => Ok(vec![unsafe {
@@ -302,5 +291,5 @@ impl FormabbleWindowShard {
 }
 
 pub fn register_shards() {
-  register_shard::<FormabbleWindowShard>();
+  register_shard::<WindowShard>();
 }
