322334eec752cde5a88b8ee7d72ad1a907c4ddaf
Giovanni Petrantoni <7008900+sinkingsugar@users.noreply.github.com>
2023-10-17 15:39:46 +0800

    Revert unschedule removal

--- include/shards/shards.h
+++ include/shards/shards.h
@@ -890,6 +890,7 @@ typedef SHMeshRef(__cdecl *SHCreateMesh)();
 typedef void(__cdecl *SHDestroyMesh)(SHMeshRef mesh);
 typedef struct SHVar(__cdecl *SHCreateMeshVar)();
 typedef SHBool(__cdecl *SHCompose)(SHMeshRef mesh, SHWireRef wire);
+typedef void(__cdecl *SHUnSchedule)(SHMeshRef mesh, SHWireRef wire);
 typedef void(__cdecl *SHSchedule)(SHMeshRef mesh, SHWireRef wire, SHBool compose);
 typedef SHBool(__cdecl *SHTick)(SHMeshRef mesh);
 typedef void(__cdecl *SHTerminate)(SHMeshRef mesh);
@@ -1027,6 +1028,7 @@ typedef struct _SHCore {
   SHCreateMeshVar createMeshVar;
   SHCompose compose;
   SHSchedule schedule;
+  SHUnSchedule unschedule;
   SHTick tick; // returns false if we had a failure
   SHTick isEmpty; // returns true if we have no wires to tick
   SHTerminate terminate;
