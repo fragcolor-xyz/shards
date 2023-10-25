f6a6b821e6551195f21c7df8b20d72bc899d491e
Guus Waals <_@guusw.nl>
2023-10-25 19:06:58 +0800

    Fix gfx::mod

--- shards/gfx/math.hpp
+++ shards/gfx/math.hpp
@@ -51,8 +51,23 @@ inline bool isRoughlyEqual(T val, T target, T tolerance = T(0.05)) {
 // results in all positive numbers for positive divisor
 // e.g. mod(-1, 3) = 2
 // https://en.wikipedia.org/wiki/Modulo#/media/File:Divmod_floored.svg (https://en.wikipedia.org/wiki/Modulo)
-template <typename T> inline constexpr T mod(T a, T b) { return a - (b * (T)std::floor(a / b)); }
+template <typename T>
+inline constexpr std::enable_if_t<!std::is_integral_v<T>, T> //
+mod(T a, T b) {
+  return a - (b * (T)std::floor(double(a) / double(b)));
+}
 
+// Modulo implemented using floored division
+template <typename T>
+inline constexpr std::enable_if_t<std::is_integral_v<T>, T> //
+mod(T a, T b) {
+  T m = a % b;
+  if (m >= 0) {
+    return m;
+  } else {
+    return m + b;
+  }
+}
 
 } // namespace gfx
 
