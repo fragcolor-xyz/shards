e18106eb7164c8cd63eca79a0f7b83852ed20410
Giovanni Petrantoni <7008900+sinkingsugar@users.noreply.github.com>
2023-10-27 16:33:12 +0800

    Improve stack_diff script

--- utils/stack_diff.py
+++ utils/stack_diff.py
@@ -1,30 +1,37 @@
 import lldb
 
 def stack_diff(debugger, command, result, internal_dict):
-    print("Running stack_diff...")  # Debugging output
-
-    # Get the current thread
+    print("Running stack_diff...")
     thread = debugger.GetSelectedTarget().GetProcess().GetSelectedThread()
-
-    # Get the current frame and its index
     current_frame = thread.GetSelectedFrame()
     current_frame_idx = current_frame.GetFrameID()
-
-    # Get the parent frame using the index
     parent_frame = thread.GetFrameAtIndex(current_frame_idx + 1)
 
     if parent_frame is None or not parent_frame.IsValid():
         print("No parent frame available.")
         return
 
-    # Get SP for current and parent frames
     current_sp = current_frame.GetSP()
     parent_sp = parent_frame.GetSP()
-
-    # Calculate and print the difference
     diff = parent_sp - current_sp
     print(f"Stack Pointer Difference: {diff} bytes")
 
+def stack_total_size(debugger, command, result, internal_dict):
+    print("Running stack_total_size...")
+    thread = debugger.GetSelectedTarget().GetProcess().GetSelectedThread()
+    num_frames = thread.GetNumFrames()
+
+    if num_frames == 0:
+        print("No stack frames available.")
+        return
+
+    first_frame_sp = thread.GetFrameAtIndex(0).GetSP()
+    last_frame_sp = thread.GetFrameAtIndex(num_frames - 1).GetSP()
+
+    total_diff = first_frame_sp - last_frame_sp
+    print(f"Total Stack Size: {total_diff} bytes")
+
 def __lldb_init_module(debugger, internal_dict):
     debugger.HandleCommand('command script add -f stack_diff.stack_diff stack_diff')
-    print("stack_diff command has been loaded.")  # Debugging output
+    debugger.HandleCommand('command script add -f stack_diff.stack_total_size stack_total_size')
+    print("stack_diff and stack_total_size commands have been loaded.")
