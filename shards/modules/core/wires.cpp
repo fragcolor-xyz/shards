04ded02eb3113483c18d7306246b7f5564ee47be
Giovanni Petrantoni <7008900+sinkingsugar@users.noreply.github.com>
2023-12-01 16:12:19 +0800

    Remove unnecessary code and clean up variables

--- shards/modules/core/wires.cpp
+++ shards/modules/core/wires.cpp
@@ -2088,11 +2088,6 @@ struct WhenDone : CapturingSpawners {
     }
     _injectedVariables.clear();
 
-    for (auto &v : _vars) {
-      v.cleanup();
-    }
-    _vars.clear();
-
     SHLOG_TRACE("WhenDone::onCleanupCleanup, released mesh and variables");
   }
 
@@ -2162,6 +2157,11 @@ struct WhenDone : CapturingSpawners {
     if (wire) {
       SHLOG_TRACE("WhenDone::cleanup, will schedule wire {}", wire->name);
     }
+
+    for (auto &v : _vars) {
+      v.cleanup();
+    }
+    _vars.clear();
 
     WireBase::cleanup(context);
   }
