3c3f666bcc384023346666b44461e856c52ac30b
Giovanni Petrantoni <7008900+sinkingsugar@users.noreply.github.com>
2024-01-22 13:38:14 +0800

    Fix issue with wire suspension in SwitchTo struct

--- shards/modules/core/wires.cpp
+++ shards/modules/core/wires.cpp
@@ -977,7 +977,7 @@ struct SwitchTo : public WireBase {
     // When we are here, pWire is suspended, (could be in the middle of a loop, anywhere!)
 
     // check if this is the end! as we might get here on a stop
-    if(context->shouldStop()) {
+    if(wire && context->shouldStop()) {
       // means we were stopped, so we need to stop the wire too
       shards::stop(pWire);
     }
