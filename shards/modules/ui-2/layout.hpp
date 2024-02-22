d29a460aa3c947cdbe2d2aa08e25d50d45403b38
Guus Waals <_@guusw.nl>
2024-02-22 20:13:58 +0800

    WIP

---
+++ shards/modules/ui-2/layout.hpp
@@ -0,0 +1,14 @@
+#ifndef D65BA92F_A730_4B18_80DF_12458449AA34
+#define D65BA92F_A730_4B18_80DF_12458449AA34
+
+#include <linalg/linalg.h>
+
+namespace ui2 {
+using namespace linalg::aliases;
+
+struct Align {
+  float2 align;
+};
+} // namespace ui2
+
+#endif /* D65BA92F_A730_4B18_80DF_12458449AA34 */
