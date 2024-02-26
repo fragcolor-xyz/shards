cf616b850da213dded056d74ca88c26ad6b80a71
Guus Waals <_@guusw.nl>
2024-02-26 12:01:33 +0800

    Add Debug.DumpEnv

--- shards/modules/debug/debug.cpp
+++ shards/modules/debug/debug.cpp
@@ -1,6 +1,7 @@
 #include <shards/core/shared.hpp>
 #include <shards/core/params.hpp>
 #include <shards/common_types.hpp>
+#include <boost/core/span.hpp>
 
 namespace shards::Debug {
 struct DebugNoop {
@@ -33,11 +34,42 @@ struct DebugNoop {
     return input;
   }
 };
+
+struct DumpEnv {
+  static SHTypesInfo inputTypes() { return shards::CoreInfo::AnyType; }
+  static SHTypesInfo outputTypes() { return shards::CoreInfo::AnyType; }
+  static SHOptionalString help() { return SHCCSTR("Dumps the variable environment during compose"); }
+
+  PARAM_IMPL();
+
+  void warmup(SHContext *context) { PARAM_WARMUP(context); }
+  void cleanup(SHContext *context) { PARAM_CLEANUP(context); }
+
+  PARAM_REQUIRED_VARIABLES();
+  SHTypeInfo compose(SHInstanceData &data) {
+    PARAM_COMPOSE_REQUIRED_VARIABLES(data);
+    std::stringstream ss;
+    ss << "Compose environment:\n";
+    auto span = boost::span(data.shared.elements, data.shared.len);
+
+    std::sort(span.begin(), span.end(),
+              [](const SHExposedTypeInfo &a, const SHExposedTypeInfo &b) { return std::string_view(a.name) < b.name; });
+    for (auto &v : data.shared) {
+      ss << fmt::format(" - {} ({})\n", v.name, v.exposedType);
+    }
+    SHLOG_INFO("{}", ss.str());
+
+    return data.inputType;
+  }
+
+  SHVar activate(SHContext *shContext, const SHVar &input) { return input; }
+};
 } // namespace shards::Debug
 
 namespace shards {
 SHARDS_REGISTER_FN(debug) {
   using namespace Debug;
   REGISTER_SHARD("Debug.Noop", DebugNoop);
+  REGISTER_SHARD("Debug.DumpEnv", DumpEnv);
 }
 } // namespace shards
