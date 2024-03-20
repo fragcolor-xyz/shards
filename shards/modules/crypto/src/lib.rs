be95f4b9c51e19df1c6aec476d7b9132c2ddcbbd
Giovanni Petrantoni <7008900+sinkingsugar@users.noreply.github.com>
2024-03-20 12:13:24 +0900

    Add tiny-bip39 crate and implement Mnemonic.Generate shard (#951)

--- shards/modules/crypto/src/lib.rs
+++ shards/modules/crypto/src/lib.rs
@@ -1,7 +1,17 @@
 /* SPDX-License-Identifier: BSD-3-Clause */
 /* Copyright © 2020 Fragcolor Pte. Ltd. */
+use shards::core::register_shard;
+use shards::shard::Shard;
 use shards::types::common_type;
+use shards::types::Context;
+use shards::types::ExposedInfo;
+use shards::types::ExposedTypes;
+use shards::types::InstanceData;
+use shards::types::ParamVar;
 use shards::types::Type;
+use shards::types::Types;
+use shards::types::{Var, ClonedVar};
+use std::convert::TryInto;
 
 #[macro_use]
 extern crate shards;
@@ -25,6 +35,63 @@ static CRYPTO_KEY_TYPES: &[Type] = &[
 
 static PUB_KEY_TYPES: &[Type] = &[common_type::bytes, common_type::bytes_var];
 
+use bip39::{Mnemonic, MnemonicType, Language, Seed};
+
+lazy_static! {
+  pub static ref INPUT_TYPES: Vec<Type> = vec![common_type::int];
+  pub static ref OUTPUT_TYPES: Vec<Type> = vec![common_type::string];
+}
+
+#[derive(shards::shard)]
+#[shard_info("Mnemonic.Generate", "Generates a BIP39 mnemonic")]
+struct MnemonicGenerate {
+  output: ClonedVar,
+}
+
+impl Default for MnemonicGenerate {
+  fn default() -> Self {
+    Self {
+      output: ClonedVar::default(),
+    }
+  }
+}
+
+#[shards::shard_impl]
+impl Shard for MnemonicGenerate {
+  fn input_types(&mut self) -> &Types {
+    &INPUT_TYPES
+  }
+
+  fn output_types(&mut self) -> &Types {
+    &OUTPUT_TYPES
+  }
+
+  fn warmup(&mut self, ctx: &Context) -> Result<(), &str> {
+    self.warmup_helper(ctx)?;
+    Ok(())
+  }
+
+  fn cleanup(&mut self, ctx: Option<&Context>) -> Result<(), &str> {
+    self.cleanup_helper(ctx)?;
+    Ok(())
+  }
+
+  fn activate(&mut self, context: &Context, input: &Var) -> Result<Var, &str> {
+    let size: i64 = input.try_into().unwrap();
+    let mnemonic_type = match size {
+      12  => MnemonicType::Words12,
+      15  => MnemonicType::Words15,
+      18  => MnemonicType::Words18,
+      21  => MnemonicType::Words21,
+      24  => MnemonicType::Words24,
+      _ => return Err("Invalid mnemonic size"),
+    };
+    let mnemonic = Mnemonic::new(mnemonic_type, Language::English);
+    self.output = Var::ephemeral_string(mnemonic.phrase()).into();
+    Ok(self.output.0)
+  }
+}
+
 #[no_mangle]
 pub extern "C" fn shardsRegister_crypto_crypto(core: *mut shards::shardsc::SHCore) {
   unsafe {
@@ -37,4 +104,5 @@ pub extern "C" fn shardsRegister_crypto_crypto(core: *mut shards::shardsc::SHCor
   substrate::register_shards();
   curve25519::register_shards();
   chachapoly::register_shards();
+  register_shard::<MnemonicGenerate>();
 }
