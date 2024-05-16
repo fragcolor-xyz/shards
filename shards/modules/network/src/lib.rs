1c9cd87239ea45c803d04ff87e79e73dfffae05a
Guus Waals <_@guusw.nl>
2024-05-16 21:16:43 +0800

    Add websocket shards & make common network interface

--- shards/modules/network/src/lib.rs
+++ shards/modules/network/src/lib.rs
@@ -1,4 +1,3 @@
-
 #[macro_use]
 extern crate shards;
 
@@ -8,13 +7,5 @@ extern crate lazy_static;
 #[macro_use]
 extern crate compile_time_crc32;
 
-pub mod ws;
-
-#[no_mangle]
-pub extern "C" fn shardsRegister_network_rust(core: *mut shards::shardsc::SHCore) {
-unsafe {
-  shards::core::Core = core;
-  }
-
-  ws::register_shards();
-}
+pub mod pollnet_context;
+pub mod ws_api;
