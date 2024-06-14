41dab4b3c1fa7cd9778744c371542a5983c073ff
Guus Waals <_@guusw.nl>
2024-06-14 18:03:46 +0800

    Fix drag checks

--- shards/egui/src/dnd/mod.rs
+++ shards/egui/src/dnd/mod.rs
@@ -27,9 +27,9 @@ pub fn drag_source<R>(
   id: egui::Id,
   body: impl FnOnce(&mut egui::Ui) -> R,
 ) -> InnerResponse<R> {
-  let is_dropped = ui.input(|i| i.pointer.any_released());
+  let is_dropped = ui.ctx().drag_stopped_id().is_some();
   let is_dragging_something = !ctx.dnd_value.borrow().0.is_none();
-  let is_being_dragged = is_dragging_something && ctx.egui_ctx.is_being_dragged(id); // ui.memory(|mem| mem.is_being_dragged(id));
+  let is_being_dragged = is_dragging_something && ctx.egui_ctx.is_being_dragged(id);
 
   let cursor_already_set =
     is_dropped || ui.output(|x| x.cursor_icon == egui::CursorIcon::NotAllowed);
@@ -98,7 +98,7 @@ pub fn drop_target<R>(
   visualize: bool,
   body: impl FnOnce(&mut egui::Ui) -> R,
 ) -> InnerResponse<R> {
-  let is_being_dragged = ui.memory(|mem| mem.is_anything_being_dragged());
+  let is_being_dragged = ui.ctx().dragged_id().is_some() || ui.ctx().drag_stopped_id().is_some();
 
   let margin = Vec2::splat(4.0);
 
@@ -315,8 +315,8 @@ impl Shard for DragDrop {
         util::activate_ui_contents(context, input, ui, &mut self.parents, &mut self.contents)
       });
 
-      let is_being_dragged = ui.memory(|mem| mem.is_anything_being_dragged());
-      if ui.input(|i| i.pointer.any_released()) {
+      let is_being_dragged = ui.ctx().drag_stopped_id().is_some();
+      if ui.ctx().drag_stopped_id().is_some() {
         // Reset cursor
         ui.ctx().set_cursor_icon(egui::CursorIcon::Default);
         if is_being_dragged && accepts_value && inner.response.hovered() {
