cf9ec406096772c6a4cd7795dce11162d6a50442
Giovanni Petrantoni <7008900+sinkingsugar@users.noreply.github.com>
2024-06-18 11:42:19 +0800

    Apple fixes and constexpr var support

--- include/shards/shards.hpp
+++ include/shards/shards.hpp
@@ -928,7 +928,7 @@ struct Var : public SHVar {
     payload.imageValue = img;
   }
 
-  explicit Var(int64_t src) : SHVar() {
+  explicit constexpr Var(int64_t src) : SHVar() {
     valueType = SHType::Int;
     payload.intValue = src;
   }
