ddb12c580bdaf346112729c26ba6a8c884177442
Guus Waals <_@guusw.nl>
2024-07-10 18:07:57 +0800

    Fix gfx-test

--- cmake/Platform.cmake
+++ cmake/Platform.cmake
@@ -249,8 +249,24 @@ endif()
 option(USE_ASAN "Use address sanitizer" OFF)
 
 if(USE_ASAN)
-  add_compile_options($<$<COMPILE_LANGUAGE:CXX>:-DBOOST_USE_ASAN> $<$<COMPILE_LANGUAGE:CXX,C>:-fsanitize=address> $<$<COMPILE_LANGUAGE:CXX,C>:-fno-optimize-sibling-calls> $<$<COMPILE_LANGUAGE:CXX,C>:-fsanitize-address-use-after-scope> $<$<COMPILE_LANGUAGE:CXX,C>:-fno-omit-frame-pointer> $<$<COMPILE_LANGUAGE:CXX,C>:-g> $<$<COMPILE_LANGUAGE:CXX,C>:-O1>)
-  add_link_options($<$<COMPILE_LANGUAGE:CXX>:-DBOOST_USE_ASAN> $<$<COMPILE_LANGUAGE:CXX,C>:-fsanitize=address> $<$<COMPILE_LANGUAGE:CXX,C>:-fno-optimize-sibling-calls> $<$<COMPILE_LANGUAGE:CXX,C>:-fsanitize-address-use-after-scope> $<$<COMPILE_LANGUAGE:CXX,C>:-fno-omit-frame-pointer> $<$<COMPILE_LANGUAGE:CXX,C>:-g> $<$<COMPILE_LANGUAGE:CXX,C>:-O1>)
+  add_compile_options(
+    $<$<COMPILE_LANGUAGE:CXX>:-DBOOST_USE_ASAN> 
+    $<$<COMPILE_LANGUAGE:CXX,C>:-fsanitize=address> 
+    $<$<COMPILE_LANGUAGE:CXX,C>:-fno-optimize-sibling-calls> 
+    $<$<COMPILE_LANGUAGE:CXX,C>:-fsanitize-address-use-after-scope> 
+    $<$<COMPILE_LANGUAGE:CXX,C>:-fno-omit-frame-pointer> 
+    $<$<COMPILE_LANGUAGE:CXX,C>:-g> 
+    $<$<COMPILE_LANGUAGE:CXX,C>:-O1>
+  )
+  add_link_options(
+    $<$<COMPILE_LANGUAGE:CXX>:-DBOOST_USE_ASAN> 
+    $<$<COMPILE_LANGUAGE:CXX,C>:-fsanitize=address> 
+    $<$<COMPILE_LANGUAGE:CXX,C>:-fno-optimize-sibling-calls> 
+    $<$<COMPILE_LANGUAGE:CXX,C>:-fsanitize-address-use-after-scope> 
+    $<$<COMPILE_LANGUAGE:CXX,C>:-fno-omit-frame-pointer> 
+    $<$<COMPILE_LANGUAGE:CXX,C>:-g> 
+    $<$<COMPILE_LANGUAGE:CXX,C>:-O1>
+  )
   add_compile_definitions($<$<COMPILE_LANGUAGE:CXX,C>:SH_USE_ASAN>)
 endif()
 
