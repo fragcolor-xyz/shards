c4192d52c5da944874b825b0db56ad1542b1bc6c
Guus Waals <_@guusw.nl>
2024-07-11 19:58:49 +0800

    Add Process.Exe

--- shards/modules/os/process.cpp
+++ shards/modules/os/process.cpp
@@ -1,7 +1,6 @@
 /* SPDX-License-Identifier: BSD-3-Clause */
 /* Copyright © 2019 Fragcolor Pte. Ltd. */
 
-#include "shards/core/module.hpp"
 #ifdef _WIN32
 #include "winsock2.h"
 #endif
@@ -214,10 +213,48 @@ struct StackTrace {
     return Var(_output);
   }
 };
-}; // namespace Process
+
+struct Exe {
+  std::string _buf;
+
+  static SHTypesInfo inputTypes() { return shards::CoreInfo::NoneType; }
+  static SHTypesInfo outputTypes() { return shards::CoreInfo::StringType; }
+  static SHOptionalString help() { return SHCCSTR("Gives the current executable path."); }
+
+  PARAM_IMPL();
+
+  void warmup(SHContext *context) { PARAM_WARMUP(context); }
+  void cleanup(SHContext *context) { PARAM_CLEANUP(context); }
+
+  PARAM_REQUIRED_VARIABLES();
+  SHTypeInfo compose(SHInstanceData &data) { return outputTypes().elements[0]; }
+
+  // Coped from C:\Projects\shards2\deps\tracy\public\client\TracyProfiler.cpp
+  static const char *GetProcessName() {}
+
+  SHVar activate(SHContext *shContext, const SHVar &input) {
+    _buf.clear();
+#ifdef _WIN32
+    _buf.resize(_MAX_PATH);
+    _buf.resize(GetModuleFileNameA(nullptr, _buf.data(), _MAX_PATH));
+#elif defined __ANDROID__
+#if __ANDROID_API__ >= 21
+    _buf = getprogname();
+#endif
+#elif defined __linux__ && defined _GNU_SOURCE
+    _buf = program_invocation_short_name;
+#elif defined __APPLE__ || defined BSD
+    _buf = getprogname();
+#endif
+    return Var(_buf);
+  }
+};
+} // namespace Process
 
 SHARDS_REGISTER_FN(process) {
   REGISTER_SHARD("Process.Run", Process::Run);
   REGISTER_SHARD("Process.StackTrace", Process::StackTrace);
+  REGISTER_SHARD("Process.Exe", Process::Exe);
 }
-}; // namespace shards
+} // namespace shards
+// namespace shards
