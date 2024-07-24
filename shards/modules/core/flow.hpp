0829f9c1b14860c5c87ed18d305b2931a81a959f
Giovanni Petrantoni <7008900+sinkingsugar@users.noreply.github.com>
2024-07-24 17:26:24 +0800

    Minor log changes

--- shards/modules/core/flow.hpp
+++ shards/modules/core/flow.hpp
@@ -478,7 +478,7 @@ struct Await : public BaseSubFlow {
 
   SHTypeInfo compose(const SHInstanceData &data) {
     if (data.onWorkerThread) {
-      SPDLOG_WARN("Await shard is being used on a worker thread, ignoring.");
+      SHLOG_DEBUG("Await shard is being used on a worker thread, ignoring, wire: {}", data.wire ? data.wire->name : "unknown");
       _alreadyOnWorker = true;
       OVERRIDE_ACTIVATE(data, activateSync);
     } else {
