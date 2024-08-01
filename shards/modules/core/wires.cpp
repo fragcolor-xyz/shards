f1e035f9f638f8f3b4e1b20bfcf00c3764acc0d5
Giovanni Petrantoni <7008900+sinkingsugar@users.noreply.github.com>
2024-08-01 17:07:03 +0800

    Fix stopwire compose

--- shards/modules/core/wires.cpp
+++ shards/modules/core/wires.cpp
@@ -528,7 +528,10 @@ struct StopWire : public WireBase {
       }
     }
 
-    return data.inputType;
+    if (passthrough)
+      return data.inputType;
+    else
+      return CoreInfo::AnyType;
   }
 
   void cleanup(SHContext *context) {
