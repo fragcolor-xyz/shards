ce87870c367516c3d26067c255ebb50750725e7e
Giovanni Petrantoni <7008900+sinkingsugar@users.noreply.github.com>
2024-08-12 15:45:58 +0800

    feat(sqlite): add SQLITE_VEC_API macro for cross-platform support
    Define SQLITE_VEC_API macro to handle DLL export on Windows and static
    linking. Update submodule commit for sqlite-vec.

--- deps/sqlite/sqlite-vec-static/sqlite-vec.h
+++ deps/sqlite/sqlite-vec-static/sqlite-vec.h
@@ -3,6 +3,16 @@
 
 #include "sqlite3ext.h"
 
+#ifdef SQLITE_VEC_STATIC
+#define SQLITE_VEC_API
+#else
+#ifdef _WIN32
+#define SQLITE_VEC_API __declspec(dllexport)
+#else
+#define SQLITE_VEC_API
+#endif
+#endif
+
 #define SQLITE_VEC_VERSION "v0.1shards"
 #define SQLITE_VEC_DATE ""
 #define SQLITE_VEC_SOURCE ""
@@ -23,9 +33,8 @@ __declspec(dllexport)
 int sqlite3_vec_fs_read_init(sqlite3 *db, char **pzErrMsg,
                           const sqlite3_api_routines *pApi);
 
-
 #ifdef __cplusplus
-}  /* end of the 'extern "C"' block */
+} /* end of the 'extern "C"' block */
 #endif
 
 #endif /* ifndef SQLITE_VEC_H */
