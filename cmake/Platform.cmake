b0a04fc4405db1102c0e49442ab92def903bd0f6
Giovanni Petrantoni <7008900+sinkingsugar@users.noreply.github.com>
2024-09-09 00:13:35 +0800

    feat(cmake): Enable -O1 optimization for ASAN and TSAN
    Uncomment -O1 optimization flags for Address Sanitizer (ASAN) and Thread Sanitizer (TSAN) in Platform.cmake to improve performance while maintaining debugging capabilities.

--- cmake/Platform.cmake
+++ cmake/Platform.cmake
@@ -284,7 +284,7 @@ if(USE_ASAN)
     $<$<COMPILE_LANGUAGE:CXX,C>:-fsanitize-address-use-after-scope>
     $<$<COMPILE_LANGUAGE:CXX,C>:-fno-omit-frame-pointer>
     $<$<COMPILE_LANGUAGE:CXX,C>:-g>
-    # $<$<COMPILE_LANGUAGE:CXX,C>:-O1>
+    $<$<COMPILE_LANGUAGE:CXX,C>:-O1>
   )
   add_link_options(
     $<$<COMPILE_LANGUAGE:CXX>:-DBOOST_USE_ASAN>
@@ -293,7 +293,7 @@ if(USE_ASAN)
     $<$<COMPILE_LANGUAGE:CXX,C>:-fsanitize-address-use-after-scope>
     $<$<COMPILE_LANGUAGE:CXX,C>:-fno-omit-frame-pointer>
     $<$<COMPILE_LANGUAGE:CXX,C>:-g>
-    # $<$<COMPILE_LANGUAGE:CXX,C>:-O1> # O1
+    $<$<COMPILE_LANGUAGE:CXX,C>:-O1>
   )
   add_compile_definitions($<$<COMPILE_LANGUAGE:CXX,C>:SH_USE_ASAN>)
 endif()
@@ -304,12 +304,12 @@ if(USE_TSAN)
   add_compile_options(
     $<$<COMPILE_LANGUAGE:CXX,C>:-fsanitize=thread>
     $<$<COMPILE_LANGUAGE:CXX,C>:-g>
-    # $<$<COMPILE_LANGUAGE:CXX,C>:-O1>
+    $<$<COMPILE_LANGUAGE:CXX,C>:-O1>
   )
   add_link_options(
     $<$<COMPILE_LANGUAGE:CXX,C>:-fsanitize=thread>
     $<$<COMPILE_LANGUAGE:CXX,C>:-g>
-    # $<$<COMPILE_LANGUAGE:CXX,C>:-O1>
+    $<$<COMPILE_LANGUAGE:CXX,C>:-O1>
   )
   add_compile_definitions($<$<COMPILE_LANGUAGE:CXX,C>:SH_USE_TSAN>)
 endif()
