60321cb0a381f72ed326e3f8ced39902f5752791
Guus Waals <_@guusw.nl>
2025-01-28 15:16:22 +0800

    Add rust very verbose

--- cmake/Rust.cmake
+++ cmake/Rust.cmake
@@ -123,6 +123,10 @@ if(CARGO_OFFLINE_MODE)
   list(APPEND RUSTC_FLAGS --offline)
 endif()
 
+if(CARGO_VERBOSE)
+  list(APPEND RUSTC_FLAGS -vv)
+endif()
+
 # if(USE_ASAN)
 # list(APPEND RUST_FLAGS -Zsanitizer=address)
 # endif()
