6711d482ec42e8aca2c0edc3173b45fe32c25534
Guus Waals <_@guusw.nl>
2025-04-04 19:10:10 +0800

    WIP

--- shards/lang/src/eval.rs
+++ shards/lang/src/eval.rs
@@ -2005,7 +2005,7 @@ impl<'e> VariableResolver<'e> {
           ("type", true) => process_type(func, line_info, self.e).map(ResolvedVar::new_const),
           ("ast", true) => process_ast(func, line_info, self.e).map(ResolvedVar::new_const),
           ("capture-eval-context", true) => {
-            let ctx = capture_eval_context(self.e);
+            let ctx: ClonedVar = capture_eval_context(self.e);
             Ok(ResolvedVar::new_const(SVar::Cloned(ctx)))
           }
           _ => {
@@ -3360,7 +3360,7 @@ fn eval_pipeline(
                   Param {
                     value: Value::Identifier(name),
                     ..
-                  },
+                  }, 
                   types,
                 ) => {
                   let make_trait_shards = Sequence {
