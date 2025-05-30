9913daa86baed30c6f7c02382c5ed496969418f0
Guus Waals <_@guusw.nl>
2025-05-30 20:07:02 +0800

    Fix implicit conversion of Var to TableVar in formatting

--- shards/core/foundation.hpp
+++ shards/core/foundation.hpp
@@ -1765,4 +1765,8 @@ inline const SHVar& format_as(const shards::TableVar& v) {
   return (SHVar&)v;
 }
 
+inline const SHVar& format_as(const shards::Var& v) {
+  return (SHVar&)v;
+}
+
 #endif // SH_CORE_FOUNDATION
