3e94de7dc7649773a33001bd63d09ed8c44a39c9
Guus Waals <_@guusw.nl>
2025-05-30 20:07:02 +0800

    Fix implicit conversion of Var to TableVar in formatting

--- include/shards/utility.hpp
+++ include/shards/utility.hpp
@@ -483,12 +483,12 @@ template <class SH_CORE> struct TTableVar : public SHVar {
 
   TTableVar(const TTableVar &other) : SHVar() { SH_CORE::cloneVar(*this, other); }
 
-  TTableVar(const SHVar &other) : SHVar() {
+  explicit TTableVar(const SHVar &other) : SHVar() {
     assert(other.valueType == SHType::Table);
     SH_CORE::cloneVar(*this, other);
   }
 
-  TTableVar(SHVar &&other) : SHVar() {
+  explicit TTableVar(SHVar &&other) : SHVar() {
     assert(other.valueType == SHType::Table);
     std::swap<SHVar>(*this, *reinterpret_cast<TTableVar *>(&other));
   }
@@ -710,7 +710,7 @@ template <class SH_CORE> struct TSeqVar : public SHVar {
 
   TSeqVar(TSeqVar &&other) : SHVar() { std::swap<SHVar>(*this, other); }
 
-  TSeqVar(SHVar &&other) : SHVar() {
+  explicit TSeqVar(SHVar &&other) : SHVar() {
     assert(other.valueType == SHType::Seq);
     std::swap<SHVar>(*this, other);
   }
@@ -906,9 +906,6 @@ template <typename T> struct hash<shards::TOwnedVar<T>> {
 };
 } // namespace std
 
-template<typename SH_CORE>
-auto format_as(const shards::TOwnedVar<SH_CORE>& ov) {
-  return (SHVar&)ov;
-}
+template <typename SH_CORE> auto format_as(const shards::TOwnedVar<SH_CORE> &ov) { return (SHVar &)ov; }
 
 #endif
