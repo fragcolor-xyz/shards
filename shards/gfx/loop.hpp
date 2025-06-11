9da75571d086e1d2ca242e615abba51da8b9584e
Guus Waals <_@guusw.nl>
2025-06-11 17:14:19 +0800

    Add wgpu-headers & gfx-headers

--- shards/gfx/loop.hpp
+++ shards/gfx/loop.hpp
@@ -18,6 +18,26 @@ struct Loop {
   bool beginFrame(double targetDeltaTime, double &deltaTime);
   double getAbsoluteTime() const;
 };
+inline Loop::Loop() : startTime(Clock::now()) {}
+inline bool Loop::beginFrame(double targetDeltaTime, double &outDeltaTime) {
+  TimePoint now = Clock::now();
+  outDeltaTime = std::chrono::duration<double>(now - lastFrameTime).count();
+  if (!haveFirstFrame) {
+    outDeltaTime = 0.0f;
+    lastFrameTime = now;
+    haveFirstFrame = true;
+    return true;
+  } else if (outDeltaTime >= targetDeltaTime) {
+    lastFrameTime = now;
+    return true;
+  }
+  return false;
+}
+
+inline double Loop::getAbsoluteTime() const {
+  TimePoint now = Clock::now();
+  return std::chrono::duration<double>(now - startTime).count();
+}
 } // namespace gfx
 
 #endif // GFX_LOOP
