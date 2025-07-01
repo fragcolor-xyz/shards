785158afe9066f316187d6b1dd05e302a281a408
Giovanni Petrantoni <7008900+sinkingsugar@users.noreply.github.com>
2025-07-01 19:09:22 +0800

    Refactor safeLookat function to accept custom up vector
    - Modified the safeLookat function in gfx/linalg.hpp to take an additional parameter for the up vector, allowing for more flexible camera orientation.
    - Updated the LookAt struct in core/linalg.hpp to utilize the new up vector parameter when calling safeLookat.
    - Cleaned up formatting in outputHelp methods for consistency.

--- shards/gfx/linalg.hpp
+++ shards/gfx/linalg.hpp
@@ -130,8 +130,8 @@ inline float4x4 rotationFromZDirection(const float3 &direction) {
   return float4x4(float4(right, 0), float4(up, 0), float4(fwd, 0), float4(0, 0, 0, 1));
 }
 
-inline float4x4 safeLookat(const float3 &eye, const float3 &center) {
-  float3 up = float3(0.0, 1.0, 0.0);
+inline float4x4 safeLookat(const float3 &eye, const float3 &center, const float3 &up_) {
+  float3 up = up_;
   float3 delta = linalg::normalize(eye - center);
   float adot = std::abs(linalg::dot(up, delta));
   const float threshold = 0.01f;
