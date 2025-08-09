2eecab8baca17a2380ae18a9f65290fa9652c03d
Giovanni Petrantoni <7008900+sinkingsugar@users.noreply.github.com>
2025-08-09 22:36:49 +0800

    Add TUI.VBox and TUIText components to TUI module
    This commit introduces the VBox and TUIText structures to the TUI module, allowing for the creation of vertical boxes and text elements within the terminal user interface. The TUI module's registration function is updated to include these new components, enhancing the overall functionality and flexibility of the TUI system. Additionally, the test script is modified to utilize the new TUI.VBox and TUI.Text components for improved demonstration and logging.

--- shards/modules/tui/tui.cpp
+++ shards/modules/tui/tui.cpp
@@ -17,8 +17,16 @@ struct TUIElement {
   ftxui::Element element;
 };
 
+struct TUIInnerElements {
+  ftxui::Elements elements;
+
+  void reset() { elements = {}; }
+  void clear() { elements.clear(); }
+};
+
 struct TUITypes {
   SHVAR_OBJECT_DECL('tuiE', "TUI.Element", Element, TUIElement);
+  SHVAR_OBJECT_DECL('tuie', "TUI.InnerElements", InnerElements, TUIInnerElements);
 };
 
 struct HBox {
@@ -26,25 +34,122 @@ struct HBox {
   static SHTypesInfo outputTypes() { return TUITypes::Element; }
   static SHOptionalString help() { return SHCCSTR("Creates a horizontal box"); }
 
+  PARAM(ShardsVar, _contents, "Contents", "The contents of the horizontal box.", {CoreInfo::ShardsOrNone});
+  PARAM_IMPL(PARAM_IMPL_FOR(_contents));
+
+  PARAM_REQUIRED_VARIABLES();
+  SHTypeInfo compose(SHInstanceData &data) {
+    PARAM_COMPOSE_REQUIRED_VARIABLES(data);
+    return TUITypes::Element;
+  }
+
   TUIElement *_element = nullptr;
 
-  void warmup(SHContext *shContext) { _element = TUITypes::ElementObjectVar.New(); }
+  void warmup(SHContext *shContext) {
+    _innerElementsVar.warmup(shContext);
+    assignVariableValue(_innerElementsVar.get(), Var::Object(&_innerElements, TUITypes::InnerElements));
+
+    _element = TUITypes::ElementObjectVar.New();
+
+    _contents.warmup(shContext);
+  }
 
   void cleanup(SHContext *shContext) {
+    _contents.cleanup(shContext);
+
+    _innerElements.reset();
+
     if (_element) {
       TUITypes::ElementObjectVar.Release(_element);
       _element = nullptr;
     }
+
+    _innerElementsVar.cleanup(shContext);
   }
 
-  ftxui::Elements _innerElements;
+  ParamVar _innerElementsVar{Var::ContextVar("_TUI.InnerElements")};
+  TUIInnerElements _innerElements;
   ShardsVar _action;
 
   SHVar activate(SHContext *shContext, const SHVar &input) {
     _innerElements.clear();
-    // push _innerElements to context
+    SHVar output{};
+    _contents.activate(shContext, input, output);
     _element->element = ftxui::hbox(_innerElements);
     return TUITypes::ElementObjectVar.Get(_element);
+  }
+};
+
+struct VBox {
+  static SHTypesInfo inputTypes() { return CoreInfo::AnyType; }
+  static SHTypesInfo outputTypes() { return TUITypes::Element; }
+  static SHOptionalString help() { return SHCCSTR("Creates a vertical box"); }
+
+  PARAM(ShardsVar, _contents, "Contents", "The contents of the horizontal box.", {CoreInfo::ShardsOrNone});
+  PARAM_IMPL(PARAM_IMPL_FOR(_contents));
+
+  PARAM_REQUIRED_VARIABLES();
+  SHTypeInfo compose(SHInstanceData &data) {
+    PARAM_COMPOSE_REQUIRED_VARIABLES(data);
+    _contents.compose(data);
+    return TUITypes::Element;
+  }
+
+  TUIElement *_element = nullptr;
+
+  void warmup(SHContext *shContext) {
+    _innerElementsVar.warmup(shContext);
+    assignVariableValue(_innerElementsVar.get(), Var::Object(&_innerElements, TUITypes::InnerElements));
+
+    _element = TUITypes::ElementObjectVar.New();
+
+    _contents.warmup(shContext);
+  }
+
+  void cleanup(SHContext *shContext) {
+    _contents.cleanup(shContext);
+
+    _innerElements.reset();
+
+    if (_element) {
+      TUITypes::ElementObjectVar.Release(_element);
+      _element = nullptr;
+    }
+
+    _innerElementsVar.cleanup(shContext);
+  }
+
+  ParamVar _innerElementsVar{Var::ContextVar("_TUI.InnerElements")};
+  TUIInnerElements _innerElements;
+  ShardsVar _action;
+
+  SHVar activate(SHContext *shContext, const SHVar &input) {
+    _innerElements.clear();
+    SHVar output{};
+    _contents.activate(shContext, input, output);
+    _element->element = ftxui::vbox(_innerElements);
+    return TUITypes::ElementObjectVar.Get(_element);
+  }
+};
+
+struct TUIText {
+  static SHTypesInfo inputTypes() { return CoreInfo::StringType; }
+  static SHTypesInfo outputTypes() { return CoreInfo::StringType; }
+  static SHOptionalString help() { return SHCCSTR("Adds a text element to the TUI context"); }
+
+  ParamVar _innerElementsVar{Var::ContextVar("_TUI.InnerElements")};
+
+  void warmup(SHContext *shContext) { _innerElementsVar.warmup(shContext); }
+
+  void cleanup(SHContext *shContext) { _innerElementsVar.cleanup(shContext); }
+
+  std::string _text;
+
+  void activate(SHContext *context, const SHVar &input) {
+    auto text = SHSTRVIEW(input);
+    _text.assign(text.data(), text.data() + text.size());
+    auto &innerElements = varAsObjectChecked<TUIInnerElements>(_innerElementsVar.get(), TUITypes::InnerElements);
+    innerElements.elements.push_back(ftxui::text(_text));
   }
 };
 
@@ -64,5 +169,11 @@ struct Render {
   }
 };
 } // namespace tui
-SHARDS_REGISTER_FN(tui) { using namespace tui; }
+SHARDS_REGISTER_FN(tui) {
+  using namespace tui;
+  REGISTER_SHARD("TUI.HBox", HBox);
+  REGISTER_SHARD("TUI.VBox", VBox);
+  REGISTER_SHARD("TUI.Text", TUIText);
+  REGISTER_SHARD("TUI.Render", Render);
+}
 } // namespace shards
