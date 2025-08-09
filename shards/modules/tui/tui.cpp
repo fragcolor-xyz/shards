dbdfdb94e2cbf443cd225ae4bc0fcd7582f464f0
Giovanni Petrantoni <7008900+sinkingsugar@users.noreply.github.com>
2025-08-09 21:52:45 +0800

    Add FTXUI submodule and integrate TUI module
    This commit adds the FTXUI submodule to the project and includes a new TUI module with its implementation in C++. The TUI module provides components for creating and rendering terminal user interfaces, enhancing the overall functionality of the application. Additionally, the CMake configuration has been updated to include the new module and its dependencies.

---
+++ shards/modules/tui/tui.cpp
@@ -0,0 +1,63 @@
+#include "ftxui/component/captured_mouse.hpp"     // for ftxui
+#include "ftxui/component/component.hpp"          // for Button, Horizontal, Renderer
+#include "ftxui/component/component_base.hpp"     // for ComponentBase
+#include "ftxui/component/screen_interactive.hpp" // for ScreenInteractive
+#include "ftxui/dom/elements.hpp"                 // for separator, gauge, text, Element, operator|, vbox, border
+
+#include <shards/shards.hpp>
+#include <shards/utility.hpp>
+#include <shards/core/shared.hpp>
+#include <shards/core/params.hpp>
+#include <shards/common_types.hpp>
+
+namespace shards {
+namespace tui {
+
+struct TUIElement {
+  ftxui::Element element;
+};
+
+struct TUITypes {
+  SHVAR_OBJECT_DECL('tuiE', "TUI.Element", Element, TUIElement);
+};
+
+struct HBox {
+  static SHTypesInfo inputTypes() { return CoreInfo::AnyType; }
+  static SHTypesInfo outputTypes() { return TUITypes::Element; }
+  static SHOptionalString help() { return SHCCSTR("Creates a horizontal box"); }
+
+  TUIElement *_element = nullptr;
+
+  void warmup(SHContext *shContext) { _element = TUITypes::ElementObjectVar.New(); }
+
+  void cleanup(SHContext *shContext) {
+    if (_element) {
+      TUITypes::ElementObjectVar.Release(_element);
+      _element = nullptr;
+    }
+  }
+
+  SHVar activate(SHContext *shContext, const SHVar &input) {
+    _element->element = ftxui::hbox({});
+    return TUITypes::ElementObjectVar.Get(_element);
+  }
+};
+
+struct Render {
+  static SHTypesInfo inputTypes() { return TUITypes::Element; }
+  static SHTypesInfo outputTypes() { return CoreInfo::StringType; }
+  static SHOptionalString help() { return SHCCSTR("Renders a TUI element into a string"); }
+
+  ftxui::Screen _screen = ftxui::Screen::Create(ftxui::Dimension::Full(), ftxui::Dimension::Full());
+  OwnedVar _output;
+
+  SHVar activate(SHContext *shContext, const SHVar &input) {
+    auto &element = varAsObjectChecked<TUIElement>(input, TUITypes::Element);
+    ftxui::Render(_screen, element.element);
+    _output = shards::Var(_screen.ToString());
+    return _output;
+  }
+};
+} // namespace tui
+SHARDS_REGISTER_FN(tui) { using namespace tui; }
+} // namespace shards
