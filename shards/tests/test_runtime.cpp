edaabd54ede130d685a26c984f48c47e2d2e4f97
Giovanni Petrantoni <7008900+sinkingsugar@users.noreply.github.com>
2025-11-22 12:49:50 +0800

    Fix TakeSeq/TakeTable syntax in test_runtime.cpp
    Update ':' to '.' for s:0, s:1, t:a, t:b patterns in C++ tests.

--- shards/tests/test_runtime.cpp
+++ shards/tests/test_runtime.cpp
@@ -1413,12 +1413,12 @@ TEST_CASE("shards-lang") {
   TEST_SUCCESS_CASE("Exp 3", "[(2 | Math.Multiply((3 | Math.Add(6)))) (2 | Math.Multiply(6)) (2 | Math.Multiply(12))] | Log")
   TEST_EVAL_ERROR_CASE("Failed EvalExpr", "#(false | Assert.Is(true))", "Assert failed - Is");
   TEST_SUCCESS_CASE("EvalExpr 1", "2 | Math.Multiply(#(1 | Math.Add(2) | Log)) | Log | Assert.Is(6)");
-  TEST_SUCCESS_CASE("SeqTake 1", "[1 2] | Log = s s:0 | Log | Assert.Is(1) s:1 | Log | Assert.Is(2)");
-  TEST_SUCCESS_CASE("SeqTake 2", "[1 2] | Log = s 1 | Math.Add(s:0) | Assert.Is(2)");
-  TEST_SUCCESS_CASE("TableTake 1", "{a: 1 b: 2} | Log = t t:a | Log | Assert.Is(1) t:b | Log | Assert.Is(2)");
+  TEST_SUCCESS_CASE("SeqTake 1", "[1 2] | Log = s s.0 | Log | Assert.Is(1) s.1 | Log | Assert.Is(2)");
+  TEST_SUCCESS_CASE("SeqTake 2", "[1 2] | Log = s 1 | Math.Add(s.0) | Assert.Is(2)");
+  TEST_SUCCESS_CASE("TableTake 1", "{a: 1 b: 2} | Log = t t.a | Log | Assert.Is(1) t.b | Log | Assert.Is(2)");
 
   SECTION("TableTake 2") {
-    auto code = "{a: 1 b: 2} | Log = t 1 | Math.Add(t:a) | Assert.Is(2)";
+    auto code = "{a: 1 b: 2} | Log = t 1 | Math.Add(t.a) | Assert.Is(2)";
     auto seq = readHelper(code);
     shards::OwnedVar ast{seq->ast};
     REQUIRE(ast.valueType == SHType::Object);
@@ -1429,7 +1429,7 @@ TEST_CASE("shards-lang") {
   }
 
   SECTION("_SubFlow 1") {
-    auto code = "{a: 1 b: 2} | {ToString | Assert.Is(\"{a: 1, b: 2}\") | Log} | Log = t t:a | Log | Assert.Is(1) t:b "
+    auto code = "{a: 1 b: 2} | {ToString | Assert.Is(\"{a: 1, b: 2}\") | Log} | Log = t t.a | Log | Assert.Is(1) t.b "
                 "| Log | Assert.Is(2)";
     auto seq = readHelper(code);
     shards::OwnedVar ast{seq->ast};
