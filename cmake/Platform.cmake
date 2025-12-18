710a7d3528e2e7dfe7d1badaa46e803d2db4c0c4
Giovanni Petrantoni <7008900+sinkingsugar@users.noreply.github.com>
2025-12-18 12:18:49 +0800

    Revert "Switch emscripten to WebAssembly native exceptions"
    This reverts commit 7804c56af688f8790bcf741c070e6c16e755efaf.

--- cmake/Platform.cmake
+++ cmake/Platform.cmake
@@ -240,9 +240,8 @@ if(EMSCRIPTEN)
     set(CMAKE_C_FLAGS_DEBUG "${CMAKE_C_FLAGS_DEBUG}" CACHE STRING "" FORCE)
   endif()
 
-  # Use WebAssembly native exceptions (more efficient than JS-based exceptions)
-  add_compile_options(-fwasm-exceptions)
-  add_link_options(-fwasm-exceptions)
+  add_compile_options("$<$<COMPILE_LANGUAGE:CXX>:-sDISABLE_EXCEPTION_CATCHING=0>")
+  add_link_options("-sDISABLE_EXCEPTION_CATCHING=0")
 
   add_compile_definitions(NO_FORCE_INLINE)
 
